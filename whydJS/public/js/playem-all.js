65a51ce6d9549e247571cb26cc09bcf7501810c0
Adrien Joly <adrien@whyd.com>
2016-09-27 12:16:53 +0200

    updated playemjs commit, for consistency, after moving commits from playemjs/whyd-master to playemjs/master

--- whydJS/public/js/playem-all.js
+++ whydJS/public/js/playem-all.js
@@ -1,6 +1,6 @@
 var YOUTUBE_API_KEY = "AIzaSyADm2ekf-_KONB3cSGm1fnuPSXx3br4fvI"; // dirty hack, until https://d3qdgup1c3urcq.cloudfront.net/html/YoutubePlayerIframe.html is refreshed/invalidated
 
-/* playemjs commit: ce6b71aab7f2dbbccf08ecb354f62149270bd2f0 */
+/* playemjs commit: b3a829a5283d5bc14a68a2ec1ca6fcb04cb04e0b */
 
 // configuration
 
