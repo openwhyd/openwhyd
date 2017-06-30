88678fcce24c524ae1b920821b61b7668de154ec
Adrien Joly <adrien@whyd.com>
2017-06-30 16:28:26 +0200

    testing: make sure that server is running against test db

--- whydJS/test/specs/test.js
+++ whydJS/test/specs/test.js
@@ -1,7 +1,25 @@
 var assert = require('assert');
+
+var URL_PREFIX = 'http://localhost:8080';
+
+before(function() {
+    // openwhyd/whydjs server should be tested again the test database
+    browser.url(URL_PREFIX + '/login?action=login&email=test%40openwhyd.org&md5=21232f297a57a5a743894a0e4a801fc3');
+    browser.url(URL_PREFIX + '/admin/config/config.json');
+    var config = JSON.parse(browser.getText('pre')).json;
+    assert.equal(config.mongoDbDatabase, 'openwhyd_test');
+    browser.url(URL_PREFIX + '/login?action=logout');
+});
+
 describe('landing page page', function() {
+
+    it('should not let visitors access admin endpoints', function () {
+        browser.url(URL_PREFIX + '/admin/config/config.json');
+        assert(!browser.isExisting('pre'));
+    });
+    
     it('should have Openwhyd in its title', function () {
-        browser.url('http://localhost:8080');
+        browser.url(URL_PREFIX);
         var title = browser.getTitle();
         assert(/Openwhyd/.test(title));
     });
