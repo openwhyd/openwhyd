5177f0f5e538ebd7be85702f2d433f884c668f09
Adrien Joly <adrien@whyd.com>
2017-06-30 16:28:09 +0200

    default admin email match with testing environment variables

--- whydDB/initdb_team.js
+++ whydDB/initdb_team.js
@@ -6,7 +6,7 @@
 print("upserting openwhyd team users ...");
 
 db.user.update({ "_id" : ObjectId("4d94501d1f78ac091dbc9b4d"), }, { "$set" : {
-	"email" : "admin@yoursite.com",
+	"email" : "test@openwhyd.org",
 	"name" : "admin",
 	"pwd" : "21232f297a57a5a743894a0e4a801fc3"
 } }, { upsert: true });
