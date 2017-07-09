cfc785cf1eadd595cc1432bd152a9ce0b62b84fb
Adrien Joly <adrien@whyd.com>
2017-07-09 19:58:57 +0200

    add youtube creds for dynamic player embed

--- whydJS/public/js/whydEmbed.js
+++ whydJS/public/js/whydEmbed.js
@@ -4,6 +4,7 @@
  **/
 
 var DEBUG = false, // for soundmanager
+	YOUTUBE_API_KEY = "AIzaSyADm2ekf-_KONB3cSGm1fnuPSXx3br4fvI",
 	SOUNDCLOUD_CLIENT_ID = "eb257e698774349c22b0b727df0238ad",
 	JAMENDO_CLIENT_ID = "2c9a11b9",
 	DEEZER_APP_ID = 190482,
