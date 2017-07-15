1985b202d829e4ef611bc93c4bc811b1ac822796
Adrien Joly <adrien@whyd.com>
2017-07-15 13:36:10 +0200

    test_img.js now uses `assert()`

--- whydJS/tests/test_img.js
+++ whydJS/tests/test_img.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+var fs = require('fs');
 var img = require('../node_modules/my/img');
 
 var imgUrl = 'http://www.azurs.net/photographies/laurier-rose-fleur-rebigue.jpg';
@@ -7,7 +9,11 @@ var thumbWidth = null; // auto-scaling
 var thumbHeight = 80;
 
 img.get(imgUrl, imgOutput, function() {
+  assert(fs.existsSync(imgOutput), imgOutput + ' should be downloaded by img.get()');
   img.makeThumb(imgOutput, thumbOutput, thumbWidth, thumbHeight, function() {
-    console.log(imgOutput + ' thumb saved at ' + thumbOutput);
+    assert(fs.existsSync(thumbOutput), thumbOutput + ' should be created by img.makeThumb()');
+    //console.log(imgOutput + ' thumb saved at ' + thumbOutput);
+    fs.unlinkSync(imgOutput);
+    fs.unlinkSync(thumbOutput);
   });
 });
