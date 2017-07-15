916e0f475c69d363fa12a5b7ca6dd7ccbe0d5492
Adrien Joly <adrien@whyd.com>
2017-07-15 13:40:44 +0200

    converted `test_img.js` to a proper unit test

--- whydJS/test/unit-tests.js
+++ whydJS/test/unit-tests.js
@@ -39,4 +39,28 @@ describe('"get" package', function() {
     });
 });
 
+describe('"img" package', function() {
+    it('should create a thumb from a downloaded image', function(done) {
+        var fs = require('fs');
+        var img = require('../node_modules/my/img');
+
+        var imgUrl = 'http://www.azurs.net/photographies/laurier-rose-fleur-rebigue.jpg';
+        var imgOutput = 'uniqueHash.jpg';
+        var thumbOutput = 'uniqueHash_thumb.jpg';
+        var thumbWidth = null; // auto-scaling
+        var thumbHeight = 80;
+
+        img.get(imgUrl, imgOutput, function () {
+            assert(fs.existsSync(imgOutput), imgOutput + ' should be downloaded by img.get()');
+            img.makeThumb(imgOutput, thumbOutput, thumbWidth, thumbHeight, function () {
+                assert(fs.existsSync(thumbOutput), thumbOutput + ' should be created by img.makeThumb()');
+                //console.log(imgOutput + ' thumb saved at ' + thumbOutput);
+                fs.unlinkSync(imgOutput);
+                fs.unlinkSync(thumbOutput);
+                done();
+            });
+        });
+    });
+});
+
 // Webdriver API documentation: http://webdriver.io/api.html
