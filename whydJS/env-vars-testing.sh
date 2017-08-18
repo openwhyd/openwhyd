4523f696ed35e5480393eaba97e6c451cfb97a6a
Adrien Joly <adrien@whyd.com>
2017-08-18 14:25:56 +0200

    fix for travis-ci error: `sh: 1: .: env-vars-testing.sh: not found`

--- whydJS/env-vars-testing.sh
+++ whydJS/env-vars-testing.sh
@@ -1,5 +1,6 @@
-# run source env-vars-testing.sh so that the following environment variables are usable in your current current shell session, before running tests
+# run ". env-vars-testing.sh" so that the following environment variables are usable
+# in your current current shell session, before running tests
 
 set -o allexport
-source env-vars-testing.conf # that file contains the actual configuration variables
+. ./env-vars-testing.conf # that file contains the actual configuration variables
 set +o allexport
