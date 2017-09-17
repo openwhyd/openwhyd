8ae4f4350abf4ca398db0dd1e068a2ea8c59b35e
Adrien Joly <adrien@whyd.com>
2017-09-17 11:50:30 +0200

    refacto: move common e2e/wdio testing commands to shared file

---
+++ whydJS/test/acceptance-cmds.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var { URL_PREFIX, ADMIN_USER, TEST_USER } = require('./fixtures.js');
+
+const EXPECTED_DB_NAME = 'openwhyd_test';
+const WAIT_DURATION = 10000;
+
+// custom wdio / webdriver.io commands
+
+browser.addCommand('waitForContent', function async (regex, context) {
+  return browser.waitUntil(function async() {
+    return this.getHTML(context || 'body').then((content) => {
+      //console.log(content.length, content.substr(0, 10), regex.toString(), regex.test(content))
+      return regex.test(content);
+    })},
+    WAIT_DURATION, `${regex.toString()} should be in the page within 5 seconds`
+  );
+});
+
+// make sure that openwhyd/whydjs server is tested against the test database
+browser.addCommand('checkTestDb', function async (user, dbName) {
+  browser.url(URL_PREFIX + `/login?action=login&email=${encodeURIComponent(user.email)}&md5=${user.md5}&redirect=/admin/config/config.json`);
+  return browser.getText('pre').then(function(content) {
+    var config = JSON.parse(content).json;
+    assert.equal(config.mongoDbDatabase, dbName);
+    return browser.url(URL_PREFIX + '/login?action=logout');
+  });
+});
+
+// before running tests: make sure that openwhyd/whydjs server is tested against the test database
+
+before(function() {
+  /*
+  browser.url(URL_PREFIX + `/login?action=login&email=${encodeURIComponent(ADMIN_USER.email)}&md5=${ADMIN_USER.md5}&redirect=/admin/config/config.json`);
+  var config = JSON.parse(browser.getText('pre')).json;
+  assert.equal(config.mongoDbDatabase, 'openwhyd_test');
+  return new Promise(function (resolve, reject) {
+    browser.url(URL_PREFIX + '/login?action=logout').then(resolve, reject);
+  });
+  */
+  browser.checkTestDb(ADMIN_USER, EXPECTED_DB_NAME);
+});
+
+// other helpers
+
+exports.takeSnapshot = function takeSnapshot() {
+  var results = browser.checkDocument(); // http://webdriver.io/guide/services/visual-regression.html
+  results.forEach((result) => {
+    assert(result.isWithinMisMatchTolerance, 'a difference was find on a snapshot');
+  });
+};
--- whydJS/test/specs/acceptance.tests.js
+++ whydJS/test/specs/acceptance.tests.js
@@ -4,48 +4,8 @@ const webUI = require('../web-ui.js');
 
 const WAIT_DURATION = 10000;
 
+require('../acceptance-cmds.js'); // also checks that openwhyd/whydjs server is tested against the test database
 // TODO: make sure that DB was reset before starting the whydJS app server
-
-function takeSnapshot() {
-    var results = browser.checkDocument(); // http://webdriver.io/guide/services/visual-regression.html
-    results.forEach((result) => {
-        assert(result.isWithinMisMatchTolerance, 'a difference was find on a snapshot');
-    });
-}
-
-browser.addCommand('waitForContent', function async (regex, context) {
-    return browser.waitUntil(function async() {
-        return this.getHTML(context || 'body').then((content) => {
-            //console.log(content.length, content.substr(0, 10), regex.toString(), regex.test(content))
-            return regex.test(content);
-        })},
-        WAIT_DURATION, `${regex.toString()} should be in the page within 5 seconds`
-    );
-});
-
-// make sure that openwhyd/whydjs server is tested against the test database
-browser.addCommand('checkTestDb', function async (user, dbName) {
-    browser.url(URL_PREFIX + `/login?action=login&email=${encodeURIComponent(user.email)}&md5=${user.md5}&redirect=/admin/config/config.json`);
-    return browser.getText('pre').then(function(content) {
-        var config = JSON.parse(content).json;
-        assert.equal(config.mongoDbDatabase, dbName);
-        return browser.url(URL_PREFIX + '/login?action=logout');
-    });
-    
-});
-
-before(function() {
-    // make sure that openwhyd/whydjs server is tested against the test database
-    /*
-    browser.url(URL_PREFIX + `/login?action=login&email=${encodeURIComponent(ADMIN_USER.email)}&md5=${ADMIN_USER.md5}&redirect=/admin/config/config.json`);
-    var config = JSON.parse(browser.getText('pre')).json;
-    assert.equal(config.mongoDbDatabase, 'openwhyd_test');
-    return new Promise(function (resolve, reject) {
-        browser.url(URL_PREFIX + '/login?action=logout').then(resolve, reject);
-    });
-    */
-    browser.checkTestDb(ADMIN_USER, 'openwhyd_test');
-});
 
 // reference scenario: https://www.youtube.com/watch?v=aZT8VlTV1YY
 
