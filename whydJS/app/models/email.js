f9d91cad154cde32c9c0d94e11797c2dc64df968
Serdar Sever <madznk@gmail.com>
2017-09-22 17:24:28 +0200

    revert additionnal empty line

--- whydJS/app/models/email.js
+++ whydJS/app/models/email.js
@@ -15,7 +15,6 @@ var emailImpl = require("./" + emailModule);
 // http://www.regular-expressions.info/email.html
 var emailCheck = /^[a-z0-9\u007F-\uffff!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9\u007F-\uffff!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}$/i;
 
-
 exports.validate = function(email) {
 	if (!email) return email;
 	if (typeof email != "string") {
