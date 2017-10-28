edd74181765672bfbd27b8315cfbf372ba3cd3e0
Adrien Joly <adrien.joly@gmail.com>
2017-10-28 12:52:12 +0200

    data analysis: fix plot-nb-posts-per-day map-reduce logic (MongoDB can invoke the reduce function more than once for the same key => sum instead of length of values)

--- data-scripts/data-analysis-scripts/src/plot-nb-posts-per-day.mongo.js
+++ data-scripts/data-analysis-scripts/src/plot-nb-posts-per-day.mongo.js
@@ -2,11 +2,15 @@ function map() {
   var DAY_MS = 1000 * 60 * 60 * 24;
   var renderDate = t =>
     new Date(DAY_MS * Math.floor(t / DAY_MS)).toISOString().split('T')[0];
+  // notice: MongoDB will not call the reduce function for a key that has only a single value
+  // => emit same kind of output as reduce()'s
   emit(renderDate(this._id.getTimestamp()), 1);
 }
 
-function reduce(day, n) {
-  return n.length;
+function reduce(day, count) {
+  // notice: MongoDB can invoke the reduce function more than once for the same key
+  var sum = (a, b) => a + b;
+  return count.reduce(sum);
 }
 
 var opts = {
@@ -14,9 +18,5 @@ var opts = {
   //limit: 1000
 };
 
-Object.values = Object.values || (arr => Object.keys(arr).map(key => arr[key]));
-
 var results = db.post.mapReduce(map, reduce, opts).results;
-
-//print(results.map(JSON.stringify).join('\n'));
-print(results.map(Object.values).join('\n'));
+print(results.map(res => [ res._id, res.value ]).join('\n'));
