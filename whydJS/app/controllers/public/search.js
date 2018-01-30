8d4240ff99bbf2681ccbc2b710ff67087274de22
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-01-30 21:56:24 +0100

    fix indentation and add semicolon

--- whydJS/app/controllers/public/search.js
+++ whydJS/app/controllers/public/search.js
@@ -120,7 +120,7 @@ var fetchDataByType = {
 					playlists[i]._id = pl.idParts[1];
 					playlists[i].nbTracks = c;
 					playlists[i].author = mongodb.usernames[pl.idParts[0]];
-          delete playlists[i].author.email
+					playlists[i].author.email;
 
 					postModel.fetchPlaylistPosts(pl.idParts[0], pl.idParts[1], {limit:MAX_PLAYLIST_THUMBS-1}, function(posts) {
 						for (var j in posts)
