d05ec06cce010b86065a9e6f675566a8e70a7e54
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-04-02 14:46:59 +0200

    doc(script): scripts/mongodb-scripts/tocsv.sh

--- scripts/mongodb-scripts/tocsv.sh
+++ scripts/mongodb-scripts/tocsv.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# gunzip -c ./$1.bson.gz | mongorestore --collection $1 --db openwhyd_dump -
-keys=`mongo openwhyd_dump --eval "rs.slaveOk();var keys = []; for(var key in db.$1.find().sort({_id: -1}).limit(1)[0]) { keys.push(key); }; keys.join(',');" --quiet`
-echo $keys
-mongoexport -d openwhyd_dump -c $1 --type=csv --fields "$keys" >$1.csv
+DB_NAME="openwhyd_dump"
+COLLECTION_NAME="$1"
+
+# Exports collection $COLLECTION_NAME from the $DB_NAME database, to a csv file.
+# Auto-extracts columns by fetching fields from the last record.
+
+# gunzip -c ./$COLLECTION_NAME.bson.gz | mongorestore --collection $COLLECTION_NAME --db $DB_NAME -
+
+keys=`mongo openwhyd_dump --eval "rs.slaveOk();var keys = []; for(var key in db.$COLLECTION_NAME.find().sort({_id: -1}).limit(1)[0]) { keys.push(key); }; keys.join(',');" --quiet`
+echo Keys: $keys
+
+mongoexport -d $DB_NAME -c $COLLECTION_NAME --type=csv --fields "$keys" >$COLLECTION_NAME.csv
