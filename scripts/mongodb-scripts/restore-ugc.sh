c22c6cdf1c2cba83252eed18b15eefbb7927a63e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-04-02 15:10:30 +0200

    feat(scripts): scripts to restore db dumps locally and exports to csv and json files

--- scripts/mongodb-scripts/restore-ugc.sh
+++ scripts/mongodb-scripts/restore-ugc.sh
@@ -3,7 +3,7 @@
 # Restore user-generated content tables from production mongodb dump to local db
 
 DATABASE="openwhyd_dump"
-COLLECTIONS="user follow track post playlog visit"
+COLLECTIONS="user follow track post"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 for COLLECTION in $COLLECTIONS
--- scripts/mongodb-scripts/tocsv.sh
+++ scripts/mongodb-scripts/tocsv.sh
@@ -6,8 +6,6 @@ COLLECTION_NAME="$1"
 # Exports collection $COLLECTION_NAME from the $DB_NAME database, to a csv file.
 # Auto-extracts columns by fetching fields from the last record.
 
-# gunzip -c ./$COLLECTION_NAME.bson.gz | mongorestore --collection $COLLECTION_NAME --db $DB_NAME -
-
 keys=`mongo openwhyd_dump --eval "rs.slaveOk();var keys = []; for(var key in db.$COLLECTION_NAME.find().sort({_id: -1}).limit(1)[0]) { keys.push(key); }; keys.join(',');" --quiet`
 echo Keys: $keys
 
