3f038c4bc447c463001936b2a4fe9e3a58f23b12
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-04-14 22:53:58 +0200

    fix(data-scripts): plot-nb-users-listening-per-day <— playlog

--- scripts/data-analysis-scripts/src/plot-nb-users-listening-per-day.sh
+++ scripts/data-analysis-scripts/src/plot-nb-users-listening-per-day.sh
@@ -1,15 +1,25 @@
 TITLE="Number of Users listening per day"
-DB=openwhyd_dump
-NAME=plot-nb-users-listening-per-day
-COLUMNS=date,users
+NAME="plot-nb-users-listening-per-day"
+COLUMNS="date,users"
 NB_COLUMNS=2
 
-echo "generate data from mongodb data ... (⚠️ may take several minutes)"
-echo $COLUMNS >$NAME.temp.csv
+# DB=openwhyd_dump
+# echo "generate data from mongodb data ... (⚠️ may take several minutes)"
+# echo $COLUMNS >$NAME.temp.csv
+# SECONDS=0
+# mongo --quiet $DB ./$NAME.mongo.js # took 10 minutes to run
+# echo ⏲ $SECONDS seconds.
+# mongoexport -d "$DB" -c "$NAME" --type=csv --fields "_id,value" | tail -n+2  >>$NAME.temp.csv
+
+echo "map-reducing data from playlog ... (⚠️  may take several minutes)"
 SECONDS=0
-mongo --quiet $DB ./$NAME.mongo.js # took 10 minutes to run
-echo ⏲ $SECONDS seconds.
-mongoexport -d "$DB" -c "$NAME" --type=csv --fields "_id,value" | tail -n+2  >>$NAME.temp.csv
+node json-helpers/run-mongo-script-from-json-dump.js $NAME.mongo.js ../playlog.json.log >$NAME.temp.json
+echo ⏲  $SECONDS seconds.
+
+echo "convert data to csv ..."
+node convert-json-to-csv.js $NAME.temp.json >$NAME.temp.csv
+# rename csv headers
+sed -i '' "1s/.*/$COLUMNS/" $NAME.temp.csv
 sed -i '' -e '$ d' $NAME.temp.csv # remove last line
 
 echo "plot data to ../plots/$NAME.png ..."
