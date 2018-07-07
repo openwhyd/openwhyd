1c3bddcd5c221597974e793b2dad793825a168d3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-07 17:01:17 +0200

    doc: add /contribute route that links to contribution FAQ on github

--- whydJS/app/controllers/static.js
+++ whydJS/app/controllers/static.js
@@ -12,19 +12,21 @@
 var config = require("../models/config.js");
 var runsLocally = config.urlPrefix.indexOf("localhost") != -1;
 
-var SUPPORT_PAGE = "https://github.com/openwhyd/openwhyd/blob/master/docs/FAQ.md#how-to-contact-openwhyds-team";
+var GITHUB_URL = "https://github.com/openwhyd/openwhyd";
+var SUPPORT_PAGE = GITHUB_URL + "/blob/master/docs/FAQ.md#how-to-contact-openwhyds-team";
 var DONATE_PAGE = "https://opencollective.com/openwhyd";
 var PRIVACY_PAGE = config.urlPrefix + "/privacy";
 
 var STATIC_FILES = {
 	"/favicon.ico": "/images/favicon"+(runsLocally ? "_orange" : "")+".ico",
 	"/favicon.png": "/images/favicon"+(runsLocally ? "_orange" : "")+".png",
-	"/faq": "https://github.com/openwhyd/openwhyd/blob/master/docs/FAQ.md",
+	"/community": GITHUB_URL,
+	"/faq": GITHUB_URL + "/blob/master/docs/FAQ.md",
+	"/contribute": GITHUB_URL + "/blob/master/docs/FAQ.md#id-love-to-contribute-to-openwhyd-how-can-i-help",
 	"/help": SUPPORT_PAGE,
 	"/team": SUPPORT_PAGE,
 	"/support": SUPPORT_PAGE,
 	"/contact": SUPPORT_PAGE,
-	"/community": "https://github.com/openwhyd/openwhyd",
 	"/sponsor": DONATE_PAGE,
 	"/donate": DONATE_PAGE,
 	"/tos": PRIVACY_PAGE,
