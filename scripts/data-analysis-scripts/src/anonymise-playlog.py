2c095d22a7245fa67964ed30c0273300ce9db8d2
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-10-30 21:35:24 +0100

    chore: add python script to anonymise playlog, for data analysis

---
+++ scripts/data-analysis-scripts/src/anonymise-playlog.py
@@ -0,0 +1,31 @@
+# This script turns playlog.json.log to a csv files in which
+# each line associates a user (identified by a number) to a
+# youtube track id that the user listened to.
+#
+# It was written by Mihangy, Damien and Adrien, during Hackergarten
+
+import json
+
+users = dict() # uId (input) -> user number (output)
+songs = dict() # eId (input), just to count unique youtube tracks
+
+counter = 0
+
+print('user,song') # header of the csv file
+
+with open("./playlog.json.log", "r") as f:
+  for line in f:
+    counter = counter + 1
+    # if counter == 10:
+    #   break
+    row = json.loads(line)
+    # identify songs
+    if '/yt/' in row['eId']:
+      eId = row['eId'][4:]
+      songs[eId] = 1
+    # identify users
+    if row['uId'] not in users:
+      users[row['uId']] = len(users)
+    print users[row['uId']], ',', eId
+
+# print 'found', len(songs), 'youtube tracks'
