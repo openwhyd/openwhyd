023c00f5deeef7d6558c406ac3942b584c30e367
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-03-03 19:00:13 +0100

    refacto: specify redirect titles in object + leaner redirection pages

--- app/controllers/redir.js
+++ app/controllers/redir.js
@@ -6,38 +6,38 @@
 var config = require('../models/config.js');
 
 var GITHUB_URL = 'https://github.com/openwhyd/openwhyd';
-var FAQ_PAGE = GITHUB_URL + '/blob/master/docs/FAQ.md';
-var SUPPORT_PAGE =
-  GITHUB_URL + '/blob/master/docs/FAQ.md#how-to-contact-openwhyds-team';
+var API_PAGE = 'https://openwhyd.github.io/openwhyd/API';
+var FAQ_PAGE = 'https://openwhyd.github.io/openwhyd/FAQ';
+var SUPPORT_PAGE = FAQ_PAGE + '#how-to-contact-openwhyds-team';
 var DONATE_PAGE = 'https://opencollective.com/openwhyd';
 var PRIVACY_PAGE = config.urlPrefix + '/privacy';
 
 var REDIRECTIONS = {
   '/about': [
     'https://medium.com/@adrienjoly/music-amongst-other-topics-a4f41657d6d',
-    "Openwhyd's story"
+    { title: "Openwhyd's story" }
   ],
-  '/api': [GITHUB_URL + '/blob/master/docs/API.md', 'API Documentation'],
-  '/community': [GITHUB_URL, 'Community'],
-  '/contact': [SUPPORT_PAGE, 'Contact'],
+  '/api': [API_PAGE, { title: 'API Documentation' }],
+  '/community': [GITHUB_URL, { title: 'Community' }],
+  '/contact': [SUPPORT_PAGE, { title: 'Contact' }],
   '/contribute': [
     FAQ_PAGE + '#id-love-to-contribute-to-openwhyd-how-can-i-help',
-    'Contribute'
+    { title: 'Contribute' }
   ],
-  '/donate': [DONATE_PAGE, 'Donate'],
-  '/faq': [FAQ_PAGE, 'Frequently Asked Questions'],
-  '/help': [SUPPORT_PAGE, 'Help'],
-  '/sponsor': [DONATE_PAGE, 'Sponsor'],
-  '/support': [SUPPORT_PAGE, 'Support'],
-  '/team': [SUPPORT_PAGE, 'Team'],
-  '/tos': [PRIVACY_PAGE, 'Terms of Service']
+  '/donate': [DONATE_PAGE, { title: 'Donate' }],
+  '/faq': [FAQ_PAGE, { title: 'Frequently Asked Questions' }],
+  '/help': [SUPPORT_PAGE, { title: 'Help' }],
+  '/sponsor': [DONATE_PAGE, { title: 'Sponsor' }],
+  '/support': [SUPPORT_PAGE, { title: 'Support' }],
+  '/team': [SUPPORT_PAGE, { title: 'Team' }],
+  '/tos': [PRIVACY_PAGE, { title: 'Terms of Service' }]
 };
 
 exports.controller = function(request, reqParams, response) {
   var path = request.url.split('?')[0];
-  var [redirUrl, redirTitle] = REDIRECTIONS[path] || [];
+  var [redirUrl, { title } = {}] = REDIRECTIONS[path] || [];
   if (redirUrl) {
-    response.redirectWithTracking(redirUrl, redirTitle);
+    response.redirectWithTracking(redirUrl, title);
   } else {
     response.notFound();
   }
