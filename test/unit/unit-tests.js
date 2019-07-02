653415b08a98d5e3ea1da739fd219f05e6556480
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-07-02 17:48:46 +0200

    feat(tests): run « get » tests against local web server instead of openwhyd.org and google.com

--- test/unit/unit-tests.js
+++ test/unit/unit-tests.js
@@ -1,4 +1,13 @@
 var assert = require('assert');
+const express = require('express');
+
+const HTML_PAGE_WITH_TITLE_AND_IMAGES = [
+  '<!DOCTYPE html>',
+  '<html>',
+  '<head><title>foo</title></head>',
+  '<body><img src="1.jpg"></body>',
+  '</html>'
+].join('\n');
 
 describe('snip.js', function() {
   var snip = require('../../app/snip.js');
@@ -26,10 +35,22 @@ describe('snip.js', function() {
 describe('"get" package', function() {
   var get = require('../../app/lib/get');
 
+  const expressApp = express();
+  let expressServer;
+
+  this.beforeAll(done => {
+    expressServer = expressApp.listen(3000, done);
+  });
+
+  this.afterAll(() => expressServer.close());
+
   it('should provide the title of a web page', function(done) {
-    get.Title('https://www.google.com/', function(err, title) {
+    expressApp.get('/title.html', function(req, res) {
+      res.send(HTML_PAGE_WITH_TITLE_AND_IMAGES);
+    });
+    get.Title('http://localhost:3000/title.html', function(err, title) {
       assert.ifError(err);
-      assert.equal(title, 'Google');
+      assert.equal(title, 'foo');
       done();
     });
   });
@@ -55,7 +76,10 @@ describe('"get" package', function() {
     });
     */
   it('should provide the images of a web page', function(done) {
-    get('https://openwhyd.org', function(err, page) {
+    expressApp.get('/images.html', function(req, res) {
+      res.send(HTML_PAGE_WITH_TITLE_AND_IMAGES);
+    });
+    get('http://localhost:3000/images.html', function(err, page) {
       assert.ifError(err);
       assert(page.getTitle());
       assert(page.getImages().length);
