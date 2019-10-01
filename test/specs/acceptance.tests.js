de2843724d14ac06011d38e37bb0d47102afd36a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-10-02 00:08:59 +0200

    fix(deps): update to wdio 5 (webdriver.io) (#229)
    On several machines (including on circle-ci, after upgrading node.js), I saw that webdriver-based tests were failing.
    
    => Upgrading from webdriver 4 to webdriver 5.
    
    * trying to run tests with webdriverio v5
    
    * use standalone instead of chrome driver
    
    * lint fix
    
    * migrate: browser.waitForVisible() -> $().waitForDisplayed()
    
    * migrate: browser.isExisting() -> $().isExisting()
    
    * migrate: browser.click() -> $().click()
    
    * migrate: browser.setValue() -> $().setValue()
    
    * lint fix
    
    * migrate: browser.scroll() -> $().scrollIntoView()
    
    * migrate: await $() in command, before calling getHTML()
    
    * migrate: browser.getText() -> $().getText()
    
    * migrate: browser.waitForText() -> $().waitForDisplayed()
    
    * remove waitForReady() command
    
    * migrate: browser.isVisible() -> $().isDisplayedInViewport()
    
    * fix waitForLinkWithText() command
    
    * migrate: browser.waitForExist() -> $().waitForExist()
    
    * fix call to browser.execute()
    
    * remove useless logging
    
    * remove test that fails to access chrome's logs
    
    * fix call to waitForContent() command
    
    * migrate: browser.element() -> $() (works for xPath)

--- test/specs/acceptance.tests.js
+++ test/specs/acceptance.tests.js
@@ -9,13 +9,13 @@ require('../acceptance-cmds.js'); // also checks that openwhyd's server is teste
 
 // reference scenario: https://www.youtube.com/watch?v=aZT8VlTV1YY
 
-describe('landing page page', function() {
-  it('should not let visitors access admin endpoints', function() {
+describe('landing page page', function () {
+  it('should not let visitors access admin endpoints', function () {
     browser.url(URL_PREFIX + '/admin/config/config.json');
-    assert(!browser.isExisting('pre'));
+    assert(!$('pre').isExisting());
   });
 
-  it('should have Openwhyd in its title', function() {
+  it('should have Openwhyd in its title', function () {
     browser.url(URL_PREFIX);
     var title = browser.getTitle();
     assert(/Openwhyd/.test(title));
@@ -32,28 +32,25 @@ describe('landing page page', function() {
     */
 });
 
-describe('onboarding', function() {
-  it('should lead new user to genre selection page', function() {
-    browser
-      .url(URL_PREFIX)
-      .click('#signup')
-      .waitForVisible('input[name="email"]');
-    browser
-      .setValue('input[name="name"]', TEST_USER.username)
-      .setValue('input[name="email"]', TEST_USER.email)
-      .setValue('input[name="password"]', TEST_USER.pwd);
+describe('onboarding', function () {
+  it('should lead new user to genre selection page', function () {
+    browser.url(URL_PREFIX);
+    $('#signup').click();
+    $('input[name="email"]').waitForDisplayed();
+    $('input[name="name"]').setValue(TEST_USER.username);
+    $('input[name="email"]').setValue(TEST_USER.email);
+    $('input[name="password"]').setValue(TEST_USER.pwd);
     // TODO: takeSnapshot();
-    browser
-      .click('input[type="submit"]')
-      .waitUntil(
-        () => /.*\/pick\/genres/.test(browser.getUrl()),
-        WAIT_DURATION,
-        'expected to be on /pick/genres after 5s'
-      );
+    $('input[type="submit"]').click();
+    browser.waitUntil(
+      () => /.*\/pick\/genres/.test(browser.getUrl()),
+      WAIT_DURATION,
+      'expected to be on /pick/genres after 5s'
+    );
     // TODO: takeSnapshot();
   });
 
-  it('should suggest people to follow after picking genres', function() {
+  it('should suggest people to follow after picking genres', function () {
     const genres = $$('#genreGallery li');
     genres.find(genre => /INDIE/.test(genre.getText())).click();
     genres.find(genre => /ROCK/.test(genre.getText())).click();
@@ -67,7 +64,7 @@ describe('onboarding', function() {
     );
   });
 
-  it('should suggest to install the extension after picking people', function() {
+  it('should suggest to install the extension after picking people', function () {
     // TODO: takeSnapshot();
     browser.clickOnLinkWithText('Next');
     browser.waitUntil(
@@ -77,7 +74,7 @@ describe('onboarding', function() {
     );
   });
 
-  it('should lead new user to the gdpr consent page, after installing extension', function() {
+  it('should lead new user to the gdpr consent page, after installing extension', function () {
     // TODO: takeSnapshot();
     browser.clickOnLinkWithText('Next');
     browser.waitUntil(
@@ -87,12 +84,12 @@ describe('onboarding', function() {
     );
   });
 
-  it('should lead to the welcome page, after giving consent', function() {
+  it('should lead to the welcome page, after giving consent', function () {
     // TODO: takeSnapshot();
     browser.waitForContent(/consent to let Openwhyd collect/); // text of the consent checkbox
-    browser.scroll('input[type="checkbox"]');
-    browser.click('input[type="checkbox"]');
-    browser.click('input[type="submit"]');
+    $('input[type="checkbox"]').scrollIntoView();
+    $('input[type="checkbox"]').click();
+    $('input[type="submit"]').click();
     browser.waitUntil(
       () => /.*\/welcome/.test(browser.getUrl()),
       WAIT_DURATION,
@@ -100,20 +97,20 @@ describe('onboarding', function() {
     );
   });
 
-  it('should display user name after skipping the welcome tutorial', function() {
+  it('should display user name after skipping the welcome tutorial', function () {
     // TODO: takeSnapshot();
     browser.waitForContent(/Ok\, Got it/);
-    var loggedInUsername = browser.getText('#loginDiv .username');
+    var loggedInUsername = $('#loginDiv .username').getText();
     assert.equal(loggedInUsername, TEST_USER.username);
   });
 
   webUI.logout();
 });
 
-describe('adding a track', function() {
+describe('adding a track', function () {
   it('should allow user to login', webUI.loginAs(ADMIN_USER));
 
-  it('should lead user to the gdpr consent page', function() {
+  it('should lead user to the gdpr consent page', function () {
     browser.waitUntil(
       () => /.*\/consent/.test(browser.getUrl()),
       WAIT_DURATION,
@@ -121,14 +118,14 @@ describe('adding a track', function() {
     );
     // now, let's give consent
     browser.waitForContent(/consent to let Openwhyd collect/); // text of the consent checkbox
-    browser.scroll('input[type="checkbox"]');
-    browser.click('input[type="checkbox"]');
-    browser.click('input[type="submit"]');
+    $('input[type="checkbox"]').scrollIntoView();
+    $('input[type="checkbox"]').click();
+    $('input[type="submit"]').click();
   });
 
-  it('should recognize a track when pasting a Youtube URL in the search box', function() {
+  it('should recognize a track when pasting a Youtube URL in the search box', function () {
     //browser.url(URL_PREFIX + '/all');
-    browser.waitForReady();
+    $('#q').waitForExist();
     $('#q').setValue('https://www.youtube.com/watch?v=aZT8VlTV1YY');
     browser.waitUntil(
       () => $$('#searchResults li a').find(a => /Demo/.test(a.getText())),
@@ -137,8 +134,8 @@ describe('adding a track', function() {
     );
   });
 
-  it('should lead to a track page when clicking on the Youtube search result', function() {
-    browser.click('#searchResults li a');
+  it('should lead to a track page when clicking on the Youtube search result', function () {
+    $('#searchResults li a').click();
     browser.waitUntil(
       () => /\/yt\/aZT8VlTV1YY/.test(browser.getUrl()),
       WAIT_DURATION,
@@ -146,7 +143,7 @@ describe('adding a track', function() {
     );
   });
 
-  it('should display the name of the track', function() {
+  it('should display the name of the track', function () {
     /*
         browser.waitForContent(/Openwhyd Demo/); // name of the track, fetched asynchronously from youtube
         browser.waitForContent(/Add to/);
@@ -154,109 +151,109 @@ describe('adding a track', function() {
     const containsName = () => {
       var crit = false;
       try {
-        crit = /Openwhyd Demo \(formerly/.test(browser.getHTML('a.btnRepost'));
-      } catch (e) {}
+        crit = /Openwhyd Demo \(formerly/.test($('a.btnRepost').getHTML());
+      } catch (e) { }
       return crit;
     };
     browser.waitUntil(containsName, WAIT_DURATION);
     assert(true);
   });
 
-  it('should open a dialog after clicking on the "Add to" button', function() {
-    //browser.click('a.btnRepost');
+  it('should open a dialog after clicking on the "Add to" button', function () {
+    //$('a.btnRepost').click();
     browser.clickOnLinkWithText('Add to');
-    browser.waitForVisible('.dlgPostBox');
+    $('.dlgPostBox').waitForDisplayed();
     browser.pause(1000);
-    assert(browser.element('.dlgPostBox').isVisible());
+    assert($('.dlgPostBox').isDisplayedInViewport());
   });
 
-  it('should show a link to the post after adding the track', function() {
+  it('should show a link to the post after adding the track', function () {
     $$('.dlgPostBox span')
       .find(a => /Add/.test(a.getText()))
       .click();
     browser.waitForLinkWithText('your tracks');
   });
 
-  it("should show the post on the user's profile after clicking the link", function() {
+  it("should show the post on the user's profile after clicking the link", function () {
     browser.clickOnLinkWithText('your tracks');
     browser.waitUntil(
       () => /\/u\//.test(browser.getUrl()),
       WAIT_DURATION,
       "expected to be on the user's profile page after 5s"
     );
-    browser.waitForVisible('.post a[data-eid="/yt/aZT8VlTV1YY"]');
+    $('.post a[data-eid="/yt/aZT8VlTV1YY"]').waitForDisplayed();
   });
 
-  it('should open the playbar after the user clicks on the post', function() {
-    browser.click('.post a[data-eid="/yt/aZT8VlTV1YY"]');
-    browser.waitForVisible('#btnPlay');
+  it('should open the playbar after the user clicks on the post', function () {
+    $('.post a[data-eid="/yt/aZT8VlTV1YY"]').click();
+    $('#btnPlay').waitForDisplayed();
   });
 
-  it('should play the track', function() {
-    browser.waitForVisible('#btnPlay.playing');
+  it('should play the track', function () {
+    $('#btnPlay.playing').waitForDisplayed();
   });
 
-  it('should pause the track when the user clicks on the play/pause button', function() {
-    browser.click('#btnPlay.playing');
+  it('should pause the track when the user clicks on the play/pause button', function () {
+    $('#btnPlay.playing').click();
     assert(!/playing/.test($('#btnPlay').classname));
   });
 
   //webUI.logout();
 });
 
-describe('re-adding a track in a playlist', function() {
+describe('re-adding a track in a playlist', function () {
   // requirement: one track should be accessible from the user's stream
 
   // webUI.loginAs(ADMIN_USER);
 
-  it('will display a pop-in dialog when clicking the "Add to" button of that track', function() {
+  it('will display a pop-in dialog when clicking the "Add to" button of that track', function () {
     browser.waitForContent(/Add to/);
     browser.clickOnLinkWithText('Add to');
-    browser.waitForVisible('.dlgPostBox');
+    $('.dlgPostBox').waitForDisplayed();
   });
 
-  it('allows to create a new playlist', function() {
-    browser.waitForVisible('#selPlaylist');
+  it('allows to create a new playlist', function () {
+    $('#selPlaylist').waitForDisplayed();
     browser.pause(1000); // leave some time for onclick handler to be setup
     $('#selPlaylist').click();
-    browser.waitForVisible('#newPlaylistName');
+    $('#newPlaylistName').waitForDisplayed();
     $('#newPlaylistName').setValue('test playlist');
     $('input[value="Create"]').click();
     browser.waitForContent(/test playlist/, '#selPlaylist');
   });
 
-  it('should show a link to the post after re-adding the track', function() {
+  it('should show a link to the post after re-adding the track', function () {
     $$('.dlgPostBox span')
       .find(a => /Add/.test(a.getText()))
       .click();
     browser.waitForLinkWithText('test playlist');
   });
 
-  it("should show the post on the user's new playlist after clicking the link", function() {
+  it("should show the post on the user's new playlist after clicking the link", function () {
     browser.clickOnLinkWithText('test playlist');
     browser.waitUntil(
       () => /\/u\//.test(browser.getUrl()),
       WAIT_DURATION,
       "expected to be on the user's playlist page after 5s"
     );
-    browser.waitForVisible('.post a[data-eid="/yt/aZT8VlTV1YY"]');
+    $('.post a[data-eid="/yt/aZT8VlTV1YY"]').waitForDisplayed();
   });
 
   //webUI.logout();
 });
 
-describe('track comments', function() {
+describe('track comments', function () {
   // requirement: at least one track should be accessible from the user's stream
 
   // webUI.loginAs(ADMIN_USER);
 
-  it(`can be displayed from the user\'s stream`, function() {
+  it(`can be displayed from the user\'s stream`, function () {
     browser.url(URL_PREFIX + '/stream');
     browser.clickOnLinkWithText('Comment');
     browser.waitForContent(/You can mention people/);
   });
 
-  it(`should appear after being added`, function() {
+  it(`should appear after being added`, function () {
     browser.keys('hello world\n');
     browser.waitForContent(new RegExp(ADMIN_USER.name), '.comments');
     browser.waitForContent(/hello world/, '.comments');
@@ -267,17 +264,17 @@ describe('track comments', function() {
   // TODO: it(`should disappear after being deleted`, function() {
 });
 
-describe('searching external tracks', function() {
-  it(`can find a youtube track with id that starts with underscore`, function() {
+describe('searching external tracks', function () {
+  it(`can find a youtube track with id that starts with underscore`, function () {
     browser.url(URL_PREFIX);
     $('#q').click();
     browser.keys('http://www.youtube.com/watch?v=_BU841qpQsI');
     const searchResult = `a[onclick="window.goToPage('/yt/_BU841qpQsI');return false;"]`;
-    browser.waitForVisible(searchResult);
+    $(searchResult).waitForDisplayed();
     const trimmed = $(searchResult)
       .getText()
       .trim();
-    console.log('text: ', trimmed);
+    //console.log('text: ', trimmed);
     assert.notEqual(trimmed, ''); // empty string => no metadata was fetched, caused to https://github.com/openwhyd/openwhyd/issues/102
   });
 });
