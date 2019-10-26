75191c03734232c4ba80c3868db1b97c98d7d23e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-10-26 14:59:52 +0200

    fix typos and refresh comments

--- app/controllers/api/login.js
+++ app/controllers/api/login.js
@@ -1,7 +1,5 @@
 /**
- * login controller
- * authentify users, with IE support
- * @author adrienjoly, whyd
+ * login controller, to authenticate users
  */
 var config = require('../../models/config.js');
 var emailModel = require('../../models/email.js');
@@ -118,11 +116,11 @@ exports.handleRequest = function(request, form, response, ignorePassword) {
             userModel.update(dbUser._id, {
               $set: {
                 fbId: form.fbUid,
-                fbTok: form.fbTok // accesstoken provided on last facebook login
+                fbTok: form.fbTok // access token provided on last facebook login
               }
             });
           renderRedirect(form.redirect || '/', dbUser);
-          return; // prevent default reponse (renderForm)
+          return; // prevent default response (renderForm)
         } else if (form.action != 'logout') {
           form.wrongPassword = 1;
           form.error = 'Your password seems wrong... Try again!';
