12563d27bb0936d5e87a3d7a7a7421f312c4ca79
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-10-27 17:12:03 +0100

    for current tests: change admin user id to 000xx01

--- config/initdb_team.js
+++ config/initdb_team.js
@@ -6,7 +6,7 @@
 print('upserting openwhyd team users ...');
 
 db.user.update(
-  { _id: ObjectId('4d94501d1f78ac091dbc9b4d') },
+  { _id: ObjectId('000000000000000000000001') },
   {
     $set: {
       email: 'test@openwhyd.org',
