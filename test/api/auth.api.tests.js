4ac9a67104df46c45bb4cfe9d0a540aca02c56e6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-10-27 19:39:41 +0100

    modernize auth.api.tests.js

--- test/api/auth.api.tests.js
+++ test/api/auth.api.tests.js
@@ -1,97 +1,72 @@
 var { promisify } = require('util');
 var assert = require('assert');
-var request = require('request');
+const request = promisify(require('request'));
 
 var { URL_PREFIX, ADMIN_USER, TEST_USER } = require('../fixtures.js');
-var { logout, loginAs, signupAs } = require('../api-client.js');
+const apiClient = require('../api-client.js');
 
-describe('auth api -- login with email', function() {
+const logout = promisify(apiClient.logout);
+const loginAs = promisify(apiClient.loginAs);
+const signupAs = promisify(apiClient.signupAs);
+
+describe('auth api -- login with email', () => {
   it('succeeds', async () => {
-    const { response, body } = await promisify(loginAs)(ADMIN_USER);
+    const { response, body } = await loginAs(ADMIN_USER);
     const cookies = ((response.headers || {})['set-cookie'] || []).join(' ');
     assert(/whydSid\=/.test(cookies));
     assert(JSON.parse(body).redirect);
   });
 
-  it('gives access to personal /stream', function(done) {
-    loginAs(ADMIN_USER, function(error, { response, body, jar }) {
-      request({ jar, url: URL_PREFIX + '/stream?format=json' }, function(
-        error,
-        response,
-        body
-      ) {
-        assert.ifError(error);
-        assert.equal(response.statusCode, 200);
-        var json = JSON.parse(body);
-        assert.ifError(json.error);
-        assert(json.join); // check that it's an array
-        done();
-      });
+  it('gives access to personal /stream', async () => {
+    const { jar } = await loginAs(ADMIN_USER);
+    const response = await request({
+      jar,
+      url: URL_PREFIX + '/stream?format=json'
     });
+    assert.equal(response.statusCode, 200);
+    var json = JSON.parse(response.body);
+    assert.ifError(json.error);
+    assert(json.join); // check that it's an array
   });
 
-  it('fails if wrong email', function(done) {
-    loginAs(Object.assign({}, ADMIN_USER, { email: 'qq' }), function(
-      error,
-      { response, body }
-    ) {
-      assert(/email/.test(JSON.parse(body).error));
-      done();
-    });
+  it('fails if wrong email', async () => {
+    const { body } = await loginAs({ ...ADMIN_USER, email: 'qq' });
+    assert(/email/.test(JSON.parse(body).error));
   });
 
-  it('fails if wrong password', function(done) {
-    loginAs(Object.assign({}, ADMIN_USER, { md5: 'qq' }), function(
-      error,
-      { response, body }
-    ) {
-      assert(JSON.parse(body).wrongPassword);
-      done();
-    });
+  it('fails if wrong password', async () => {
+    const { body } = await loginAs({ ...ADMIN_USER, md5: 'qq' });
+    assert(JSON.parse(body).wrongPassword);
   });
 });
 
-describe('auth api -- logout', function() {
-  it('denies access to personal /stream', function(done) {
-    loginAs(ADMIN_USER, function(error, { response, body, jar }) {
-      logout(jar, function(error, { response, body, jar }) {
-        request({ jar, url: URL_PREFIX + '/stream?format=json' }, function(
-          error,
-          response,
-          body
-        ) {
-          assert.ifError(error);
-          assert(/login/.test(JSON.parse(body).error));
-          done();
-        });
-      });
+describe('auth api -- logout', () => {
+  it('denies access to personal /stream', async () => {
+    const { jar: loginJar } = await loginAs(ADMIN_USER);
+    const { jar } = await logout(loginJar);
+    const response = await request({
+      jar,
+      url: URL_PREFIX + '/stream?format=json'
     });
+    assert(/login/.test(JSON.parse(response.body).error));
   });
 });
 
-//describe('auth api -- forgot password', function() {}); // TODO <= mock emails
+//describe('auth api -- forgot password', () => {); // TODO <= mock emails
 
 // Register / sign up a new user
 
-describe('auth api -- signup', function() {
-  it('gives access to personal /stream', function(done) {
-    signupAs(TEST_USER, function(error, { response, body, jar }) {
-      assert.ifError(error);
-      try {
-        assert.ifError(JSON.parse(body).error);
-      } catch (e) {}
-      request({ jar, url: URL_PREFIX + '/stream?format=json' }, function(
-        error,
-        response,
-        body
-      ) {
-        assert.ifError(error);
-        assert.equal(response.statusCode, 200);
-        var json = JSON.parse(body);
-        assert.ifError(json.error);
-        assert(json.join); // check that it's an array
-        done();
-      });
+describe('auth api -- signup', () => {
+  it('gives access to personal /stream', async () => {
+    const { jar, body } = await signupAs(TEST_USER);
+    assert.ifError(body.error);
+    const response = await request({
+      jar,
+      url: URL_PREFIX + '/stream?format=json'
     });
+    assert.equal(response.statusCode, 200);
+    const json = JSON.parse(response.body);
+    assert.ifError(json.error);
+    assert(json.join); // check that it's an array
   });
 });
