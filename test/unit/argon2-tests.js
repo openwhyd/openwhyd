8e6399935d289381c4061938d225b233037aad12
agatesse <agathe.vaisse@gmail.com>
2019-11-26 21:17:21 +0100

    Invalid password test

--- test/unit/argon2-tests.js
+++ test/unit/argon2-tests.js
@@ -2,17 +2,24 @@ const assert = require('assert');
 const argon2 = require('argon2');
 
 describe('argon2', function() {
-  it('should cypher a secret', async () => {
+  it('should hash a password', async () => {
     const hash = await argon2.hash('password');
     const hex = hash.toString('hex');
     assert(hex);
   });
 
-  it('should generate the same hash, given a same salt', async () => {
+  it('should check if password is valid', async () => {
     const password = 'secret';
     const hash = await argon2.hash(password);
     const valid = await argon2.verify(hash, password);
     assert(valid);
+  });
+
+  it('should check if password is invalid', async () => {
+    const password = 'secret';
+    const hash = await argon2.hash(password);
+    const valid = await argon2.verify(hash, 'secret2');
+    assert.strictEqual(valid, false);
   });
 
   it('should not generate the same hash, given a different salt', async () => {
