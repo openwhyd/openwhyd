5cd2c93902474e4e62132b739f58890482d91887
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-09 16:01:28 +0100

    cleanup tests

--- cypress/integration/bookmarklet.spec.js
+++ cypress/integration/bookmarklet.spec.js
@@ -40,51 +40,16 @@ context('Openwhyd', () => {
     // should list more than 1 track
     cy.get('.whydThumb').should('have.length.above', 1);
 
-    // TODO:
     /*
     // should list the main track of the page
     cy.get('.whydThumb', { timeout: 10000 }).should(function($thumbs) {
-      //expect($thumbs).to.have.length.above(1);
-      //if (!$thumbs) return; //throw new Error('we expect thumbs');
-      // cy.log($thumbs);
-      // cy.log($thumbs.length);
-      if ($thumbs.length < 1) {
-        throw new Error('we expect at least one thumb');
+      cy.log('coucou', ($thumbs || []).length); // problem: always zero...
+      for (const i = 0; i < ($thumbs || []).length; i++) {
+        cy.log(i, $thumbs[i]);
+        const style = Cypress.$($thumbs[i]).attr('style');
+        expect(style).to.include(youtubeId);
       }
-      for (const i = 0; i < $thumbs.length; i++) {
-        if ($thumbs[i].style.includes(youtubeId)) return;
-      }
-      throw new Error('we expect one thumb to match the video');
-      // const className = $div[0].className;
-
-      // if (!className.match(/heading-/)) {
-      //   throw new Error(`No class "heading-" in ${className}`);
-      // }
-      // //.should('contain', youtubeId)
-      // cy.log(typeof $thumbs, $thumbs, $thumbs.toString());
-      // const styles = $thumbs.map((i, el) => {
-      //   cy.log({ i, style: Cypress.$(el).attr('style') });
-      //   //return Cypress.$(el).attr('style');
-      // });
-      // //return styles.find(style => style.includes(youtubeId));
     });
-  
-    it(`should list no more than 10 tracks`, function() {
-      const timeoutMs = 10 * 1000;
-      const tStart = Date.now();
-      return new Promise((resolve, reject) => {
-        const interval = setInterval(async () => {
-          const nbThumbs = (await $$('.whydThumb')).length;
-          console.log('number of .whydThumb elements', nbThumbs);
-          if (nbThumbs > 10) {
-            clearInterval(interval);
-            reject(new Error('expected 10 tracks or less. got ' + nbThumbs));
-          } else if (Date.now() - tStart >= timeoutMs) {
-            clearInterval(interval);
-            resolve();
-          }
-        }, 1000);
-      });
-      */
+    */
   });
 });
