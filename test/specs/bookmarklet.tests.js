ab3a95cbb36f954b53d8498fcea7e17ce57f79d5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-09 16:27:43 +0100

    clean up bookmarklet test suite

--- test/specs/bookmarklet.tests.js
+++ test/specs/bookmarklet.tests.js
@@ -1,8 +1,5 @@
-var assert = require('assert');
-var { URL_PREFIX, ADMIN_USER, TEST_USER } = require('../fixtures.js');
+const { URL_PREFIX, ADMIN_USER } = require('../fixtures.js');
 const webUI = require('../web-ui.js');
-
-const WAIT_DURATION = 10000;
 
 require('../acceptance-cmds.js'); // also checks that openwhyd's server is tested against the test database
 // TODO: make sure that DB was reset before starting Openwhyd's app server
