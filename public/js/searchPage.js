3c4cc851f0eef58696c712a2da4b5d8a59cf108c
compiuta <christianr@rocketmail.com>
2020-03-14 01:14:06 -0700

    fix(ui): never-ending loading animation on empty search (#260)
    Closes #226.
    
    ## What does this PR do / solve?
    
    The console javascript error appears because there is a results condition in the file app/templates/searchPage.html.
    
    This condition wraps the tab_tracks element that the file public/js/searchPage.js is looking for. I found 2 front end solutions to this specific issue.
    
    ## Overview of changes
    
    First we can wrap the searchPage.js file within the same condition as the html and give some sort of html message stating an invalid search query has been inputed.
    
    Or we can have a condition within the searchPage.js file checking for the element. If the element is not found then the page will redirect to the homepage. This is the technique I found being used by google and amazon when I added an empty query in the url. In this pull request I have used this second solution.
    
    ## How to test this PR?
    
    I used the javascript debugger and the console to add a javascript break before entering the condition. After a valid and  empty search there were no errors showing and the page redirected successfully when an empty search was given.

--- public/js/searchPage.js
+++ public/js/searchPage.js
@@ -1,99 +1,103 @@
-function selectTab() {
-  $('#tabSelector a.selected').removeClass('selected');
-  $(this).addClass('selected');
-  $('.resultPage').hide();
-  var current = $(this)
-    .attr('href')
-    .split('#')
-    .pop();
-  $('.resultPage#' + current).show();
+if(document.getElementById('q').value.replace(/ /g, '')) {
+  function selectTab() {
+    $('#tabSelector a.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('.resultPage').hide();
+    var current = $(this)
+      .attr('href')
+      .split('#')
+      .pop();
+    $('.resultPage#' + current).show();
+  }
+
+  $('#tabSelector a').click(selectTab);
+  if (window.location.href.indexOf('tab=') != -1) {
+    var tab = (window.location.href.split('tab=').pop() || '').split('&')[0];
+    if (tab) $('#tab_' + tab).each(selectTab);
+  }
+
+  // merges results from youtube and soundcloud with currently displayed results
+  $(window).ready(
+    function (query) {
+      var $body = $('body');
+      $body.addClass('loading');
+      function renderTrack(track) {
+        return (
+          '<div class="post external ' +
+          track.eId.substr(1, 2) +
+          '">' +
+          '<a class="thumb" href="' +
+          track.url +
+          '" target="_blank" data-eid="' +
+          track.eId +
+          '"' +
+          ' onclick="return playTrack(this);" style="background-image:url(\'' +
+          track.img +
+          '\');">' +
+          '<img src="' +
+          track.img +
+          '">' +
+          '<div class="play"></div>' +
+          '</a>' +
+          '<h2 ><a  onclick="window.goToPage(\'' +
+          track.eId.split('#')[0] +
+          '\');return false;"  target="_blank">' +
+          htmlEntities(track.name) +
+          '</a></h2>' +
+          '</a><p class="author">' +
+          '<span style="background-image:url(\'/images/icon-' +
+          track.playerLabel.toLowerCase() +
+          '.png\');"></span>' +
+          'Powered by ' +
+          track.playerLabel +
+          '</p>' +
+          '<div class="btns">' +
+          '<a class="btnRepost" href="javascript:publishPost({eId:\'' +
+          track.eId +
+          '\'});">Add to</a>' +
+          '</div>' +
+          '</div>'
+        );
+      }
+      function isAlreadyListed(track) {
+        return !!document.querySelector('a.thumb[data-eid="' + track.eId + '"]');
+      }
+      var tracks = [];
+      function displayDynamicSearchResults() {
+        var trackCounter = document.getElementById('tab_tracks');
+        var trackCount = parseInt(trackCounter.innerHTML);
+        document.getElementById('tracks').innerHTML += tracks
+          .map(renderTrack)
+          .join('\n');
+        trackCounter.innerHTML = trackCounter.innerHTML.replace(
+          '' + trackCount,
+          trackCount + tracks.length
+        );
+        $body.removeClass('loading');
+        if (trackCount + tracks.length === 0) {
+          $('#tracks .noResult').show();
+        }
+
+        if ($('#playlists.resultPage .playlist').length) {
+          $('#playlists.resultPage .noResult').hide();
+        } else {
+          $('#playlists.resultPage .noResult').show();
+        }
+
+        if ($('#users.resultPage .user').length) {
+          $('#users.resultPage .noResult').hide();
+        } else {
+          $('#users.resultPage .noResult').show();
+        }
+      }
+      window.searchExternalTracks(query, function (track) {
+        if (track && !isAlreadyListed(track)) {
+          track.name = track.title;
+          tracks.push(track);
+        } else displayDynamicSearchResults();
+      });
+    }.bind(window, document.getElementById('q').value)
+  );
+} else {
+  window.location.replace('/');
 }
-
-$('#tabSelector a').click(selectTab);
-if (window.location.href.indexOf('tab=') != -1) {
-  var tab = (window.location.href.split('tab=').pop() || '').split('&')[0];
-  if (tab) $('#tab_' + tab).each(selectTab);
-}
-
-// merges results from youtube and soundcloud with currently displayed results
-$(window).ready(
-  function(query) {
-    var $body = $('body');
-    $body.addClass('loading');
-    function renderTrack(track) {
-      return (
-        '<div class="post external ' +
-        track.eId.substr(1, 2) +
-        '">' +
-        '<a class="thumb" href="' +
-        track.url +
-        '" target="_blank" data-eid="' +
-        track.eId +
-        '"' +
-        ' onclick="return playTrack(this);" style="background-image:url(\'' +
-        track.img +
-        '\');">' +
-        '<img src="' +
-        track.img +
-        '">' +
-        '<div class="play"></div>' +
-        '</a>' +
-        '<h2 ><a  onclick="window.goToPage(\'' +
-        track.eId.split('#')[0] +
-        '\');return false;"  target="_blank">' +
-        htmlEntities(track.name) +
-        '</a></h2>' +
-        '</a><p class="author">' +
-        '<span style="background-image:url(\'/images/icon-' +
-        track.playerLabel.toLowerCase() +
-        '.png\');"></span>' +
-        'Powered by ' +
-        track.playerLabel +
-        '</p>' +
-        '<div class="btns">' +
-        '<a class="btnRepost" href="javascript:publishPost({eId:\'' +
-        track.eId +
-        '\'});">Add to</a>' +
-        '</div>' +
-        '</div>'
-      );
-    }
-    function isAlreadyListed(track) {
-      return !!document.querySelector('a.thumb[data-eid="' + track.eId + '"]');
-    }
-    var tracks = [];
-    function displayDynamicSearchResults() {
-      var trackCounter = document.getElementById('tab_tracks');
-      var trackCount = parseInt(trackCounter.innerHTML);
-      document.getElementById('tracks').innerHTML += tracks
-        .map(renderTrack)
-        .join('\n');
-      trackCounter.innerHTML = trackCounter.innerHTML.replace(
-        '' + trackCount,
-        trackCount + tracks.length
-      );
-      $body.removeClass('loading');
-      if (trackCount + tracks.length === 0) {
-        $('#tracks .noResult').show();
-      }
-
-      if ($('#playlists.resultPage .playlist').length) {
-        $('#playlists.resultPage .noResult').hide();
-      } else {
-        $('#playlists.resultPage .noResult').show();
-      }
-
-      if ($('#users.resultPage .user').length) {
-        $('#users.resultPage .noResult').hide();
-      } else {
-        $('#users.resultPage .noResult').show();
-      }
-    }
-    window.searchExternalTracks(query, function(track) {
-      if (track && !isAlreadyListed(track)) {
-        track.name = track.title;
-        tracks.push(track);
-      } else displayDynamicSearchResults();
-    });
-  }.bind(window, document.getElementById('q').value)
-);
