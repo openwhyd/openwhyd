1a1429c71163e1ebf278a2e51cf6498ea8bab1b9
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-21 17:34:02 +0100

    test(#295): add non-regression test for pagination (#299)
    In PR #296, we found out that a reformat of our front-end js files including mustache/hogan blocks caused runtime errors which prevented the "load more" button to work as expected.
    
    In order to prevent future regressions on pagination and runtime errors, this PR adds Cypress tests.
    
    Note: It's expected that the Cypress tests fails, until we merge #296.

---
+++ config/initdb_testing.js
@@ -0,0 +1,333 @@
+// Usage: this file should be run by runShellScript() or by mongo's CLI:
+// $ mongo openwhyd_data initdb_testing.js
+
+//db = db.getSiblingDB("openwhyd_data"); //connect("localhost:27017/whyd_music");
+
+print('upserting openwhyd testing users ...');
+
+db.user.update(
+  { _id: ObjectId('000000000000000000000001') },
+  {
+    $set: {
+      email: 'test@openwhyd.org',
+      handle: 'admin',
+      name: 'admin',
+      img:
+        '/images/blank_user.gif' /* needed for "has default avatar" e2e test */,
+      pwd: '21232f297a57a5a743894a0e4a801fc3' /* password = "admin" */
+    }
+  },
+  { upsert: true }
+);
+
+db.user.update(
+  { _id: ObjectId('000000000000000000000002') },
+  {
+    $set: {
+      email: 'dummy@openwhyd.org',
+      handle: 'dummy',
+      name: 'dummy',
+      img: '/images/blank_user.gif',
+      pwd: '21232f297a57a5a743894a0e4a801fc3' /* password = "admin" */
+    }
+  },
+  { upsert: true }
+);
+
+print('upserting openwhyd testing posts ...');
+
+db.post.update(
+  { _id: ObjectId(`0`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #0`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`1`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #1`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`2`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #2`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`3`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #3`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`4`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #4`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`5`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #5`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`6`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #6`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`7`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #7`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`8`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #8`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`9`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #9`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`10`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #10`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`11`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #11`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`12`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #12`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`13`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #13`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`14`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #14`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`15`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #15`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`16`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #16`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`17`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #17`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`18`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #18`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`19`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #19`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+db.post.update(
+  { _id: ObjectId(`20`.padStart(24, '0')) },
+  {
+    $set: {
+      name: `Fake track #20`,
+      eId: '/yt/Wch3gJG2GJ4' /* 1-second video, from YouTube */,
+      uId: '000000000000000000000002' /* user id of dummy user defined above */,
+      uNm: 'dummy',
+      img: '/images/cover-track.png'
+    }
+  },
+  { upsert: true }
+);
+
+print('done! :-)');
---
+++ cypress/integration/stream.spec.ts
@@ -0,0 +1,73 @@
+/// <reference types="Cypress" />
+
+context('Openwhyd stream', () => {
+  // TODO: can load next page of the global stream
+  it('can load next page of profile when user not logged in', () => {
+    cy.visit('/u/000000000000000000000002'); // will show profile page of user 'dummy' defined in initdb_testing.js
+
+    cy.get('.post h2')
+      .should('have.length', 20)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #1')
+      .should('not.include.text', 'Fake track #0');
+
+    cy.get('.btnLoadMore').click();
+
+    cy.get('.post h2')
+      .should('have.length', 21)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #0');
+
+    /*
+    // should list the main track of the page
+    cy.get('.whydThumb', { timeout: 10000 }).should(function($thumbs) {
+      cy.log('coucou', ($thumbs || []).length); // problem: always zero...
+      for (const i = 0; i < ($thumbs || []).length; i++) {
+        cy.log(i, $thumbs[i]);
+        const style = Cypress.$($thumbs[i]).attr('style');
+        expect(style).to.include(youtubeId);
+      }
+    });
+    */
+  });
+
+  it('can load next page of profile when user is logged in', () => {
+    cy.loginAsAdmin();
+
+    cy.visit('/u/000000000000000000000002'); // will show profile page of user 'dummy' defined in initdb_testing.js
+
+    cy.get('.post h2')
+      .should('have.length', 20)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #1')
+      .should('not.include.text', 'Fake track #0');
+
+    cy.get('.btnLoadMore').click();
+
+    cy.get('.post h2')
+      .should('have.length', 21)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #0');
+  });
+
+  it('can load next page of stream when user is logged in', () => {
+    cy.fixture('users.js').then(({ dummy }) => {
+      cy.login(dummy);
+    });
+
+    cy.visit('/'); // will show the home/stream of the user 'dummy' defined in initdb_testing.js
+
+    cy.get('.post h2')
+      .should('have.length', 20)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #1')
+      .should('not.include.text', 'Fake track #0');
+
+    cy.get('.btnLoadMore').click();
+
+    cy.get('.post h2')
+      .should('have.length', 21)
+      .should('include.text', 'Fake track #20')
+      .should('include.text', 'Fake track #0');
+  });
+});
