c94854d242f60a0eb92be06fc141f34af9ccd91a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-22 12:20:12 +0100

    fix type defs

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -8,13 +8,12 @@ declare namespace Cypress {
     /**
      * Login, given an email address and md5 password hash
      * @example
-     * cy.loginAsAdmin()
+     * cy.login({ email: admin.email, md5: admin.md5 })
      */
     login({ email, md5 }: { email: string; md5: string }): Chainable<any>;
+
     /**
      * Login as the admin user defined in initdb_testing.js
-     * @example
-     * cy.loginAsAdmin()
      */
     loginAsAdmin(): Chainable<any>;
   }
