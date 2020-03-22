6115b30406fd34dabf65bdbfa4438669f9ee2ee0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-22 16:27:12 +0100

    migrated second part of webdriver test suite

--- cypress/integration/acceptance.spec.ts
+++ cypress/integration/acceptance.spec.ts
@@ -4,6 +4,69 @@
 // as inspired by https://www.youtube.com/watch?v=aZT8VlTV1YY
 
 context('Openwhyd', () => {
+  it('should allow user to login', () => {
+    cy.visit('/');
+
+    cy.get('#signin') // https://on.cypress.io/interacting-with-elements
+      .click()
+      .get('.btnCreateAccount')
+      .should('be.visible');
+
+    cy.fixture('users.js').then(({ admin }) => {
+      cy.get('input[name=email]').type(admin.email); // https://on.cypress.io/type
+      cy.get('input[name=password]').type(admin.password);
+      cy.get('form').submit();
+      cy.get('#loginDiv .username').should('have.text', admin.name); // https://youtu.be/5XQOK0v_YRE?t=1430
+    });
+  });
+
+  it('should allow a user to add a track', function() {
+    // should allow user to login
+    cy.loginAsAdmin();
+
+    // should recognize a track when pasting a Youtube URL in the search box
+    cy.get('#q').type('https://www.youtube.com/watch?v=aZT8VlTV1YY');
+    cy.get('#searchResults').contains('Demo');
+
+    // should lead to a track page when clicking on the Youtube search result
+    cy.get('#searchResults li a')
+      .first()
+      .click();
+    cy.url().should('include', '/yt/aZT8VlTV1YY');
+
+    // should display the name of the track
+    cy.get('a.btnRepost[href*="Openwhyd Demo (formerly"]').should('exist');
+
+    // should open a dialog after clicking on the "Add to" button
+    cy.contains('Add to').click(); //$('a.btnRepost').click();
+    cy.get('.dlgPostBox').should('be.visible');
+
+    // should show a link to the post after adding the track
+    cy.wait(500); // TODO: we should not have to wait for the "Add" link to be clickable
+    cy.get('.dlgPostBox span')
+      .contains('Add')
+      .click();
+    cy.contains('your tracks'); // notification bar with link to "your tracks"
+
+    // should show the post on the user's profile after clicking the link
+    cy.get('a')
+      .contains('your tracks')
+      .click();
+    cy.url().should('include', '/u/');
+    cy.get('.post a[data-eid="/yt/aZT8VlTV1YY"]').should('be.visible');
+
+    // should open the playbar after the user clicks on the post
+    cy.get('.post a[data-eid="/yt/aZT8VlTV1YY"]').click();
+    cy.get('#btnPlay').should('be.visible');
+
+    // should play the track
+    cy.get('#btnPlay.playing').should('be.visible');
+
+    // should pause the track when the user clicks on the play/pause button
+    cy.get('#btnPlay').click();
+    cy.get('#btnPlay').should('not.have.class', 'playing');
+  });
+
   it('should allow a visitor to sign up and follow the onboarding process', function() {
     // should not let visitors access admin endpoints
     cy.visit('/admin/config/config.json');
@@ -56,21 +119,5 @@ context('Openwhyd', () => {
     });
 
     cy.logout();
-  });
-
-  it('should allow user to login', () => {
-    cy.visit('/');
-
-    cy.get('#signin') // https://on.cypress.io/interacting-with-elements
-      .click()
-      .get('.btnCreateAccount')
-      .should('be.visible');
-
-    cy.fixture('users.js').then(({ admin }) => {
-      cy.get('input[name=email]').type(admin.email); // https://on.cypress.io/type
-      cy.get('input[name=password]').type(admin.password);
-      cy.get('form').submit();
-      cy.get('#loginDiv .username').should('have.text', admin.name); // https://youtu.be/5XQOK0v_YRE?t=1430
-    });
   });
 });
