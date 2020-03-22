7055450eff2dabdb3ca0793b3390695081bb7f95
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-22 19:55:34 +0100

    remove webdriver implementation of acceptance test suite

--- test/specs/acceptance.tests.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var { URL_PREFIX, ADMIN_USER, TEST_USER } = require('../fixtures.js');
-const webUI = require('../web-ui.js');
-
-const WAIT_DURATION = 10000;
-
-require('../acceptance-cmds.js'); // also checks that openwhyd's server is tested against the test database
-
-// Webdriver API documentation: http://webdriver.io/api.html
