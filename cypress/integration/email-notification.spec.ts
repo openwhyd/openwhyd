77a7461b42f65ab37f1aaa245bb74fa8fc4f84a5
Harlequin <salaynadoukoure@gmail.com>
2020-08-25 20:35:05 +0200

    Fix Email Test: linted

--- cypress/integration/email-notification.spec.ts
+++ cypress/integration/email-notification.spec.ts
@@ -1,14 +1,14 @@
-context('reduce frequency of email notifications',() =>{
-  
-  it('user has instant email notifications',()=>{
+context('reduce frequency of email notifications', () => {
+  it('user has instant email notifications', () => {
     cy.loginAsAdmin();
-    cy.request('api/user')
-      .should((response)=>{
-        expect(response.body).to.have.property('pref').to.have.property('emSub',0);
-      });
+    cy.request('api/user').should(response => {
+      expect(response.body)
+        .to.have.property('pref')
+        .to.have.property('emSub', 0);
+    });
   });
-  
-  it('user activates daily email notifications',()=>{
+
+  it('user activates daily email notifications', () => {
     cy.loginAsAdmin();
     cy.visit('/settings');
     cy.get('a')
@@ -18,9 +18,10 @@ context('reduce frequency of email notifications',() =>{
       .contains('Daily')
       .click();
     cy.get('#tabNotif input[type="submit"]').click();
-    cy.request('api/user')
-      .should((response) => {
-        expect(response.body).to.have.property('pref').to.have.property('emSub','1');
-      });
-  })
+    cy.request('api/user').should(response => {
+      expect(response.body)
+        .to.have.property('pref')
+        .to.have.property('emSub', '1');
+    });
+  });
 });
