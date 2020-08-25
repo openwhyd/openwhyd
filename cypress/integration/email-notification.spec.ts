7a46788632ffa2ead5fb8256533b1090536c52a7
Harlequin <salaynadoukoure@gmail.com>
2020-08-26 00:16:56 +0200

    fix: Migrating email notifications tests to Cypress

--- cypress/integration/email-notification.spec.ts
+++ cypress/integration/email-notification.spec.ts
@@ -8,7 +8,7 @@ context('reduce frequency of email notifications', () => {
     });
   });
 
-  it('user activates daily email notifications', () => {
+  it('user activates daily email notifications then reduce it to weekly', () => {
     cy.loginAsAdmin();
     cy.visit('/settings');
     cy.get('a')
@@ -23,5 +23,32 @@ context('reduce frequency of email notifications', () => {
         .to.have.property('pref')
         .to.have.property('emSub', '1');
     });
+    cy.visit(`/api/unsubscribe?uId=000000000000000000000001&type=emSub&action=reduce`);
+    cy.get('body').contains('weekly');
+    cy.request('api/user').should(response => {
+      expect(response.body)
+        .to.have.property('pref')
+        .to.have.property('emSub', 7);
+    });
+  });
+  
+  it('user has instant eamail notification', () => {
+    cy.loginAsAdmin();
+    cy.request('/api/user').should((response) =>{
+      expect(response.body)
+        .to.have.property('pref')
+        .to.have.property('emSub', 0)
+    });
+  });
+  it('user unsubscribes from email notification', () => {
+    cy.loginAsAdmin();
+    cy.visit('/api/unsubscribe?uId=000000000000000000000001&type=emSub');
+    cy.get('body').contains('unsubscribed');
+    cy.request('api/user').should(response => {
+      expect(response.body)
+        .to.have.property('pref')
+        .to.have.property('emSub', -1);
+    });
+    cy.logout();
   });
 });
