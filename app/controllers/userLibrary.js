4ee25a42e0c406cd2876bb5c9d0e0f48bd270825
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-29 17:34:40 +0200

    fix eslint/no-this-alias

--- app/controllers/userLibrary.js
+++ app/controllers/userLibrary.js
@@ -63,28 +63,26 @@ LibraryController.prototype.renderPage = function (
   sidebarHtml,
   feedHtml
 ) {
-  var self = this;
   if (!this.options.embedW) {
     this.options.content = feedHtml;
-    var html = feedTemplate.renderFeedPage(user, this.options);
+    let html = feedTemplate.renderFeedPage(user, this.options);
     var loggedUserId = (this.options.loggedUser || {}).id;
     if (loggedUserId) {
-      userModel.fetchByUid(loggedUserId, function (user) {
+      userModel.fetchByUid(loggedUserId, (user) => {
         if (user && !user.consent) {
-          var thisUrl = encodeURIComponent(self.options.pageUrl || '/');
+          var thisUrl = encodeURIComponent(this.options.pageUrl || '/');
           html = loggingTemplate.htmlRedirect('/consent?redirect=' + thisUrl);
         }
-        self.render({ html: html });
+        this.render({ html });
       });
-    } else this.render({ html: html });
+    } else this.render({ html });
   } else {
-    var html = feedTemplate.renderFeedEmbed(feedHtml, this.options);
-    this.render({ html: html });
+    this.render({ html: feedTemplate.renderFeedEmbed(feedHtml, this.options) });
   }
 };
 
 LibraryController.prototype.renderJson = function (json) {
-  this.render({ json: json });
+  this.render({ json });
 };
 
 LibraryController.prototype.renderOther = function (data, mimeType) {
