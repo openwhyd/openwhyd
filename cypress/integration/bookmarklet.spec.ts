440dda6b92e5ad055cc1c6013d1792049d2b57c1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-30 19:22:56 +0200

    fix(lint): Fix ESLint & Codacy issues (#348)
    Follow up for PR #346.
    
    => no more syntax errors reported by `$(npm bin)/prettier . --write` 🎉
    
    Also, it:
    - finishes the migration of the `bookmarklet` test from Webdriver to Cypress.
    - updates argon2 from v0.25.1 to v0.27.0, in order to prevent fix the broken download of binaries from GitHub releases
    - fixes "Uncaught ReferenceError: Barelog is not defined" on CI

--- cypress/integration/bookmarklet.spec.ts
+++ cypress/integration/bookmarklet.spec.ts
@@ -1,50 +1,64 @@
 /// <reference types="Cypress" />
 
 context('Openwhyd bookmarklet', () => {
+  const VIDEO = {
+    id: '-F9vo4Z5lO4',
+    url: 'https://www.youtube.com/watch?v=-F9vo4Z5lO4',
+    name: 'YouTube Video 1',
+  };
+
+  const injectBookmarklet = (win) => {
+    win.document.body.appendChild(
+      win.document.createElement('script')
+    ).src = `${Cypress.config().baseUrl}/js/bookmarklet.js?${Date.now()}`;
+  };
+
   beforeEach('login', () => {
+    Cypress.on('uncaught:exception', () => {
+      // We prevent the following uncaught exceptions from failing the test:
+      // - YouTube failing to load "/cast/sdk/libs/sender/1.0/cast_framework.js"
+      // - Deezer failing to find window.jQuery
+      return false; // prevents Cypress from failing the test
+    });
+
     cy.fixture('users.js').then(({ dummy }) => {
       cy.login(dummy);
     });
   });
 
-  it('can pick a track from a youtube page', () => {
-    const youtubeId = '-F9vo4Z5lO4';
-    const youtubeName = 'YouTube Video 1';
-    const youtubeURL = `https://www.youtube.com/watch?v=${youtubeId}`;
-    const bkURL = `${Cypress.config().baseUrl}/js/bookmarklet.js?${Date.now()}`;
-
+  it('can be opened twice from the same youtube page', () => {
     cy.visit('http://localhost:8080/html/test-resources/youtube-videos.html');
 
-    cy.get(`[href="${youtubeURL}"]`).should('exist');
-
-    cy.window().then((win) => {
-      // win.document.body.innerHTML += `<script src="${bkURL}"></script>`; // does not work
-      // to test in your own browser: window.document.body.appendChild(window.document.createElement('script')).src = 'http://localhost:8080/js/bookmarklet.js';
-      win.document.body.appendChild(
-        win.document.createElement('script')
-      ).src = bkURL;
-    });
-
-    // check that bookmarklet is loaded
-    cy.window().should('have.property', '_initWhydBk');
+    cy.window().then(injectBookmarklet);
 
     // should list more than 1 track
-    cy.get('.whydThumb', { timeout: 10000 }).should('have.length.above', 1);
+    cy.get('.whydThumb').should('have.length.above', 1);
+
+    // close the bookmarklet
+    cy.get('#whydHeader div').click();
+
+    // there should be no thumbs displayed anymore
+    cy.get('.whydThumb').should('have.length', 0);
+
+    // re-inject the bookmarklet
+    cy.window().then(injectBookmarklet);
+
+    // should list more than 1 track
+    cy.get('.whydThumb').should('have.length.above', 1);
+  });
+
+  it('can pick a track from a youtube page', () => {
+    cy.visit('http://localhost:8080/html/test-resources/youtube-videos.html');
+
+    cy.window().then(injectBookmarklet);
+
+    // should list more than 1 track
+    cy.get('.whydThumb').should('have.length.above', 1);
 
     // should list the main track of the page
-
-    cy.get('.whydThumb', { timeout: 10000 }).should(function ($thumbs) {
-      cy.log('thumbs:', ($thumbs || []).length); // removing this log causes the test to occasionally hang...
-      for (let i = 0; i < ($thumbs || []).length; i++) {
-        expect($($thumbs[i]).html()).to.include(youtubeId);
-        expect($($thumbs[i]).find('p')).to.include(youtubeName);
-      }
-    });
-
-    // TODO
-    // cy.get('.whydThumb').should('contain.html', youtubeId); // causing "Uncaught ReferenceError: YOUTUBE_API_KEY is not defined"
-    // cy.get('.whydThumb').first().should('contain.text', youtubeName);
-    // cy.get('.whydThumb').first().click();
+    cy.get('#whydContent').should('contain.html', VIDEO.id);
+    cy.get('.whydThumb').first().should('contain.text', VIDEO.name);
+    cy.get('.whydThumb').first().click();
   });
 
   it('can display the "add track" dialog from /post', () => {
