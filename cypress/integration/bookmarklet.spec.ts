94fbb1747bf7b2c7c314c4d8eee520c3ee889443
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-30 09:40:30 +0200

    ❌ error when trying to click on a video found by the bookarklet: "Uncaught ReferenceError: YOUTUBE_API_KEY is not defined"

--- cypress/integration/bookmarklet.spec.ts
+++ cypress/integration/bookmarklet.spec.ts
@@ -9,6 +9,7 @@ context('Openwhyd bookmarklet', () => {
 
   it('can add a track from a youtube page', () => {
     const youtubeId = '-F9vo4Z5lO4';
+    const youtubeName = 'YouTube Video 1';
     const youtubeURL = `https://www.youtube.com/watch?v=${youtubeId}`;
     const bkURL = `${Cypress.config().baseUrl}/js/bookmarklet.js?${Date.now()}`;
 
@@ -27,18 +28,20 @@ context('Openwhyd bookmarklet', () => {
     cy.window().should('have.property', '_initWhydBk');
 
     // should list more than 1 track
-    cy.get('.whydThumb').should('have.length.above', 1);
+    cy.get('.whydThumb', { timeout: 10000 }).should('have.length.above', 1);
 
-    /*
     // should list the main track of the page
-    cy.get('.whydThumb', { timeout: 10000 }).should(function($thumbs) {
-      cy.log('coucou', ($thumbs || []).length); // problem: always zero...
-      for (const i = 0; i < ($thumbs || []).length; i++) {
-        cy.log(i, $thumbs[i]);
-        const style = Cypress.$($thumbs[i]).attr('style');
-        expect(style).to.include(youtubeId);
+
+    cy.get('.whydThumb', { timeout: 10000 }).should(function ($thumbs) {
+      cy.log('thumbs:', ($thumbs || []).length); // removing this log causes the test to occasionally hang...
+      for (let i = 0; i < ($thumbs || []).length; i++) {
+        expect($($thumbs[i]).html()).to.include(youtubeId);
+        expect($($thumbs[i]).find('p')).to.include(youtubeName);
       }
     });
-    */
+
+    cy.get('.whydThumb').should('contain.html', youtubeId); // causing "Uncaught ReferenceError: YOUTUBE_API_KEY is not defined"
+    cy.get('.whydThumb').first().should('contain.text', youtubeName);
+    cy.get('.whydThumb').first().click();
   });
 });
