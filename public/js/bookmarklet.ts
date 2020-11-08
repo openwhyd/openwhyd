a92785fa7caf1ff58948ab3eaf0cb83d2cf539dd
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-08 18:25:07 +0100

    add scripts/transpile-bookmarklet.sh

--- public/js/bookmarklet.ts
+++ public/js/bookmarklet.ts
@@ -1,13 +1,9 @@
-/**
- * To transpile TypeScript files into bookmarklet.js, run:
- * $ npx tsc --outFile public/js/bookmarklet.js public/js/bookmarklet*.ts && \
- *   npx prettier public/js/bookmarklet.js --write
- *
- * Then, run tests:
- * $ npx mocha test/unit/bookmarklet-tests.js && \
- *   . ./.env-docker && npx cypress run --spec cypress/integration/bookmarklet.spec.ts
- */
-
+// To transpile TypeScript files into bookmarklet.js, run:
+// $ scripts/transpile-bookmarklet.sh
+//
+// Then, run tests:
+// $ npx mocha test/unit/bookmarklet-tests.js && \
+//   . ./.env-docker && npx cypress run --spec cypress/integration/bookmarklet.spec.ts
 import './bookmarkletLogic';
 import './bookmarkletYouTube';
 import './bookmarkletUI';
