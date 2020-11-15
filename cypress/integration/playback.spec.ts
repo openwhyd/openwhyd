00b3ff1fa13ba22c392447a01ce15986c45555f8
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-15 17:47:36 +0100

    ❌ new test: allow user to play a bandcamp track

---
+++ cypress/integration/playback.spec.ts
@@ -0,0 +1,18 @@
+// Tests to prevent regressions on playback of tracks.
+
+context('Playback', () => {
+  it('should allow user to play a Bandcamp track', () => {
+    cy.visit('/bc/harissa/rooftop');
+
+    // should open the playbar after the user clicks on the post
+    cy.get(`.post a.thumb`).click();
+    cy.get('#btnPlay').should('be.visible');
+
+    // should play the track
+    cy.get('#btnPlay.playing', { timeout: 10000 }).should('be.visible');
+
+    // should pause the track when the user clicks on the play/pause button
+    cy.get('#btnPlay').click();
+    cy.get('#btnPlay').should('not.have.class', 'playing');
+  });
+});
