a169acb98ed655d9b3f04efc550f2c301be01926
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-15 17:25:22 +0100

    add bandcamp extractor api

--- app/controllers/api/bandcampExtractor.js
+++ app/controllers/api/bandcampExtractor.js
@@ -1,6 +1,11 @@
-const REGEX = /https:\/\/[^.]+\.bcbits\.com\/stream\/[^;"]*/g;
+const request = require('request');
+var config = require('../../models/config.js');
 
-exports.extractBandcampStreamURLs = (plainText) => plainText.match(REGEX);
+const RE_EID = /^\/bc\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)/;
+const RE_STREAM_URL = /https:\/\/[^.]+\.bcbits\.com\/stream\/[^;"]*/g;
+
+exports.extractBandcampStreamURLs = (plainText) =>
+  plainText.match(RE_STREAM_URL);
 
 exports.extractBandcampStreamURLsFromHTML = (html) => {
   const withDecodedEntities = htmlDecode(html);
@@ -15,4 +20,43 @@ function htmlDecode(str) {
     .replace(/&quot;/g, '"');
 }
 
-// TODO: add controller + make sure it's only callable from openwhyd.org
+
+exports.controller = async function (req, reqParams = {}, res) {
+  req.logToConsole('bandcampExtractor.controller', reqParams);
+
+  // make sure it's only callable from openwhyd.org
+  const ref = req.getReferer();
+  if (typeof ref !== 'string' || !ref.includes(config.urlPrefix)) {
+    res.badRequest();
+    return;
+  }
+
+  const eId = reqParams.eId;
+  const matched = typeof eId === 'string' ? eId.match(RE_EID) : null;
+
+  if (!matched) {
+    res.badRequest();
+    return;
+  }
+
+  try {
+    const track = matched.pop();
+    const artist = matched.pop();
+    const { body } = await fetch(
+      `https://${artist}.bandcamp.com/track/${track}`
+    );
+    res.json({
+      eId,
+      streamURL: exports.extractBandcampStreamURLsFromHTML(body)[0],
+    });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+
+const fetch = (url) =>
+  new Promise((resolve, reject) =>
+    request(url, (error, response, body) =>
+      error ? reject(error) : resolve({ response, body })
+    )
+  );
