1128e93c86c95897cb37eb162e6f9b76c8f34660
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-29 14:22:45 +0100

    first visual tests (working locally)

--- cypress/integration/visual-snapshots.ts
+++ cypress/integration/visual-snapshots.ts
@@ -7,6 +7,7 @@ context('Visual Snapshots', () => {
       testName: 'Visual Snapshots',
       browser: { width: 1000, height: 660 }, // cf https://docs.cypress.io/api/commands/viewport.html#Defaults
     });
+    // TODO: insert a few tracks, to also check regressions on the rendering of those tracks
   });
 
   after(() => {
@@ -14,8 +15,22 @@ context('Visual Snapshots', () => {
   });
 
   it('visitor', () => {
-    // should have Openwhyd in its title
-    cy.visit('/');
+    cy.visit('/'); // Home page (full stream)
     cy.eyesCheckWindow('visitor on /');
+
+    cy.contains('Got it!').click(); // Remove cookie banner
+    cy.eyesCheckWindow('visitor on / (discarded cookie banner)');
+
+    cy.contains('Hot Tracks').click();
+    // cy.get('#pageLoader').should('have.css', { opacity: 0.5 });
+    cy.location('pathname').should('equal', '/hot');
+    // cy.get('#pageLoader').should('have.css', { opacity: 0 });
+    cy.contains('/ All'); // in the header of the list of tracks
+    cy.eyesCheckWindow('visitor on /hot');
+
+    cy.contains('Login').click();
+    cy.location('pathname').should('equal', '/login');
+    cy.contains('No account yet?'); // below the sign in form
+    cy.eyesCheckWindow('visitor on /login');
   });
 });
