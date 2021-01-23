400cdd4518a61728a888e73fc9ac20968c02bc67
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-23 14:46:34 +0100

    try to fix "Error: connect ECONNREFUSED 127.0.0.1:80" by adding serverUrl to applitools config file

--- applitools.config.js
+++ applitools.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   batchId: process.env.APPLITOOLS_BATCH_ID,
+  serverUrl: 'eyesapi.applitools.com',
+  serverURL: 'eyesapi.applitools.com',
 };
+
+console.log('applitools', module.exports);
