63228adcd48d3f01e933b1aa3d9709941e8dfae2
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-23 16:32:34 +0100

    cp node_modules/@applitools/eyes-cypress/eyes-index.d.ts ./cypress/support/
    (see "manual installation" instructions from https://www.npmjs.com/package/@applitools/eyes-cypress)

--- cypress/support/eyes-index.d.ts
+++ cypress/support/eyes-index.d.ts
@@ -1,1 +1,34 @@
-import '@applitools/eyes-cypress';
+/// <reference types="Cypress" />
+/// <reference types="@applitools/visual-grid-client" />
+
+declare namespace Cypress {
+    interface Chainable {
+      /**
+       * Create an Applitools test.
+       * This will start a session with the Applitools server.
+       * @example
+       * cy.eyesOpen({ appName: 'My App' })
+      */
+      eyesOpen(options?: Eyes.Open.Options): null // add isDisabled
+
+      /**
+       * Generate a screenshot of the current page and add it to the Applitools Test.
+       * @example
+       * cy.eyesCheckWindow()
+       *
+       * OR
+       *
+       * cy.eyesCheckWindow({
+       *  target: 'region',
+       *  selector: '.my-element'
+       * });
+      */
+      eyesCheckWindow(config?: Eyes.Check.Options|String): null
+
+      /**
+       * Close the applitools test and check that all screenshots are valid.
+       * @example cy.eyesClose()
+      */
+      eyesClose(): null
+    }
+  }
