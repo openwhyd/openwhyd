aaeba4939d052ed0981266bbf564e349bd399496
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-23 14:56:07 +0100

    applitools config is loaded but visual test suite still fails when trying to reach port 80 https://github.com/openwhyd/openwhyd/pull/432/checks?check_run_id=1754092143#step:8:97 => try to remove applitools server url

--- applitools.config.js
+++ applitools.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   batchId: process.env.APPLITOOLS_BATCH_ID,
-  serverUrl: 'eyesapi.applitools.com',
-  serverURL: 'eyesapi.applitools.com',
 };
 
 console.log('applitools', module.exports);
