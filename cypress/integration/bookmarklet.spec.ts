openwhyd/openwhyd

commit a4fe26b9792081c8d9175155605abd228adc54dc

fix(deps): Update eslint and prettier (#429)

public/js/postBoxV2.js
@@ -1,5 +1,7 @@
 /* global $ */
 
+const globals = window;
+
 /**
  * post box for openwhyd music
  * @author adrienjoly, whyd
@@ -25,7 +27,7 @@ function htmlDecode(str) {
 
 //================ WHYD POST CLASS (to submit posts)
 
-function WhydPost(embedRef) {
+function WhydPost(/*embedRef*/) {
   var that = this;
   this.postData = {
     action: 'insert',
@@ -85,7 +87,7 @@ function WhydPlaylistSelector($selPlaylist, defaultPlaylist, onSelect) {
   var $arrow = $head.find('span');
   var $playlistMenu = $selPlaylist.find('.content');
   var $form = $selPlaylist.find('form');
-  function hideMenu(e) {
+  function hideMenu(/*e*/) {
     // only hide if e is null, or if a playlist was clicked (avoid playlist menu head and form)
     // if (!e || !($head[0] == e.target || $head[0] == e.target.parentNode
     // 		|| $form[0] == e.target.parentNode)){
@@ -183,13 +185,13 @@ function WhydTextWithMentions(textArea) {
 
 //================ javascript code moved from postEdit.html
 
-function initPostBox(params) {
+globals.initPostBox = function (params) {
   var addingFromBookmarklet = params.mode === 'addFromBookmarklet';
   var editingPost = params.mode === 'editPost';
   var reposting = params.mode === 'repost';
 
   function initWhydPost(embedRef) {
-    whydPost = new WhydPost();
+    const whydPost = new WhydPost();
     whydPost.setEmbedRef(embedRef);
     // select default playlist (to override playlist of original author, when reposted)
     whydPost.setPlaylist('null', 'full stream');
@@ -206,12 +208,12 @@ function initPostBox(params) {
     try {
       var submitted = whydPost.postData,
         stored = whydPost.storedPost;
-      window.Whyd.tracking.log(
+      globals.Whyd.tracking.log(
         'Added track' + (addingFromBookmarklet ? ' using bookmarklet' : ''),
         stored._id
       );
       if ((submitted.pl || {}).id == 'create')
-        window.Whyd.tracking.log('Created playlist', stored.pl.id);
+        globals.Whyd.tracking.log('Created playlist', stored.pl.id);
     } catch (e) {
       console.warn('error', e, e.stack);
     }
@@ -220,7 +222,7 @@ function initPostBox(params) {
   var $body = $('body');
 
   function close() {
-    if (window.avgrundClose) avgrundClose();
+    if (globals.avgrundClose) globals.avgrundClose();
     else window.close();
     // TODO: also close bookmarklet overlay
   }
@@ -261,9 +263,9 @@ function initPostBox(params) {
 
   // for openwhyd ui (add/edit/repost)
   function closeAndShowTrack(posted) {
-    avgrundClose();
+    globals.avgrundClose();
     if (!posted)
-      return (window.showMessage || alert)(
+      return (globals.showMessage || alert)(
         'Oops; an error occurred... Please try again!',
         true
       );
@@ -271,13 +273,13 @@ function initPostBox(params) {
     if (posted.pl) {
       url += '/playlist/' + posted.pl.id;
     }
-    if (window.showMessage) {
+    if (globals.showMessage) {
       var plName = posted.pl ? posted.pl.name : 'your tracks';
-      showMessage(
+      globals.showMessage(
         "Successfully added track to <a target='_blank' href='" +
           url +
           "'>" +
-          encodeHtmlEntities(plName) +
+          globals.encodeHtmlEntities(plName) +
           '</a>'
       );
     } else if (url) window.location.href = url;
@@ -318,7 +320,7 @@ function initPostBox(params) {
     var descBox = new WhydTextWithMentions(document.getElementById('text'));
     $('#btnSubmit')
       .unbind()
-      .click(function (e) {
+      .click(function (/*e*/) {
         descBox.getTextWithMentions(function (text) {
           // sample text: "pouet @[adrien](user:4ecb6ec933d9308badd7b7b4) test"
           console.log('WhydTextWithMentions RESULT:', text);
@@ -334,17 +336,17 @@ function initPostBox(params) {
     }, 200);
   }
 
-  function makePlayemStreamDetector(eidSet) {
+  function makePlayemStreamDetector(/*eidSet*/) {
     var players = {
       // playem-all.js must be loaded at that point
-      yt: new YoutubePlayer({}),
-      sc: new SoundCloudPlayer({}),
-      vi: new VimeoPlayer({}),
-      dm: new DailymotionPlayer({}),
-      dz: new DeezerPlayer({}),
-      bc: new BandcampPlayer({}),
-      ja: new JamendoPlayer({}),
-      fi: new AudioFilePlayer({}),
+      yt: new globals.YoutubePlayer({}),
+      sc: new globals.SoundCloudPlayer({}),
+      vi: new globals.VimeoPlayer({}),
+      dm: new globals.DailymotionPlayer({}),
+      dz: new globals.DeezerPlayer({}),
+      bc: new globals.BandcampPlayer({}),
+      ja: new globals.JamendoPlayer({}),
+      fi: new globals.AudioFilePlayer({}),
       // TODO: make sure that the list of players is always up to date
     };
     function getPlayerId(url) {
@@ -407,26 +409,27 @@ function initPostBox(params) {
     $body.removeClass('failed');
     // todo: make sure that playemjs is loaded and ready to use
     // todo: in editPost case: populate track object and display using post metadata, instead of fetching
-    makePlayemStreamDetector()(params.embed.replace(/\&amp\;/g, '&'), function (
-      track
-    ) {
-      console.log('postBox detected track:', track);
-      if ((track || {}).eId) {
-        track.src = params.src;
-        track.name = $('#contentTitle').text(); // || $("#contentTitleInput").val();
-        if (!track.name || track.name == 'undefined')
-          // weak equality necessary because of text()
-          track.name = track.title;
-      } else track = {};
-      populateTrackUi(track);
-    });
+    makePlayemStreamDetector()(
+      params.embed.replace(/&amp;/g, '&'),
+      function (track) {
+        console.log('postBox detected track:', track);
+        if ((track || {}).eId) {
+          track.src = params.src;
+          track.name = $('#contentTitle').text(); // || $("#contentTitleInput").val();
+          if (!track.name || track.name == 'undefined')
+            // weak equality necessary because of text()
+            track.name = track.title;
+        } else track = {};
+        populateTrackUi(track);
+      }
+    );
     /*
 		$("#lnkDeletePost").unbind().click(function() {
-			avgrundClose();
+			globals.avgrundClose();
 			window.setTimeout(function(){
 				var html = '<div><div style="background-image:url(\'' + params.img + '\');"></div>'
 					+ '<span>' + params.title + '</span></div>' // TODO: sanitize title?
-			  		+ '<span class="btnDelete greenButton" onclick="avgrundClose();removePost(\'' + params.pId + '\');">Delete</span>';
+			  		+ '<span class="btnDelete greenButton" onclick="window.avgrundClose();removePost(\'' + params.pId + '\');">Delete</span>';
 				openJqueryDialog($(html), "dlgDeletePost", "Delete this post");
 			}, 600);
 		});
@@ -439,9 +442,9 @@ function initPostBox(params) {
 
   $(document).ajaxComplete(function () {
     try {
-      FB.XFBML.parse();
+      globals.FB.XFBML.parse();
     } catch (ex) {
       console.error(ex);
     }
   });
-}
+};

.eslintignore
@@ -1,6 +1,7 @@
 public/js/*-min.js
 public/js/*.min.js
 public/js/*jquery*.js
+public/js/dropzone.js
 public/js/md5.js
 public/js/mustache.js
 public/js/playem-all.js

.prettierignore
@@ -4,6 +4,7 @@ CHANGELOG.md
 tipsy.css
 jquery*.js
 playem-all.js
+public/js/dropzone.js
 public/js/soundmanager2.js
 public/press/js/sliderz.v1.js
 docs/_site/*.html

app/controllers/LibUser.js
@@ -201,16 +201,17 @@ function renderFriends(friends) {
 }
 
 function populateUsers(subscr, options, cb) {
-  followModel.fetchSubscriptionArray(options.loggedUser.id, function (
-    mySubscr
-  ) {
-    var subscrSet = snip.arrayToSet(mySubscr);
-    for (let i in subscr)
-      if (subscrSet[subscr[i].id]) subscr[i].subscribed = true;
-    userModel.fetchUserBios(subscr, function () {
-      cb(renderFriends(subscr));
-    });
-  });
+  followModel.fetchSubscriptionArray(
+    options.loggedUser.id,
+    function (mySubscr) {
+      var subscrSet = snip.arrayToSet(mySubscr);
+      for (let i in subscr)
+        if (subscrSet[subscr[i].id]) subscr[i].subscribed = true;
+      userModel.fetchUserBios(subscr, function () {
+        cb(renderFriends(subscr));
+      });
+    }
+  );
 }
 
 function fetchAndRenderPlaylist(options, callback, process) {
@@ -292,45 +293,46 @@ function fetchAndRenderProfile(options, callback, process) {
     options.tabTitle = 'Activity';
     options.bodyClass += ' userActivity';
     options.pageTitle = options.user.name + "'s recent activity";
-    followModel.fetchUserSubscriptions(options.loggedUser.id, function (
-      mySubscr
-    ) {
-      //console.log("mySubscr.subscriptions", mySubscr.subscriptions);
-      var mySubscrUidList = snip.objArrayToValueArray(
-        mySubscr.subscriptions,
-        'id'
-      );
-      activityController.generateActivityFeed(
-        [options.user.id],
-        mySubscrUidList,
-        options,
-        function (result) {
-          for (let i in result.recentActivity.items)
-            if (result.recentActivity.items[i].subscriptions) {
-              result.recentActivity.items[i].subscribedUsers =
-                result.recentActivity.items[i].subscriptions;
-              delete result.recentActivity.items[i].subscriptions;
+    followModel.fetchUserSubscriptions(
+      options.loggedUser.id,
+      function (mySubscr) {
+        //console.log("mySubscr.subscriptions", mySubscr.subscriptions);
+        var mySubscrUidList = snip.objArrayToValueArray(
+          mySubscr.subscriptions,
+          'id'
+        );
+        activityController.generateActivityFeed(
+          [options.user.id],
+          mySubscrUidList,
+          options,
+          function (result) {
+            for (let i in result.recentActivity.items)
+              if (result.recentActivity.items[i].subscriptions) {
+                result.recentActivity.items[i].subscribedUsers =
+                  result.recentActivity.items[i].subscriptions;
+                delete result.recentActivity.items[i].subscriptions;
+              }
+            //console.log("ACTIVITY result", result.recentActivity);
+            options.showActivity = result.recentActivity;
+            if (result.hasMore)
+              options.hasMore = { lastPid: result.hasMore.last_id };
+            else {
+              var creation = mongodb.ObjectId(options.user.id);
+              options.showActivity.items.push({
+                _id: creation,
+                other: { text: 'joined whyd' },
+                //						ago: uiSnippets.renderTimestamp(new Date() - creation.getTimestamp())
+              });
             }
-          //console.log("ACTIVITY result", result.recentActivity);
-          options.showActivity = result.recentActivity;
-          if (result.hasMore)
-            options.hasMore = { lastPid: result.hasMore.last_id };
-          else {
-            var creation = mongodb.ObjectId(options.user.id);
-            options.showActivity.items.push({
-              _id: creation,
-              other: { text: 'joined whyd' },
-              //						ago: uiSnippets.renderTimestamp(new Date() - creation.getTimestamp())
-            });
+            for (let i in options.showActivity.items)
+              options.showActivity.items[i].ago = uiSnippets.renderTimestamp(
+                new Date() - options.showActivity.items[i]._id.getTimestamp()
+              );
+            process([]);
           }
-          for (let i in options.showActivity.items)
-            options.showActivity.items[i].ago = uiSnippets.renderTimestamp(
-              new Date() - options.showActivity.items[i]._id.getTimestamp()
-            );
-          process([]);
-        }
-      );
-    });
+        );
+      }
+    );
   } else if (options.showSubscribers) {
     options.tabTitle = 'Followers';
 

app/controllers/admin/analytics.js
@@ -187,15 +187,19 @@ exports.controller = function (request, reqParams, response) {
 
   var fetchPendingInvites = function (callback) {
     console.log('fetching invite collection...');
-    mongodb.collections['invite'].find({}, { limit: 999999 }, function (
-      err,
-      cursor
-    ) {
-      console.log('done fetching invite collection.');
-      cursor.toArray(function (err, items) {
-        callback('Number of pending invites', (pendingInvites = items).length);
-      });
-    });
+    mongodb.collections['invite'].find(
+      {},
+      { limit: 999999 },
+      function (err, cursor) {
+        console.log('done fetching invite collection.');
+        cursor.toArray(function (err, items) {
+          callback(
+            'Number of pending invites',
+            (pendingInvites = items).length
+          );
+        });
+      }
+    );
   };
 
   function countRecentInvites(cb) {

app/controllers/admin/recom.js
@@ -259,25 +259,27 @@ var fileGenerators = {
       return [snip.normalizeArtistName(band)];
     });
     console.log('bands', bands);
-    recomModel.matchingEngine.fetchUsersByArtists(bands, null, function (
-      users
-    ) {
-      const processedUsers = snip.mapToObjArray(users).map(function (user) {
-        var artists = countOccurences(user.posted || []);
-        user.score = artists.length;
-        user.artists = artists
-          .map(function (artist) {
-            return artist.id + ' (' + artist.c + ')';
-          })
-          .join(', ');
-        delete user.posted;
-        return user;
-      });
-      processedUsers.sort(function (a, b) {
-        return b.score - a.score;
-      });
-      cb(processedUsers);
-    });
+    recomModel.matchingEngine.fetchUsersByArtists(
+      bands,
+      null,
+      function (users) {
+        const processedUsers = snip.mapToObjArray(users).map(function (user) {
+          var artists = countOccurences(user.posted || []);
+          user.score = artists.length;
+          user.artists = artists
+            .map(function (artist) {
+              return artist.id + ' (' + artist.c + ')';
+            })
+            .join(', ');
+          delete user.posted;
+          return user;
+        });
+        processedUsers.sort(function (a, b) {
+          return b.score - a.score;
+        });
+        cb(processedUsers);
+      }
+    );
   },
   'bandsToUsers.tsv': function (p, cb) {
     fileGenerators['bandsToUsers.json'](p, function (users) {

app/controllers/admin/tests/follow.js
@@ -11,31 +11,33 @@ exports.makeTests = function (p) {
       'fetchUserSubscriptions',
       function fetchSubscriptions(cb) {
         //console.time("fetchUserSubscriptions");
-        followModel.fetchUserSubscriptions(p.loggedUser.id, function (
-          subscriptions
-        ) {
-          testVars.uidList = [p.loggedUser.id];
-          for (let i in subscriptions.subscriptions)
-            if (subscriptions.subscriptions[i].id)
-              testVars.uidList.push(
-                ('' + subscriptions.subscriptions[i].id).replace('/u/', '')
-              );
-          //console.timeEnd("fetchUserSubscriptions");
-          cb(true);
-        });
+        followModel.fetchUserSubscriptions(
+          p.loggedUser.id,
+          function (subscriptions) {
+            testVars.uidList = [p.loggedUser.id];
+            for (let i in subscriptions.subscriptions)
+              if (subscriptions.subscriptions[i].id)
+                testVars.uidList.push(
+                  ('' + subscriptions.subscriptions[i].id).replace('/u/', '')
+                );
+            //console.timeEnd("fetchUserSubscriptions");
+            cb(true);
+          }
+        );
       },
     ],
     [
       'fetchSubscriptionArray == fetchUserSubscriptions',
       function fetchSubscriptions(cb) {
         //console.time("fetchSubscriptionArray");
-        followModel.fetchSubscriptionArray(p.loggedUser.id, function (
-          subscriptions
-        ) {
-          subscriptions.push(p.loggedUser.id);
-          //console.timeEnd("fetchSubscriptionArray");
-          cb(subscriptions.sort().join() === testVars.uidList.sort().join());
-        });
+        followModel.fetchSubscriptionArray(
+          p.loggedUser.id,
+          function (subscriptions) {
+            subscriptions.push(p.loggedUser.id);
+            //console.timeEnd("fetchSubscriptionArray");
+            cb(subscriptions.sort().join() === testVars.uidList.sort().join());
+          }
+        );
       },
     ],
   ];

app/controllers/admin/tests/stream.js
@@ -21,13 +21,14 @@ exports.makeTests = function (p) {
       'fetchSubscriptionArray',
       function fetchSubscriptions(cb) {
         //console.time("fetchSubscriptionArray");
-        followModel.fetchSubscriptionArray(p.loggedUser.id, function (
-          subscriptions
-        ) {
-          testVars.uidList = subscriptions.concat([p.loggedUser.id]);
-          //console.timeEnd("fetchSubscriptionArray");
-          cb(true);
-        });
+        followModel.fetchSubscriptionArray(
+          p.loggedUser.id,
+          function (subscriptions) {
+            testVars.uidList = subscriptions.concat([p.loggedUser.id]);
+            //console.timeEnd("fetchSubscriptionArray");
+            cb(true);
+          }
+        );
       },
     ],
     [

commit e5e148d2925ba57960ad3845b21c9442fa3ebfa2

fix(bookmarklet): "Identifier 'globals' has already been declared" when adding a track (#439)
Contributes to #439, at least for YouTube tracks.

The bug is probably caused by https://github.com/openwhyd/openwhyd/pull/429/files#diff-d0670720fd54d514c34d0215269319d1cc83b4fcca7113085edd1040a262febcR3.

public/js/postBoxV2.js
@@ -1,6 +1,6 @@
 /* global $ */
 
-const globals = window;
+window.globals = window;
 
 /**
  * post box for openwhyd music

cypress/integration/bookmarklet.spec.ts
@@ -12,11 +12,20 @@ context('Openwhyd bookmarklet', () => {
   };
 
   beforeEach('login', () => {
-    Cypress.on('uncaught:exception', () => {
-      // We prevent the following uncaught exceptions from failing the test:
-      // - YouTube failing to load "/cast/sdk/libs/sender/1.0/cast_framework.js"
-      // - Deezer failing to find window.jQuery
-      return false; // prevents Cypress from failing the test
+    // We prevent the following uncaught exceptions from failing the test:
+    const skippedErrors = [
+      'cast_framework.js', // YouTube failing to load "/cast/sdk/libs/sender/1.0/cast_framework.js"
+      'window.jQuery', // Deezer failing to find window.jQuery
+      `Cannot read property 'style' of null`, // TODO: fix this error
+      'YOUTUBE_API_KEY is not defined', // TODO: fix this error
+      'The request cannot be completed because you have exceeded your <a href="/youtube/v3/getting-started#quota">quota', // Note: this error was witnessed in CI
+    ];
+    Cypress.on('uncaught:exception', (err) => {
+      if (skippedErrors.some((errMsg) => err.message.includes(errMsg))) {
+        return false; // prevents Cypress from failing the test
+      } else {
+        return true;
+      }
     });
 
     cy.fixture('users.js').then(({ dummy }) => {