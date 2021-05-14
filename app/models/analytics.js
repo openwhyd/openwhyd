25ff2399730d425aa561e30509afd40be8485c53
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-05-14 12:32:17 +0200

    try to fix "permission denied, open './playlog.json.log'" cf https://github.com/openwhyd/openwhyd/pull/455/checks?check_run_id=2583057189#step:7:27

--- app/models/analytics.js
+++ app/models/analytics.js
@@ -8,13 +8,15 @@ var fs = require('fs');
 var snip = require('../snip.js');
 
 var playlogStream = fs.createWriteStream('./playlog.json.log', {
-  flags: 'a',
+  flags: 'a', // append
+  mode: 0o640, // file permissions: user can read+write, group can read
   encoding: 'utf8',
   autoClose: true,
 });
 
 var visitStream = fs.createWriteStream('./visits.json.log', {
-  flags: 'a',
+  flags: 'a', // append
+  mode: 0o640, // file permissions: user can read+write, group can read
   encoding: 'utf8',
   autoClose: true,
 });
