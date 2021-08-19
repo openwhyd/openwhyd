0917b091adb18eda55b9da551741568c802cf7a0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-08-19 17:52:58 +0200

    fix: include test data in test instead of importing tracks from production , to prevent tests from breaking after tracks are added on adrien's profile

--- test/approval.tests.js
+++ test/approval.tests.js
@@ -1,12 +1,27 @@
+// Before running these tests, make sure that:
+// - Openwhyd is running on port 8080 (`$ docker-compose up --build`)
+// - Its database is empty but initialized
+
 const test = require('ava');
 const { promisify } = require('util');
 const openwhyd = require('./api-client');
+const {
+  readMongoDocuments,
+  insertTestData,
+  refreshOpenwhydCache,
+} = require('./db-helpers');
+
+const MONGODB_URL =
+  process.env.MONGODB_URL || 'mongodb://localhost:27117/openwhyd_test';
 
 const getPage = promisify(openwhyd.getRaw);
 
-// Before running these tests, make sure that:
-// - Openwhyd is running on port 8080 (`$ docker-compose up --build`)
-// - Adrien's profile was imported locally (`$ node scripts/import-from-prod.js adrien`)
+test.before(async () => {
+  const users = await readMongoDocuments(__dirname + '/approval.users.json');
+  const posts = await readMongoDocuments(__dirname + '/approval.posts.json');
+  await insertTestData(MONGODB_URL, users, posts);
+  await refreshOpenwhydCache();
+});
 
 test('Visitor, Home, page 1, HTML', async (t) => {
   const { body } = await getPage(null, '/');
@@ -21,7 +36,7 @@ test('Visitor, Profile, page 1, HTML', async (t) => {
 test('Visitor, Profile, page 2, HTML', async (t) => {
   const { body } = await getPage(
     null,
-    '/adrien?after=600ec1c703e2014e630c8137' // TODO: include test data in test instead of importing tracks from production , to prevent tests from breaking after tracks are added on adrien's profile
+    '/adrien?after=600ec1c703e2014e630c8137'
   );
   t.snapshot(body);
 });
