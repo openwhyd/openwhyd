8c965d99bd82e0b0c2adff066e4866cae5845f2e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-08-19 14:49:10 +0200

    document test

--- test/approval.tests.js
+++ test/approval.tests.js
@@ -4,7 +4,11 @@ const openwhyd = require('./api-client');
 
 const getPage = promisify(openwhyd.getRaw);
 
-test('HelloWorld component', async (t) => {
+// Before running these tests, make sure that:
+// - Openwhyd is running on port 8080 (`$ docker-compose up --build`)
+// - Adrien's profile was imported locally (`$ node scripts/import-from-prod.js adrien`)
+
+test('Visitor, Home, page 1, HTML', async (t) => {
   const { body } = await getPage(null, '/');
   t.snapshot(body);
 });
