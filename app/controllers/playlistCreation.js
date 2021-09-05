39d8840af8b79f00a051b14770869991a314974e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-09-05 15:33:18 +0200

    fix: support /u/<uid> routes + error mgt and redirection

--- app/controllers/playlistCreation.js
+++ app/controllers/playlistCreation.js
@@ -2,6 +2,7 @@ const config = require('../models/config.js');
 const userModel = require('../models/user.js');
 const feedTemplate = require('../templates/feed.js');
 const templateLoader = require('../templates/templateLoader.js');
+const errorTemplate = require('../templates/error.js');
 
 exports.controller = async function (request, reqParams, response) {
   request.logToConsole('playlistCreation.controller', reqParams);
@@ -11,23 +12,47 @@ exports.controller = async function (request, reqParams, response) {
     return;
   }
 
-  const user = await new Promise((resolve) =>
-    userModel.fetchByHandle(reqParams.handle, resolve)
-  );
+  let user;
+
+  if (reqParams.handle) {
+    user = await new Promise((resolve) =>
+      userModel.fetchByHandle(reqParams.handle, resolve)
+    );
+  } else if (reqParams.id) {
+    user = await new Promise((resolve) =>
+      userModel.fetchByUid(reqParams.id, resolve)
+    );
+    if (user.handle) {
+      const path = request.url
+        .split('?')[0]
+        .replace('/u/' + reqParams.id, '/' + user.handle);
+      return response.temporaryRedirect(path, {});
+    }
+  }
+
+  if (!user)
+    return errorTemplate.renderErrorResponse(
+      { errorCode: 'USER_NOT_FOUND' },
+      response,
+      reqParams.format,
+      request.getUser()
+    );
 
   const options = {
     bodyClass: ' userPlaylistV2',
     customFeedTemplate: templateLoader.loadTemplate(
       'app/templates/userPlaylistV2.html'
     ),
-    pageUrl: request.url.replace('/adrien', `/u/${user.id}`),
+    pageUrl: user.handle
+      ? request.url.replace(`/${user.handle}`, `/u/${user.id}`)
+      : request.url,
     pageTitle: 'new playlist',
     pageImage: `${config.urlPrefix}/img/playlist/${user.id}_create`,
     loggedUser: { ...request.getUser() },
     user,
     playlist: {
       id: 'create',
-      name: `Playlist #${user.pl.length}`,
+      name: `Playlist #${(user.pl || []).length}`,
     },
   };
 
