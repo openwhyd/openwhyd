bf94c8ddbe24db11c691e357be5cabb56fcfdc78
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-09-05 14:46:22 +0200

    all "create playlist" tests pass !

--- app/controllers/playlistCreation.js
+++ app/controllers/playlistCreation.js
@@ -1,6 +1,7 @@
 const config = require('../models/config.js');
 const userModel = require('../models/user.js');
 const feedTemplate = require('../templates/feed.js');
+const templateLoader = require('../templates/templateLoader.js');
 
 exports.controller = async function (request, reqParams, response) {
   request.logToConsole('playlistCreation.controller', reqParams);
@@ -15,17 +16,36 @@ exports.controller = async function (request, reqParams, response) {
   );
 
   const options = {
+    bodyClass: ' userPlaylistV2',
+    customFeedTemplate: templateLoader.loadTemplate(
+      // TODO: rely on express to render template
+      'app/templates/userPlaylistV2.html'
+    ),
     pageUrl: request.url.replace('/adrien', '/u/4d94501d1f78ac091dbc9b4d'), // TODO: get rid of the replace()
     pageTitle: 'new playlist',
     pageImage: config.urlPrefix + '/img/playlist/' + user.id + '_create',
     loggedUser: request.getUser() || {},
+    user,
     playlist: {
       id: 'create',
       name: /*(reqParams || {}).name ||*/ 'Playlist #' + user.pl.length,
     },
   };
 
-  const html = feedTemplate.renderFeedEmbed('', options);
+  const trackingHtml = [
+    '<script>',
+    ' window.Whyd.tracking.log("Visit profile", "' + user.id + '");',
+    '</script>',
+  ].join('\n');
 
-  response.send(html);
+  const feedHtml = await new Promise((resolve) =>
+    feedTemplate.renderFeedAsync([], options, resolve)
+  );
+
+  const pageHtml = feedTemplate.renderFeedPage(user, {
+    ...options,
+    content: trackingHtml + feedHtml,
+  });
+
+  response.send(pageHtml);
 };
