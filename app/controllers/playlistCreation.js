f7712f2cc212ff9daf94eb71b7a4f307896337ba
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-09-29 10:26:13 +0200

    naively include call to response rendering logic, from userLibrary.js

--- app/controllers/playlistCreation.js
+++ app/controllers/playlistCreation.js
@@ -1,7 +1,23 @@
-const userLibrary = require('./userLibrary');
+// const userLibrary = require('./userLibrary');
+
+var mongodb = require('../models/mongodb.js');
+var userModel = require('../models/user.js');
 
 exports.controller = function (request, reqParams, response) {
-  return userLibrary.controller(request, reqParams, response);
+  // return userLibrary.controller(request, reqParams, response);
+
+  if (reqParams.id) {
+    if (!mongodb.isObjectId(reqParams.id))
+      return render({ errorCode: 'USER_NOT_FOUND' });
+    userModel.fetchByUid(reqParams.id, function (user) {
+      if (!user) render({ errorCode: 'USER_NOT_FOUND' });
+      else if (user.handle && !reqParams.embedW)
+        redirectTo(path.replace('/u/' + reqParams.id, '/' + user.handle));
+      else renderUserLibrary(lib, user);
+    });
+  } else {
+    response.badRequest();
+  }
 };
 
 // To run tests:
