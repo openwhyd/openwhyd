01644c8e1a6bb26f27c931c94668c02b2710808b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-09-29 10:22:48 +0200

    fix: call the legacy controller from the new controller

--- app/controllers/playlistCreation.js
+++ app/controllers/playlistCreation.js
@@ -1,9 +1,8 @@
-const userLibraryController = require('./userLibrary');
+const userLibrary = require('./userLibrary');
 
 exports.controller = function (request, reqParams, response) {
-  response.send('');
-  // TODO: return userLibraryController(request, reqParams, response);
+  return userLibrary.controller(request, reqParams, response);
 };
 
 // To run tests:
-// docker-compose up --build --detach web && sleep 5 && WHYD_GENUINE_SIGNUP_SECRET="whatever" npx ava test/approval.tests.js
+// docker-compose stop web && docker-compose up --build --detach web && sleep 5 && WHYD_GENUINE_SIGNUP_SECRET="whatever" npx ava test/approval.tests.js
