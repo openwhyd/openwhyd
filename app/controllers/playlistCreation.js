8ed6ddf7977bcee3d5c4007cb17c63892ad2d893
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-09-29 10:26:47 +0200

    copy the render() function from userLibrary.js

--- app/controllers/playlistCreation.js
+++ app/controllers/playlistCreation.js
@@ -6,6 +6,45 @@ var userModel = require('../models/user.js');
 exports.controller = function (request, reqParams, response) {
   // return userLibrary.controller(request, reqParams, response);
 
+  function render(data, mimeType) {
+    if (mimeType)
+      return response.legacyRender(data, null, { 'content-type': mimeType });
+    data = data || {
+      error:
+        'Nothing to render! Please send the URL of this page to ' +
+        process.appParams.feedbackEmail,
+    };
+    if (data.errorCode) {
+      //response.renderHTML(errorTemplate.renderErrorCode(data.errorCode));
+      errorTemplate.renderErrorResponse(
+        data,
+        response,
+        reqParams.format,
+        loggedInUser
+      );
+    } else if (data.error) {
+      console.error('userLibrary ERROR: ', data.error);
+      //response.renderHTML(errorTemplate.renderErrorMessage(data.error));
+      errorTemplate.renderErrorResponse(
+        data,
+        response,
+        reqParams.format,
+        loggedInUser
+      );
+    } else if (data.html) {
+      response.renderHTML(data.html);
+      // console.log('rendering done!');
+      if (
+        loggedInUser &&
+        loggedInUser.id &&
+        !reqParams.after &&
+        !reqParams.before
+      )
+        analytics.addVisit(loggedInUser, request.url /*"/u/"+uid*/);
+    } else if (typeof data == 'object') response.renderJSON(data.json || data);
+    else response.legacyRender(data.error);
+  }
+
   if (reqParams.id) {
     if (!mongodb.isObjectId(reqParams.id))
       return render({ errorCode: 'USER_NOT_FOUND' });
