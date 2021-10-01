462b295f59ed878a831025e74d302781de256a2a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-01 11:54:08 +0200

    extract prepare* functions from fetchAndRenderProfile()

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -231,165 +231,189 @@ function fetchAndRenderProfile(options, callback) {
   options.bodyClass += ' userProfileV2';
   options.nbPlaylists = (options.user.pl || []).length;
   if (options.showPlaylists) {
-    const playlists = options.user.pl;
-    options.pageTitle = 'Playlists by ' + options.user.name;
-    options.tabTitle = 'Playlists';
-    options.bodyClass += ' userPlaylists';
-    options.playlists = [...playlists].reverse(); // clone before reversing
-    options.showPlaylists = { items: renderPlaylists(options) };
-    callback(null, []); // no posts
+    preparePlaylistsPageRendering(options, callback); // no posts
   } else if (options.showLikes) {
-    options.tabTitle = 'Likes';
-    options.bodyClass += ' userLikes';
-    options.pageTitle = options.user.name + "'s liked tracks";
-    postModel.fetchPosts(
-      { lov: options.uid },
-      /*params*/ null,
-      { after: options.after },
-      (tracks) => callback(null, tracks)
-    );
+    prepareLikesPageRendering(options, callback);
   } else if (options.showActivity) {
-    options.tabTitle = 'Activity';
-    options.bodyClass += ' userActivity';
-    options.pageTitle = options.user.name + "'s recent activity";
-    followModel.fetchUserSubscriptions(
-      options.loggedUser.id,
-      function (mySubscr) {
-        var mySubscrUidList = snip.objArrayToValueArray(
-          mySubscr.subscriptions,
-          'id'
-        );
-        activityController.generateActivityFeed(
-          [options.user.id],
-          mySubscrUidList,
-          options,
-          function (result) {
-            for (let i in result.recentActivity.items)
-              if (result.recentActivity.items[i].subscriptions) {
-                result.recentActivity.items[i].subscribedUsers =
-                  result.recentActivity.items[i].subscriptions;
-                delete result.recentActivity.items[i].subscriptions;
-              }
-            options.showActivity = result.recentActivity;
-            if (result.hasMore)
-              options.hasMore = { lastPid: result.hasMore.last_id };
-            else {
-              var creation = mongodb.ObjectId(options.user.id);
-              options.showActivity.items.push({
-                _id: creation,
-                other: { text: 'joined whyd' },
-              });
-            }
-            for (let i in options.showActivity.items)
-              options.showActivity.items[i].ago = uiSnippets.renderTimestamp(
-                new Date() - options.showActivity.items[i]._id.getTimestamp()
-              );
-            callback(null, []);
-          }
-        );
-      }
-    );
+    prepareActivityPageRendering(options, callback);
   } else if (options.showSubscribers) {
-    options.tabTitle = 'Followers';
-
-    options.bodyClass += ' userSubscribers';
-    options.pageTitle = options.user.name + "'s followers";
-    var params = {
-      sort: { _id: -1 },
-      limit: MAX_SUBSCRIPTIONS + 1,
-      fields: { _id: 0, uId: 1 },
-      skip: Number(options.after || 0),
-    };
-    followModel.fetch({ tId: options.user.id }, params, function (subscr) {
-      if (subscr.length > MAX_SUBSCRIPTIONS) {
-        options.hasMore = {
-          lastPid: params.skip + MAX_SUBSCRIPTIONS,
-        };
-        subscr = subscr.slice(0, MAX_SUBSCRIPTIONS);
-      }
-      for (let i in subscr) subscr[i] = { id: subscr[i].uId };
-      populateUsers(subscr, options, function (subscr) {
-        options.showSubscribers = {
-          items: subscr,
-        };
-        callback(null, []);
-      });
-    });
+    prepareSubscribersPageRendering(options, callback);
   } else if (options.showSubscriptions) {
-    options.tabTitle = 'Following';
-
-    options.bodyClass += ' userSubscriptions';
-    options.pageTitle = options.user.name + "'s following";
-    const params = {
-      sort: { _id: -1 },
-      limit: MAX_SUBSCRIPTIONS + 1,
-      fields: { _id: 0, tId: 1 },
-      skip: Number(options.after || 0),
-    };
-    followModel.fetch({ uId: options.user.id }, params, function (subscr) {
-      if (subscr.length > MAX_SUBSCRIPTIONS) {
-        options.hasMore = {
-          lastPid: params.skip + MAX_SUBSCRIPTIONS,
-        };
-        subscr = subscr.slice(0, MAX_SUBSCRIPTIONS);
-      }
-      for (let i in subscr) subscr[i] = { id: subscr[i].tId };
-      populateUsers(subscr, options, function (subscr) {
-        options.showSubscriptions = {
-          items: subscr,
-        };
-        callback(null, []);
-      });
-    });
+    prepareSubscriptionsPageRendering(options, callback);
   } else {
-    options.tabTitle = 'Tracks';
-
-    options.bodyClass += ' userTracks';
-    options.showTracks = true;
-    options.pageTitle = options.user.name + "'s tracks";
-    const proceed = () =>
-      postModel.fetchByAuthors([options.uid], options.fetchParams, (tracks) =>
-        callback(null, tracks)
-      );
-
-    if (options.after || options.before)
-      // no page rendering required
-      proceed();
-    else {
-      // SIDEBAR
-      fetchActivity(options, function () {
-        // => populates options.activity
-        var ownProfile = options.user.id == (options.loggedUser || {}).id;
-        // render playlists
-        if ((options.user.pl || []).length || ownProfile)
-          options.playlists = {
-            url: '/u/' + options.user.id + '/playlists',
-            items: renderPlaylists(options, MAX_PLAYLISTS_SIDE),
-          };
-        // fetch and render friends
-        var params = {
-          sort: { _id: -1 },
-          limit: MAX_FRIENDS,
-          fields: { _id: 0, tId: 1 },
-        };
-        followModel.fetch({ uId: options.user.id }, params, function (subscr) {
-          if (subscr.length || ownProfile) {
-            for (let i in subscr) subscr[i] = { id: subscr[i].tId };
-            userModel.fetchUserBios(subscr, function () {
-              options.friends = {
-                url: '/u/' + options.user.id + '/subscriptions',
-                items: renderFriends(subscr),
-              };
-              proceed();
-            });
-          } else proceed();
-        });
-      });
-    }
+    prepareUserTracksPageRendering(options, callback);
   }
 }
 
 var bareFormats = new Set(['json', 'links']);
+
+function prepareUserTracksPageRendering(options, callback) {
+  options.tabTitle = 'Tracks';
+
+  options.bodyClass += ' userTracks';
+  options.showTracks = true;
+  options.pageTitle = options.user.name + "'s tracks";
+  const proceed = () =>
+    postModel.fetchByAuthors([options.uid], options.fetchParams, (tracks) =>
+      callback(null, tracks)
+    );
+
+  if (options.after || options.before)
+    // no page rendering required
+    proceed();
+  else {
+    // SIDEBAR
+    fetchActivity(options, function () {
+      // => populates options.activity
+      var ownProfile = options.user.id == (options.loggedUser || {}).id;
+      // render playlists
+      if ((options.user.pl || []).length || ownProfile)
+        options.playlists = {
+          url: '/u/' + options.user.id + '/playlists',
+          items: renderPlaylists(options, MAX_PLAYLISTS_SIDE),
+        };
+      // fetch and render friends
+      var params = {
+        sort: { _id: -1 },
+        limit: MAX_FRIENDS,
+        fields: { _id: 0, tId: 1 },
+      };
+      followModel.fetch({ uId: options.user.id }, params, function (subscr) {
+        if (subscr.length || ownProfile) {
+          for (let i in subscr) subscr[i] = { id: subscr[i].tId };
+          userModel.fetchUserBios(subscr, function () {
+            options.friends = {
+              url: '/u/' + options.user.id + '/subscriptions',
+              items: renderFriends(subscr),
+            };
+            proceed();
+          });
+        } else proceed();
+      });
+    });
+  }
+}
+
+function prepareSubscriptionsPageRendering(options, callback) {
+  options.tabTitle = 'Following';
+
+  options.bodyClass += ' userSubscriptions';
+  options.pageTitle = options.user.name + "'s following";
+  const params = {
+    sort: { _id: -1 },
+    limit: MAX_SUBSCRIPTIONS + 1,
+    fields: { _id: 0, tId: 1 },
+    skip: Number(options.after || 0),
+  };
+  followModel.fetch({ uId: options.user.id }, params, function (subscr) {
+    if (subscr.length > MAX_SUBSCRIPTIONS) {
+      options.hasMore = {
+        lastPid: params.skip + MAX_SUBSCRIPTIONS,
+      };
+      subscr = subscr.slice(0, MAX_SUBSCRIPTIONS);
+    }
+    for (let i in subscr) subscr[i] = { id: subscr[i].tId };
+    populateUsers(subscr, options, function (subscr) {
+      options.showSubscriptions = {
+        items: subscr,
+      };
+      callback(null, []);
+    });
+  });
+}
+
+function prepareSubscribersPageRendering(options, callback) {
+  options.tabTitle = 'Followers';
+
+  options.bodyClass += ' userSubscribers';
+  options.pageTitle = options.user.name + "'s followers";
+  var params = {
+    sort: { _id: -1 },
+    limit: MAX_SUBSCRIPTIONS + 1,
+    fields: { _id: 0, uId: 1 },
+    skip: Number(options.after || 0),
+  };
+  followModel.fetch({ tId: options.user.id }, params, function (subscr) {
+    if (subscr.length > MAX_SUBSCRIPTIONS) {
+      options.hasMore = {
+        lastPid: params.skip + MAX_SUBSCRIPTIONS,
+      };
+      subscr = subscr.slice(0, MAX_SUBSCRIPTIONS);
+    }
+    for (let i in subscr) subscr[i] = { id: subscr[i].uId };
+    populateUsers(subscr, options, function (subscr) {
+      options.showSubscribers = {
+        items: subscr,
+      };
+      callback(null, []);
+    });
+  });
+}
+
+function prepareActivityPageRendering(options, callback) {
+  options.tabTitle = 'Activity';
+  options.bodyClass += ' userActivity';
+  options.pageTitle = options.user.name + "'s recent activity";
+  followModel.fetchUserSubscriptions(
+    options.loggedUser.id,
+    function (mySubscr) {
+      var mySubscrUidList = snip.objArrayToValueArray(
+        mySubscr.subscriptions,
+        'id'
+      );
+      activityController.generateActivityFeed(
+        [options.user.id],
+        mySubscrUidList,
+        options,
+        function (result) {
+          for (let i in result.recentActivity.items)
+            if (result.recentActivity.items[i].subscriptions) {
+              result.recentActivity.items[i].subscribedUsers =
+                result.recentActivity.items[i].subscriptions;
+              delete result.recentActivity.items[i].subscriptions;
+            }
+          options.showActivity = result.recentActivity;
+          if (result.hasMore)
+            options.hasMore = { lastPid: result.hasMore.last_id };
+          else {
+            var creation = mongodb.ObjectId(options.user.id);
+            options.showActivity.items.push({
+              _id: creation,
+              other: { text: 'joined whyd' },
+            });
+          }
+          for (let i in options.showActivity.items)
+            options.showActivity.items[i].ago = uiSnippets.renderTimestamp(
+              new Date() - options.showActivity.items[i]._id.getTimestamp()
+            );
+          callback(null, []);
+        }
+      );
+    }
+  );
+}
+
+function prepareLikesPageRendering(options, callback) {
+  options.tabTitle = 'Likes';
+  options.bodyClass += ' userLikes';
+  options.pageTitle = options.user.name + "'s liked tracks";
+  postModel.fetchPosts(
+    { lov: options.uid },
+    /*params*/ null,
+    { after: options.after },
+    (tracks) => callback(null, tracks)
+  );
+}
+
+function preparePlaylistsPageRendering(options, callback) {
+  const playlists = options.user.pl;
+  options.pageTitle = 'Playlists by ' + options.user.name;
+  options.tabTitle = 'Playlists';
+  options.bodyClass += ' userPlaylists';
+  options.playlists = [...playlists].reverse(); // clone before reversing
+  options.showPlaylists = { items: renderPlaylists(options) };
+  callback(null, []);
+}
 
 function fetchAndRender(options, callback) {
   options.bodyClass = '';
