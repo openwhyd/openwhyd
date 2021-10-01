d6d396ad48e22c69fbb6104397823a2478b9a2e7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-01 15:41:27 +0200

    start moving data fetching functions to LibUserData.js

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -10,12 +10,20 @@ var mongodb = require('../models/mongodb.js');
 var userModel = require('../models/user.js');
 var followModel = require('../models/follow.js');
 var postModel = require('../models/post.js');
-var activityModel = require('../models/activity.js');
 var activityController = require('../controllers/recentActivity.js');
 var feedTemplate = require('../templates/feed.js');
 var uiSnippets = require('../templates/uiSnippets.js');
 
 var templateLoader = require('../templates/templateLoader.js');
+const {
+  fetchActivity,
+  fetchPlaylists,
+  countSubscribers,
+  countSubscriptions,
+  fetchIsSubscribed,
+  fetchLikes,
+  fetchNbTracks,
+} = require('./LibUserData');
 var profileTemplateV2 = templateLoader.loadTemplate(
   'app/templates/userProfileV2.html'
 );
@@ -25,95 +33,7 @@ var playlistTemplateV2 = templateLoader.loadTemplate(
 
 var MAX_PLAYLISTS_SIDE = 4;
 var MAX_FRIENDS = 6;
-var MAX_HISTORY = 3;
 var MAX_SUBSCRIPTIONS = 50;
-
-// DATA FETCHING HELPERS
-
-function fetchPlaylists(options) {
-  return new Promise((resolve) =>
-    userModel.fetchPlaylists(options.user, {}, resolve)
-  );
-}
-
-function fetchLikes(options) {
-  return new Promise((resolve) =>
-    postModel.countLovedPosts(options.user.id, resolve)
-  );
-}
-
-function countSubscriptions(options) {
-  return new Promise((resolve) =>
-    followModel.countSubscriptions(options.user.id, resolve)
-  );
-}
-
-function countSubscribers(options) {
-  return new Promise((resolve) =>
-    followModel.countSubscribers(options.user.id, resolve)
-  );
-}
-
-const fetchIsSubscribed = (options) =>
-  new Promise((resolve) =>
-    followModel.get(
-      { uId: options.loggedUser.id, tId: options.user.id },
-      (err, res) => resolve(!!res)
-    )
-  );
-
-function fetchNbTracks(options) {
-  return new Promise((resolve) =>
-    postModel.countUserPosts(options.user.id, resolve)
-  );
-}
-
-// used to render the sidebar
-async function fetchActivity(options) {
-  const activities = await new Promise((resolve) =>
-    activityModel.fetchHistoryFromUidList(
-      [options.user.id],
-      { limit: MAX_HISTORY },
-      resolve
-    )
-  );
-  if (activities.length < MAX_HISTORY)
-    activities.push({
-      _id: mongodb.ObjectId(options.user.id),
-      other: { text: 'joined whyd' },
-    });
-  var postsToPopulate = [];
-  for (let i in activities)
-    if (activities[i]) {
-      activities[i].ago = uiSnippets.renderTimestamp(
-        new Date() - activities[i]._id.getTimestamp()
-      );
-      if (activities[i].like)
-        postsToPopulate.push(mongodb.ObjectId('' + activities[i].like.pId));
-    }
-
-  const posts = await new Promise((resolve) =>
-    postModel.fetchPosts(
-      { _id: { $in: postsToPopulate } },
-      /*params*/ null,
-      /*options*/ null,
-      resolve
-    )
-  );
-
-  var postSet = {};
-  const activity = [];
-  for (let i in posts) postSet['' + posts[i]._id] = posts[i];
-  for (let i in activities) {
-    if ((activities[i] || {}).like) {
-      if (postSet['' + activities[i].like.pId])
-        activities[i].like.post = postSet['' + activities[i].like.pId];
-      else continue; // do not keep null posts
-    }
-    activity.push(activities[i]);
-  }
-  return activity;
-}
 
 // PAGE RENDERING
 
