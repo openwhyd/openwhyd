d8ae09b4ae13587e5ae0bfdba5b6455014e78352
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-01 19:09:08 +0200

    move global functions to PlaylistPageGenerator class

--- app/controllers/PlaylistPageGenerator.js
+++ app/controllers/PlaylistPageGenerator.js
@@ -7,78 +7,80 @@ var playlistTemplateV2 = templateLoader.loadTemplate(
   'app/templates/userPlaylistV2.html'
 );
 
-function preparePlaylistPageRendering(options, callback) {
-  // 1. populate page template parameters
-  populatePlaylistPageTemplateParameters(options);
-
-  // 2. fetch and render list of tracks
-  if (!options.playlist) callback('meh... this playlist does not exist!');
-  else {
-    populateNextAndPrevPlaylistPageUrl(options);
-    postModel.fetchPlaylistPosts(
-      options.uid,
-      options.playlistId,
-      options.fetchParams,
-      (tracks) => callback(null, tracks)
-    );
-  }
-}
-
-function populateNextAndPrevPlaylistPageUrl(options) {
-  var prevId = null;
-  for (let p = options.user.pl.length - 1; p > -1; --p) {
-    var pl = options.user.pl[p];
-    if (!pl) continue;
-    if (pl.id == options.playlistId) {
-      if (prevId !== null)
-        options.prevPageInList = '/u/' + options.uid + '/playlist/' + prevId;
-      for (--p; p > -1; --p) {
-        if (options.user.pl[p]) {
-          options.nextPageInList =
-            '/u/' + options.uid + '/playlist/' + options.user.pl[p].id;
-          break;
-        }
-      }
-      break;
-    }
-    prevId = pl.id;
-  }
-}
-function populatePlaylistPageTemplateParameters(options) {
-  options.bodyClass += ' userPlaylistV2';
-  options.user.pl = options.user.pl || [];
-  for (let i in options.user.pl)
-    if (options.user.pl[i] && options.user.pl[i].id == options.playlistId) {
-      options.playlist = options.user.pl[i];
-    }
-  if (options.playlistId == 'create') {
-    options.playlist = {
-      id: 'create',
-      name: 'Playlist #' + options.user.pl.length,
-    };
-    options.pageTitle = 'new playlist';
-  } else {
-    options.pageTitle =
-      ((options.playlist || {}).name || 'a playlist') +
-      ' by ' +
-      options.user.name;
-  }
-  if (!options.format && !options.embedW && options.playlist) {
-    options.pageImage =
-      config.urlPrefix +
-      '/img/playlist/' +
-      options.user.id +
-      '_' +
-      options.playlistId;
-  }
-}
-
 class PlaylistPageGenerator extends PageGenerator {
   constructor(user, options) {
     super(user, options);
   }
+
+  populateNextAndPrevPlaylistPageUrl(options) {
+    var prevId = null;
+    for (let p = options.user.pl.length - 1; p > -1; --p) {
+      var pl = options.user.pl[p];
+      if (!pl) continue;
+      if (pl.id == options.playlistId) {
+        if (prevId !== null)
+          options.prevPageInList = '/u/' + options.uid + '/playlist/' + prevId;
+        for (--p; p > -1; --p) {
+          if (options.user.pl[p]) {
+            options.nextPageInList =
+              '/u/' + options.uid + '/playlist/' + options.user.pl[p].id;
+            break;
+          }
+        }
+        break;
+      }
+      prevId = pl.id;
+    }
+  }
+
+  populatePlaylistPageTemplateParameters(options) {
+    options.bodyClass += ' userPlaylistV2';
+    options.user.pl = options.user.pl || [];
+    for (let i in options.user.pl)
+      if (options.user.pl[i] && options.user.pl[i].id == options.playlistId) {
+        options.playlist = options.user.pl[i];
+      }
+    if (options.playlistId == 'create') {
+      options.playlist = {
+        id: 'create',
+        name: 'Playlist #' + options.user.pl.length,
+      };
+      options.pageTitle = 'new playlist';
+    } else {
+      options.pageTitle =
+        ((options.playlist || {}).name || 'a playlist') +
+        ' by ' +
+        options.user.name;
+    }
+    if (!options.format && !options.embedW && options.playlist) {
+      options.pageImage =
+        config.urlPrefix +
+        '/img/playlist/' +
+        options.user.id +
+        '_' +
+        options.playlistId;
+    }
+  }
+
+  preparePlaylistPageRendering(options, callback) {
+    // 1. populate page template parameters
+    this.populatePlaylistPageTemplateParameters(options);
+
+    // 2. fetch and render list of tracks
+    if (!options.playlist) callback('meh... this playlist does not exist!');
+    else {
+      this.populateNextAndPrevPlaylistPageUrl(options);
+      postModel.fetchPlaylistPosts(
+        options.uid,
+        options.playlistId,
+        options.fetchParams,
+        (tracks) => callback(null, tracks)
+      );
+    }
+  }
+
   prepareTemplateData = () =>
-    util.promisify(preparePlaylistPageRendering)(this.options);
+    util.promisify(this.preparePlaylistPageRendering)(this.options);
   getCustomFeedTemplate = () => playlistTemplateV2;
 }
 
