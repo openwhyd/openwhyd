ce038428fad36d9b0d4395228f8d3692e77690e1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-01 17:40:21 +0200

    move preparePlaylistPageRendering() to dedicated file

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -19,6 +19,7 @@ const {
   fetchSubscriptions,
   populateFriendsData,
 } = require('./LibUserData');
+const { preparePlaylistPageRendering } = require('./LibUserPlaylist');
 
 var feedTemplate = require('../templates/feed.js');
 var templateLoader = require('../templates/templateLoader.js');
@@ -71,73 +72,6 @@ function renderFriends(friends) {
     friends[i].img = '/img/u/' + friends[i].id;
   }
   return friends;
-}
-
-function preparePlaylistPageRendering(options, callback) {
-  // 1. populate page template parameters
-  populatePlaylistPageTemplateParameters(options);
-
-  // 2. fetch and render list of tracks
-  if (!options.playlist) callback('meh... this playlist does not exist!');
-  else {
-    populateNextAndPrevPlaylistPageUrl(options);
-    postModel.fetchPlaylistPosts(
-      options.uid,
-      options.playlistId,
-      options.fetchParams,
-      (tracks) => callback(null, tracks)
-    );
-  }
-}
-
-function populateNextAndPrevPlaylistPageUrl(options) {
-  var prevId = null;
-  for (let p = options.user.pl.length - 1; p > -1; --p) {
-    var pl = options.user.pl[p];
-    if (!pl) continue;
-    if (pl.id == options.playlistId) {
-      if (prevId !== null)
-        options.prevPageInList = '/u/' + options.uid + '/playlist/' + prevId;
-      for (--p; p > -1; --p) {
-        if (options.user.pl[p]) {
-          options.nextPageInList =
-            '/u/' + options.uid + '/playlist/' + options.user.pl[p].id;
-          break;
-        }
-      }
-      break;
-    }
-    prevId = pl.id;
-  }
-}
-
-function populatePlaylistPageTemplateParameters(options) {
-  options.bodyClass += ' userPlaylistV2';
-  options.user.pl = options.user.pl || [];
-  for (let i in options.user.pl)
-    if (options.user.pl[i] && options.user.pl[i].id == options.playlistId) {
-      options.playlist = options.user.pl[i];
-    }
-  if (options.playlistId == 'create') {
-    options.playlist = {
-      id: 'create',
-      name: 'Playlist #' + options.user.pl.length,
-    };
-    options.pageTitle = 'new playlist';
-  } else {
-    options.pageTitle =
-      ((options.playlist || {}).name || 'a playlist') +
-      ' by ' +
-      options.user.name;
-  }
-  if (!options.format && !options.embedW && options.playlist) {
-    options.pageImage =
-      config.urlPrefix +
-      '/img/playlist/' +
-      options.user.id +
-      '_' +
-      options.playlistId;
-  }
 }
 
 function prepareOtherPageRendering(options, callback) {
