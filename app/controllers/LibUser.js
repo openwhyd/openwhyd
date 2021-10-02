84cd91ad403eeb308f78441ef98e87dcf719ebb8
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-02 14:32:24 +0200

    hypothèse: forcer l'appel aux autres fonctions fetch*()

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -580,14 +580,14 @@ function renderUserLibrary(lib, user) {
   var fcts = [fetchAndRender, renderResponse];
 
   // prepend required fetching operations in head of the call chain
-  if (!options.after && !options.before)
-    // main tab: tracks (full layout to render, with sidebar)
-    fcts = [
-      fetchPlaylists,
-      /*fetchSubscriptions,*/ fetchStats,
-      fetchLikes,
-      fetchNbTracks /*fetchSimilarity*/,
-    ].concat(fcts);
+  // if (!options.after && !options.before)
+  // main tab: tracks (full layout to render, with sidebar)
+  fcts = [
+    fetchPlaylists,
+    /*fetchSubscriptions,*/ fetchStats,
+    fetchLikes,
+    fetchNbTracks /*fetchSimilarity*/,
+  ].concat(fcts);
   //if (options.showSubscribers || options.showSubscriptions || options.showActivity)
   //	fcts = [fetchSubscriptions].concat(fcts);
 
