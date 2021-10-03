2450fed50b01145bf865374a16eaef91cd4ac43d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-03 10:37:25 +0200

    move fetchAndRenderProfile()'s dependencies to LibUserProfile.js

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -4,17 +4,12 @@
  * @author adrienjoly, whyd
  **/
 
-var snip = require('../snip.js');
 var config = require('../models/config.js');
-var mongodb = require('../models/mongodb.js');
 var userModel = require('../models/user.js');
 var followModel = require('../models/follow.js');
 var postModel = require('../models/post.js');
-var activityModel = require('../models/activity.js');
-var activityController = require('../controllers/recentActivity.js');
 var feedTemplate = require('../templates/feed.js');
 const feedOptions = require('../templates/feedOptions.js');
-var uiSnippets = require('../templates/uiSnippets.js');
 const {
   playlistTemplateV2,
   fetchAndRenderPlaylist,
@@ -23,11 +18,6 @@ const {
   profileTemplateV2,
   fetchAndRenderProfile,
 } = require('./LibUserProfile.js');
-
-var MAX_PLAYLISTS_SIDE = 4;
-var MAX_FRIENDS = 6;
-var MAX_HISTORY = 3;
-var MAX_SUBSCRIPTIONS = 50;
 
 // DATA FETCHING HELPERS
 
@@ -71,50 +61,6 @@ function fetchNbTracks(options, callback) {
   });
 }
 
-// used to render the sidebar
-function fetchActivity(options, cb) {
-  activityModel.fetchHistoryFromUidList(
-    [options.user.id],
-    { limit: MAX_HISTORY },
-    function (activities) {
-      if (activities.length < MAX_HISTORY)
-        activities.push({
-          _id: mongodb.ObjectId(options.user.id),
-          other: { text: 'joined whyd' },
-        });
-      var postsToPopulate = [];
-      for (let i in activities)
-        if (activities[i]) {
-          activities[i].ago = uiSnippets.renderTimestamp(
-            new Date() - activities[i]._id.getTimestamp()
-          );
-          if (activities[i].like)
-            postsToPopulate.push(mongodb.ObjectId('' + activities[i].like.pId));
-        }
-
-      postModel.fetchPosts(
-        { _id: { $in: postsToPopulate } },
-        /*params*/ null,
-        /*options*/ null,
-        function (posts) {
-          var postSet = {};
-          options.activity = [];
-          for (let i in posts) postSet['' + posts[i]._id] = posts[i];
-          for (let i in activities) {
-            if ((activities[i] || {}).like) {
-              if (postSet['' + activities[i].like.pId])
-                activities[i].like.post = postSet['' + activities[i].like.pId];
-              else continue; // do not keep null posts
-            }
-            options.activity.push(activities[i]);
-          }
-          cb();
-        }
-      );
-    }
-  );
-}
-
 // PAGE RENDERING
 
 function generateMixpanelCode(options) {
@@ -123,50 +69,6 @@ function generateMixpanelCode(options) {
     ' window.Whyd.tracking.log("Visit profile", "' + options.uid + '");',
     '</script>',
   ].join('\n');
-}
-
-function renderPlaylists(options, maxNb) {
-  var playlists = options.user.pl || [];
-  if (maxNb) {
-    if (playlists.length > maxNb) playlists = playlists.slice(0, maxNb);
-    else if (playlists.length < maxNb) {
-      if (options.loggedUser && options.user.id == options.loggedUser.id)
-        playlists.push({
-          url: 'javascript:dlgCreatePlaylist();',
-          class: 'btnNewPlaylist',
-          img: '#',
-          name: 'Create a playlist',
-        });
-    }
-  }
-  for (let i in playlists)
-    if (playlists[i].id !== undefined) {
-      playlists[i].img =
-        '/img/playlist/' + options.user.id + '_' + playlists[i].id;
-    }
-  return playlists;
-}
-
-function renderFriends(friends) {
-  for (let i in friends) {
-    friends[i].url = '/u/' + friends[i].id;
-    friends[i].img = '/img/u/' + friends[i].id;
-  }
-  return friends;
-}
-
-function populateUsers(subscr, options, cb) {
-  followModel.fetchSubscriptionArray(
-    options.loggedUser.id,
-    function (mySubscr) {
-      var subscrSet = snip.arrayToSet(mySubscr);
-      for (let i in subscr)
-        if (subscrSet[subscr[i].id]) subscr[i].subscribed = true;
-      userModel.fetchUserBios(subscr, function () {
-        cb(renderFriends(subscr));
-      });
-    }
-  );
 }
 
 var bareFormats = new Set(['json', 'links']);
--- app/controllers/LibUserProfile.js
+++ app/controllers/LibUserProfile.js
@@ -1,9 +1,111 @@
+var snip = require('../snip.js');
+var mongodb = require('../models/mongodb.js');
+var userModel = require('../models/user.js');
+var followModel = require('../models/follow.js');
+var postModel = require('../models/post.js');
+var activityModel = require('../models/activity.js');
+var activityController = require('../controllers/recentActivity.js');
+const feedOptions = require('../templates/feedOptions.js');
+var uiSnippets = require('../templates/uiSnippets.js');
 var templateLoader = require('../templates/templateLoader.js');
 var profileTemplateV2 = templateLoader.loadTemplate(
   'app/templates/userProfileV2.html'
 );
 
+var MAX_PLAYLISTS_SIDE = 4;
+var MAX_FRIENDS = 6;
+var MAX_SUBSCRIPTIONS = 50;
+var MAX_HISTORY = 3;
+
 exports.profileTemplateV2 = profileTemplateV2;
+
+function renderFriends(friends) {
+  for (let i in friends) {
+    friends[i].url = '/u/' + friends[i].id;
+    friends[i].img = '/img/u/' + friends[i].id;
+  }
+  return friends;
+}
+
+function renderPlaylists(options, maxNb) {
+  var playlists = options.user.pl || [];
+  if (maxNb) {
+    if (playlists.length > maxNb) playlists = playlists.slice(0, maxNb);
+    else if (playlists.length < maxNb) {
+      if (options.loggedUser && options.user.id == options.loggedUser.id)
+        playlists.push({
+          url: 'javascript:dlgCreatePlaylist();',
+          class: 'btnNewPlaylist',
+          img: '#',
+          name: 'Create a playlist',
+        });
+    }
+  }
+  for (let i in playlists)
+    if (playlists[i].id !== undefined) {
+      playlists[i].img =
+        '/img/playlist/' + options.user.id + '_' + playlists[i].id;
+    }
+  return playlists;
+}
+
+function populateUsers(subscr, options, cb) {
+  followModel.fetchSubscriptionArray(
+    options.loggedUser.id,
+    function (mySubscr) {
+      var subscrSet = snip.arrayToSet(mySubscr);
+      for (let i in subscr)
+        if (subscrSet[subscr[i].id]) subscr[i].subscribed = true;
+      userModel.fetchUserBios(subscr, function () {
+        cb(renderFriends(subscr));
+      });
+    }
+  );
+}
+
+// used to render the sidebar
+function fetchActivity(options, cb) {
+  activityModel.fetchHistoryFromUidList(
+    [options.user.id],
+    { limit: MAX_HISTORY },
+    function (activities) {
+      if (activities.length < MAX_HISTORY)
+        activities.push({
+          _id: mongodb.ObjectId(options.user.id),
+          other: { text: 'joined whyd' },
+        });
+      var postsToPopulate = [];
+      for (let i in activities)
+        if (activities[i]) {
+          activities[i].ago = uiSnippets.renderTimestamp(
+            new Date() - activities[i]._id.getTimestamp()
+          );
+          if (activities[i].like)
+            postsToPopulate.push(mongodb.ObjectId('' + activities[i].like.pId));
+        }
+
+      postModel.fetchPosts(
+        { _id: { $in: postsToPopulate } },
+        /*params*/ null,
+        /*options*/ null,
+        function (posts) {
+          var postSet = {};
+          options.activity = [];
+          for (let i in posts) postSet['' + posts[i]._id] = posts[i];
+          for (let i in activities) {
+            if ((activities[i] || {}).like) {
+              if (postSet['' + activities[i].like.pId])
+                activities[i].like.post = postSet['' + activities[i].like.pId];
+              else continue; // do not keep null posts
+            }
+            options.activity.push(activities[i]);
+          }
+          cb();
+        }
+      );
+    }
+  );
+}
 
 exports.fetchAndRenderProfile = function (options, callback, process) {
   // TODO: remove process => use callback only
