0041bd5dd9a43cbb82c310cadaa8ea996608bf52
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-03 11:36:10 +0200

    in renderUserLibrary, call remaining functions explicitly too

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -194,10 +194,6 @@ async function renderUserLibrary(lib, user) {
       );
   }
 
-  // add final rendering functions at queue of the call chain
-  const fcts = [fetchAndRender, renderResponse];
-
-  // prepend required fetching operations in head of the call chain
   if (feedOptions.mustRenderWholeProfilePage(options)) {
     // main tab: tracks (full layout to render, with sidebar)
     await new Promise((resolve) => fetchPlaylists(options, resolve));
@@ -205,13 +201,9 @@ async function renderUserLibrary(lib, user) {
     await new Promise((resolve) => fetchLikes(options, resolve));
     await new Promise((resolve) => fetchNbTracks(options, resolve));
   }
-  // run the call chain
-  (function next(res) {
-    var fct = fcts.shift();
-    fct(res || options, function (res) {
-      next(res || options);
-    });
-  })();
+
+  const res = await new Promise((resolve) => fetchAndRender(options, resolve));
+  await new Promise((resolve) => renderResponse(res, resolve));
 }
 
 exports.render = renderUserLibrary;
