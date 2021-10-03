156aada14dd3a25e28951e5ec87c676a18c20ecb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-03 11:20:57 +0200

    extract preparePaginationParameters()

--- app/controllers/LibUser.js
+++ app/controllers/LibUser.js
@@ -72,21 +72,7 @@ function fetchAndRender(options, callback) {
 
   const renderer = options.playlistId ? playlistRenderer : profileRenderer;
 
-  options.fetchParams = {
-    after: options.after,
-    before: options.before,
-    limit: options.limit,
-  };
-  if (options.embedW)
-    options.fetchParams.limit = config.nbTracksPerPlaylistEmbed;
-  else if (options.limit && typeof options.limit !== 'number') {
-    if (typeof options.limit === 'string')
-      options.fetchParams.limit = parseInt(options.limit);
-    else if (typeof options.limit === 'object' && options.limit.push)
-      options.fetchParams.limit = parseInt(options.limit.pop());
-    // keep only the last value
-    // see https://github.com/openwhyd/openwhyd/issues/89
-  }
+  preparePaginationParameters(options);
 
   renderer.fetchAndRender(options, (error, posts) => {
     if (error) {
@@ -111,6 +97,24 @@ var LNK_URL_PREFIX = {
   yt: 'youtube.com/user/',
   igrm: 'instagram.com/',
 };
+
+function preparePaginationParameters(options) {
+  options.fetchParams = {
+    after: options.after,
+    before: options.before,
+    limit: options.limit,
+  };
+  if (options.embedW)
+    options.fetchParams.limit = config.nbTracksPerPlaylistEmbed;
+  else if (options.limit && typeof options.limit !== 'number') {
+    if (typeof options.limit === 'string')
+      options.fetchParams.limit = parseInt(options.limit);
+    else if (typeof options.limit === 'object' && options.limit.push)
+      options.fetchParams.limit = parseInt(options.limit.pop());
+    // keep only the last value
+    // see https://github.com/openwhyd/openwhyd/issues/89
+  }
+}
 
 function renderUserLinks(lnk) {
   // clean social links
