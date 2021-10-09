3a79399f9ef490aa34906a2b3f47d6a9f404fb0a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-09 09:20:32 +0200

    fix(tests): Make approval tests run faster (#484)
    ... by letting approval tests start Openwhyd server programmatically, instead of relying on Docker:
    
    ```sh
    $ npm run test-approval
    ```

--- test/db-helpers.js
+++ test/db-helpers.js
@@ -2,11 +2,25 @@ const fs = require('fs');
 const { promisify } = require('util');
 const mongodb = require('mongodb');
 const request = require('request');
+const childProcess = require('child_process');
+const waitOn = require('wait-on');
+
+const readFile = (file) => fs.promises.readFile(file, 'utf-8');
+
+const loadEnvVars = async (file) => {
+  const envVars = {};
+  (await readFile(file)).split(/[\r\n]+/).forEach((envVar) => {
+    if (!envVar) return;
+    const [key, def] = envVar.split('=');
+    envVars[key] = def.replace(/^"|"$/g, '');
+  });
+  return envVars;
+};
 
 async function readMongoDocuments(file) {
   const ISODate = (d) => new Date(d);
   const ObjectId = (id) => mongodb.ObjectID.createFromHexString(id);
-  return eval(await fs.promises.readFile(file, 'utf-8'));
+  return eval(await readFile(file));
 }
 
 async function insertTestData(url, docsPerCollection) {
@@ -23,13 +37,25 @@ async function insertTestData(url, docsPerCollection) {
   await mongoClient.close();
 }
 
+async function startOpenwhydServer(env) {
+  const serverProcess = childProcess.fork(
+    './app.js',
+    ['--fakeEmail', '--digestInterval', '-1'],
+    { env, silent: true }
+  );
+  await waitOn({ resources: [`http://localhost:${env.WHYD_PORT}`] });
+  return serverProcess;
+}
+
 /* refresh openwhyd's in-memory cache of users, to allow this user to login */
 async function refreshOpenwhydCache(urlPrefix = 'http://localhost:8080') {
   await promisify(request.post)(urlPrefix + '/testing/refresh');
 }
 
 module.exports = {
+  loadEnvVars,
   readMongoDocuments,
   insertTestData,
+  startOpenwhydServer,
   refreshOpenwhydCache,
 };
