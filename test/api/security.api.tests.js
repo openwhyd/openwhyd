b41cc7e1727b083f41c59942683aef5320aa3842
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-24 16:57:22 +0200

    fix test criteria

--- test/api/security.api.tests.js
+++ test/api/security.api.tests.js
@@ -13,40 +13,35 @@ before(cleanup);
 
 describe('security', () => {
   describe('Open Redirect', () => {
-    it('should redirect to /stream', async () => {
+    it('should allow redirect to /stream', async () => {
       const target = `/stream`;
       const { jar } = await loginAs(ADMIN_USER);
       const { response } = await postRaw(jar, `/consent`, {
         lang: 'en',
         redirect: target,
       });
-      console.log('response.headers', response.headers);
       assert.equal(response.statusCode, 302);
       assert.equal(response.headers.location, target);
     });
 
-    it(`should redirect to ${URL_PREFIX}/stream`, async () => {
+    it(`should allow redirect to ${URL_PREFIX}/stream`, async () => {
       const target = `${URL_PREFIX}/stream`;
       const { jar } = await loginAs(ADMIN_USER);
       const { response } = await postRaw(jar, `/consent`, {
         lang: 'en',
         redirect: target,
       });
-      console.log('response.headers', response.headers);
       assert.equal(response.statusCode, 302);
       assert.equal(response.headers.location, target);
     });
 
-    it('should NOT redirect to other domain', async () => {
-      const target = `http://google.com`;
+    it('should NOT allow redirect to other domain', async () => {
       const { jar } = await loginAs(ADMIN_USER);
       const { response } = await postRaw(jar, `/consent`, {
         lang: 'en',
-        redirect: target,
+        redirect: `http://google.com`,
       });
-      console.log('response.headers', response.headers);
-      assert.equal(response.statusCode, 403);
-      assert.notEqual(response.headers.location, target);
+      assert.equal(response.statusCode, 403); // forbidden
     });
   });
 });
