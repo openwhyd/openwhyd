7c0eddc9dc9e60fa163624d356837e1a111018d1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-10-24 15:37:08 +0200

    try to fix `The play() request was interrupted by a call to pause(). https://goo.gl/LdLk22` - cf https://github.com/openwhyd/openwhyd/runs/3988567033?check_suite_focus=true#step:7:124 - cf https://github.com/openwhyd/openwhyd/runs/3988567033?check_suite_focus=true#step:7:242

--- cypress/integration/acceptance.spec.ts
+++ cypress/integration/acceptance.spec.ts
@@ -62,6 +62,8 @@ context('Openwhyd', () => {
     // should play the track
     cy.get('#btnPlay.playing', { timeout: 10000 }).should('be.visible');
 
+    cy.wait(1000);
+
     // should pause the track when the user clicks on the play/pause button
     cy.get('#btnPlay').click();
     cy.get('#btnPlay').should('not.have.class', 'playing');
--- cypress/integration/playback.spec.ts
+++ cypress/integration/playback.spec.ts
@@ -11,6 +11,8 @@ context('Playback', () => {
     // should play the track
     cy.get('#btnPlay.playing', { timeout: 10000 }).should('be.visible');
 
+    cy.wait(1000);
+
     // should pause the track when the user clicks on the play/pause button
     cy.get('#btnPlay').click();
     cy.get('#btnPlay').should('not.have.class', 'playing');
