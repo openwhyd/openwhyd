e615a3c2c73a8c8d2eb4957de247927f2d594d69
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-12-10 15:27:55 +0100

    try to fix `Cypress command timeout of `4000ms` exceeded` by disabling @cypress/code-coverage
    cf https://github.com/openwhyd/openwhyd/runs/4484506970?check_suite_focus=true#step:8:5351
    
    details:
    "after each" hook for "should allow user to re-add a track into a playlist":
         CypressError: Cypress command timeout of `4000ms` exceeded.
    
    Because this error occurred during a `after each` hook we are skipping all of the remaining tests.
          at cypressErr (http://localhost:8080/__cypress/runner/cypress_runner.js:172722:18)
          at Object.errByPath (http://localhost:8080/__cypress/runner/cypress_runner.js:172791:10)
          at http://localhost:8080/__cypress/runner/cypress_runner.js:174541:80

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config);
+  // require('@cypress/code-coverage/task')(on, config);
   require('cypress-log-to-output').install(on); // note: only works with Chrome
 
   // add other tasks to be registered here
