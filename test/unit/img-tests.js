8adb406e038b41b04bfa2468847d36a6c40fd34b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-22 17:09:44 +0100

    extract img-tests.js from unit-test.js

---
+++ test/unit/img-tests.js
@@ -0,0 +1,51 @@
+/* global describe, it */
+
+const assert = require('assert');
+const fs = require('fs');
+
+describe('"img" package', function () {
+  //var imgUrl = 'http://www.azurs.net/photographies/laurier-rose-fleur-rebigue.jpg';
+  var imgOutput = 'public/images/logo-s.png';
+  assert(fs.existsSync(imgOutput), imgOutput + ' should exist');
+  var log = console.log;
+
+  /* // does not work on docker & travis-ci (no access to the web?)
+    it('should download an image', function(done) {
+        //console.log = function() {}; // prevent ContentEmbed from printing to the console
+        console.log('loading img:', imgUrl);
+        img.get(imgUrl, imgOutput, function () {
+            //console.log = log; // restore console.log
+            assert(fs.existsSync(imgOutput), imgOutput + ' should be downloaded by img.get()');
+            done();
+        }, function(err) {
+            assert.ifError(err);
+        });
+    });
+    */
+
+  it('should create a thumb', function (done) {
+    var thumbWidth = null; // auto-scaling
+    var thumbHeight = 80;
+    var thumbOutput = 'uniqueHash_thumb.jpg';
+    try {
+      fs.unlinkSync(thumbOutput);
+    } catch (e) {
+      console.error(e.message);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    console.log = function () {}; // prevent ContentEmbed from printing to the console
+    var img = require('../../app/lib/my-img');
+    img.makeThumb(imgOutput, thumbOutput, thumbWidth, thumbHeight, function () {
+      console.log = log; // restore console.log
+      assert(
+        fs.existsSync(thumbOutput),
+        thumbOutput + ' should be created by img.makeThumb()'
+      );
+      //console.log(imgOutput + ' thumb saved at ' + thumbOutput);
+      //fs.unlinkSync(imgOutput);
+      fs.unlinkSync(thumbOutput);
+      done();
+    });
+  });
+});
--- test/unit/unit-tests.js
+++ test/unit/unit-tests.js
@@ -1,6 +1,8 @@
 /* global describe, it */
 
-var assert = require('assert');
+// $ npx mocha test/unit/unit-tests.js
+
+const assert = require('assert');
 const express = require('express');
 
 const HTML_PAGE_WITH_TITLE_AND_IMAGES = [
@@ -88,55 +90,6 @@ describe('"get" package', function () {
       assert.ifError(err);
       assert(page.getTitle());
       assert(page.getImages().length);
-      done();
-    });
-  });
-});
-
-describe('"img" package', function () {
-  var fs = require('fs');
-
-  //var imgUrl = 'http://www.azurs.net/photographies/laurier-rose-fleur-rebigue.jpg';
-  var imgOutput = 'public/images/logo-s.png';
-  assert(fs.existsSync(imgOutput), imgOutput + ' should exist');
-  var log = console.log;
-
-  /* // does not work on docker & travis-ci (no access to the web?)
-    it('should download an image', function(done) {
-        //console.log = function() {}; // prevent ContentEmbed from printing to the console
-        console.log('loading img:', imgUrl);
-        img.get(imgUrl, imgOutput, function () {
-            //console.log = log; // restore console.log
-            assert(fs.existsSync(imgOutput), imgOutput + ' should be downloaded by img.get()');
-            done();
-        }, function(err) {
-            assert.ifError(err);
-        });
-    });
-    */
-
-  it('should create a thumb', function (done) {
-    var thumbWidth = null; // auto-scaling
-    var thumbHeight = 80;
-    var thumbOutput = 'uniqueHash_thumb.jpg';
-    try {
-      fs.unlinkSync(thumbOutput);
-    } catch (e) {
-      console.error(e.message);
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    console.log = function () {}; // prevent ContentEmbed from printing to the console
-    var img = require('../../app/lib/my-img');
-    img.makeThumb(imgOutput, thumbOutput, thumbWidth, thumbHeight, function () {
-      console.log = log; // restore console.log
-      assert(
-        fs.existsSync(thumbOutput),
-        thumbOutput + ' should be created by img.makeThumb()'
-      );
-      //console.log(imgOutput + ' thumb saved at ' + thumbOutput);
-      //fs.unlinkSync(imgOutput);
-      fs.unlinkSync(thumbOutput);
       done();
     });
   });
