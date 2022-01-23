b93e0dff5f1936e0b0bf4611942773881280421a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-23 14:42:42 +0100

    🟢 hot tracks feature should list one track in first position, if just that track was posted

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -1,5 +1,9 @@
 const assert = require('assert');
 
+function getHotTracks() {
+  return [{ eId: '1' }];
+}
+
 describe('hot tracks feature', () => {
   it('should list one track in first position, if just that track was posted', () => {
     const postedTrack = { eId: '1' };
