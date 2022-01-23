16384178b10c619804b6a23824b9ba19f0bc78fa
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-23 17:21:23 +0100

    fix `TypeError: fetchPostsByPid is not a function`

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -5,9 +5,11 @@ describe('hot tracks feature', () => {
   it('should list one track in first position, if just that track was posted', async () => {
     const postedTrack = { eId: '1' };
     const getTracksByDescendingScore = () => Promise.resolve([postedTrack]);
-    assert.deepEqual(await getHotTracks(getTracksByDescendingScore), [
-      postedTrack,
-    ]);
+    const fetchPostsByPid = () => Promise.resolve({});
+    assert.deepEqual(
+      await getHotTracks(getTracksByDescendingScore, fetchPostsByPid),
+      [postedTrack]
+    );
   });
 
   it('should list the track with higher score in first position, given two posted tracks with different scores', async () => {
@@ -15,9 +17,10 @@ describe('hot tracks feature', () => {
     const bestTrack = { eId: '2', score: 2 };
     const getTracksByDescendingScore = () =>
       Promise.resolve([bestTrack, regularTrack]);
-    assert.deepEqual(await getHotTracks(getTracksByDescendingScore), [
-      bestTrack,
-      regularTrack,
-    ]);
+    const fetchPostsByPid = () => Promise.resolve({});
+    assert.deepEqual(
+      await getHotTracks(getTracksByDescendingScore, fetchPostsByPid),
+      [bestTrack, regularTrack]
+    );
   });
 });
