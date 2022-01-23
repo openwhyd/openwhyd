2f0229bcd7ce5f5fed2e705cfa4ec81a15abe8c3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-23 14:43:06 +0100

    🔴 hot tracks feature should list the track with higher score in first position, given two posted tracks with different scores

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -9,4 +9,10 @@ describe('hot tracks feature', () => {
     const postedTrack = { eId: '1' };
     assert.deepEqual(getHotTracks(), [postedTrack]);
   });
+
+  it('should list the track with higher score in first position, given two posted tracks with different scores', () => {
+    const regularTrack = { eId: '1', score: 1 };
+    const bestTrack = { eId: '2', score: 2 };
+    assert.deepEqual(getHotTracks(), [bestTrack, regularTrack]);
+  });
 });
