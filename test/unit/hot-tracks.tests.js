7256a9a6bb21d709fd7a583499116c58f5ae4b5d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-23 14:53:15 +0100

    refactor: getHostTracks(getPostedTracks: () => Posts)

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
 
-function getHotTracks(postedTracks) {
+function getHotTracks(getPostedTracks) {
   // sorts tracks by descending score
-  return postedTracks.sort((a, b) => b.score - a.score);
+  return getPostedTracks().sort((a, b) => b.score - a.score);
 }
 
 describe('hot tracks feature', () => {
   it('should list one track in first position, if just that track was posted', () => {
     const postedTrack = { eId: '1' };
-    const postedTracks = [postedTrack];
-    assert.deepEqual(getHotTracks(postedTracks), [postedTrack]);
+    const getPostedTracks = () => [postedTrack];
+    assert.deepEqual(getHotTracks(getPostedTracks), [postedTrack]);
   });
 
   it('should list the track with higher score in first position, given two posted tracks with different scores', () => {
     const regularTrack = { eId: '1', score: 1 };
     const bestTrack = { eId: '2', score: 2 };
-    const postedTracks = [regularTrack, bestTrack];
-    assert.deepEqual(getHotTracks(postedTracks), [bestTrack, regularTrack]);
+    const getPostedTracks = () => [regularTrack, bestTrack];
+    assert.deepEqual(getHotTracks(getPostedTracks), [bestTrack, regularTrack]);
   });
 });
