c3655ba97a362ff36d110c1db24fd8ac5b3c1dee
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-23 17:19:42 +0100

    fix `TypeError: getTracksByDescendingScore(...).then is not a function`

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -4,14 +4,15 @@ const { getHotTracks } = require('../../app/features/hot-tracks.js');
 describe('hot tracks feature', () => {
   it('should list one track in first position, if just that track was posted', () => {
     const postedTrack = { eId: '1' };
-    const getTracksByDescendingScore = () => [postedTrack];
+    const getTracksByDescendingScore = () => Promise.resolve([postedTrack]);
     assert.deepEqual(getHotTracks(getTracksByDescendingScore), [postedTrack]);
   });
 
   it('should list the track with higher score in first position, given two posted tracks with different scores', () => {
     const regularTrack = { eId: '1', score: 1 };
     const bestTrack = { eId: '2', score: 2 };
-    const getTracksByDescendingScore = () => [bestTrack, regularTrack];
+    const getTracksByDescendingScore = () =>
+      Promise.resolve([bestTrack, regularTrack]);
     assert.deepEqual(getHotTracks(getTracksByDescendingScore), [
       bestTrack,
       regularTrack,
