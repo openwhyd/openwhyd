d328fe2201f31c1a35e6b6edca94678feffd3c1c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-01-28 11:51:06 +0100

    test: it should return tracks with post metadata

--- test/unit/hot-tracks.tests.js
+++ test/unit/hot-tracks.tests.js
@@ -23,4 +23,32 @@ describe('hot tracks feature', () => {
       [bestTrack, regularTrack]
     );
   });
+
+  it('should return tracks with post metadata', async () => {
+    const posts = [
+      {
+        _id: '61e19a3f078b4c9934e72ce4',
+        eId: '1',
+        pl: { name: 'soundtrack of my life', id: 0 }, // metadata from the post that will be included in the list of hot tracks
+      },
+      {
+        _id: '61e19a3f078b4c9934e72ce5',
+        eId: '2',
+        text: 'my favorite track ever!', // metadata from the post that will be included in the list of hot tracks
+      },
+    ];
+    const tracks = [
+      { eId: posts[0].eId, pId: posts[0]._id },
+      { eId: posts[1].eId, pId: posts[1]._id },
+    ];
+    const getTracksByDescendingScore = () => Promise.resolve(tracks);
+    const fetchPostsByPid = (pidList) =>
+      Promise.resolve(posts.filter(({ _id }) => pidList.includes(_id)));
+    const hotTracks = await getHotTracks(
+      getTracksByDescendingScore,
+      fetchPostsByPid
+    );
+    assert.deepEqual(hotTracks[0].pl, posts[0].pl);
+    assert.deepEqual(hotTracks[1].text, posts[1].text);
+  });
 });
