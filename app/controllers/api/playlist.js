openwhyd/openwhyd

commit 9aff4ac0ad21cd1de2992cad9bc72a2eed7c1185

fix: Migrate playlist creation to Hexagonal Architecture (#563)
See https://github.com/openwhyd/openwhyd/issues/559 and https://github.com/openwhyd/openwhyd-solo/issues/7.

Video recording of the Devoxx talk in which we performed that migration: [Architecturoplastie hexagonale d’un backend Node.js (Jordan Nourry, Adrien Joly, Julien Topçu) - YouTube](https://www.youtube.com/watch?v=r2XMwAUqZBA)

Co-authored-by: Jordan NOURRY <jordan.nourry@gmail.com>
Co-authored-by: Julien Topçu <52136235+julien-topcu@users.noreply.github.com>

app/controllers/api/playlist.js
@@ -1,3 +1,4 @@
+//@ts-check
 /**
  * api endpoint for playlists
  * @author adrienjoly, whyd
@@ -5,14 +6,15 @@
 
 var postModel = require('../../models/post.js');
 var userModel = require('../../models/user.js');
-var notifModel = require('../../models/notif.js');
 var uploadCtr = require('../uploadedFile.js');
 
 exports.actions = {
-  sendToUsers: notifModel.sendPlaylistToUsers,
-  create: function (p, callback) {
-    userModel.createPlaylist(p.uId, p.name, callback);
-    // returns {id, name}
+  /**
+   * @param playlistRequest {{uId:string, name:string}}
+   * @param createPlaylist {import ('../../domain/api/Features').CreatePlaylist}
+   */
+  create: function ({ uId: userId, name }, callback, { createPlaylist }) {
+    createPlaylist(userId, name).then(callback);
   },
   rename: function (p, callback) {
     userModel.renamePlaylist(p.uId, p.id, p.name, callback);
@@ -30,13 +32,13 @@ exports.actions = {
       uploadCtr.deleteFile(imgPath);
       userModel.fetchPlaylist(p.uId, p.id, function (pl) {
         /*
-				if (pl && pl.img && pl.img.indexOf("blank") == -1) {
-					console.log("deleting previous playlist pic: " + pl.img);
-					uploadCtr.deleteFile(pl.img);
-				}
-				function actualUpdate(newFilename) {
-					userModel.setPlaylistImg(p.uId, p.id, newFilename || p.img, cb);
-				}*/
+        if (pl && pl.img && pl.img.indexOf("blank") == -1) {
+          console.log("deleting previous playlist pic: " + pl.img);
+          uploadCtr.deleteFile(pl.img);
+        }
+        function actualUpdate(newFilename) {
+          userModel.setPlaylistImg(p.uId, p.id, newFilename || p.img, cb);
+        }*/
         if (p.img.indexOf('blank') == -1)
           uploadCtr.renameTo(p.img, imgPath, function () {
             cb(pl);
@@ -47,7 +49,7 @@ exports.actions = {
   },
 };
 
-exports.handlePostRequest = function (request, reqParams, response) {
+exports.handlePostRequest = function (request, reqParams, response, features) {
   request.logToConsole('aoi.playlist.handleRequest', reqParams);
 
   // make sure a registered user is logged, or return an error page
@@ -63,14 +65,18 @@ exports.handlePostRequest = function (request, reqParams, response) {
   reqParams.uId = user.id;
   reqParams.uNm = user.name;
 
-  exports.actions[reqParams.action](reqParams, function (res, args) {
-    console.log(reqParams, '=>', res);
-    response.legacyRender(
-      res,
-      null,
-      args || { 'content-type': 'application/json' }
-    );
-  });
+  exports.actions[reqParams.action](
+    reqParams,
+    function (res, args) {
+      console.log(reqParams, '=>', res);
+      response.legacyRender(
+        res,
+        null,
+        args || { 'content-type': 'application/json' }
+      );
+    },
+    features
+  );
 };
 
 function fetchPlaylist(p, cb) {
@@ -123,12 +129,12 @@ function fetchPlaylist(p, cb) {
   );
 }
 
-exports.controller = function (request, getParams, response) {
+exports.controller = function (request, getParams, response, features) {
   getParams = getParams || {};
   getParams.id = getParams.id || getParams._1;
   request.logToConsole('apiPost.controller', getParams);
   if (request.method.toLowerCase() === 'post')
-    exports.handlePostRequest(request, request.body, response);
+    exports.handlePostRequest(request, request.body, response, features);
   else if (getParams.id)
     fetchPlaylist(getParams, function (playlists) {
       response.renderJSON(playlists);

app/controllers/api/post.js
@@ -1,3 +1,4 @@
+// @ts-check
 /**
  * api endpoint for posts
  * @author adrienjoly, whyd
@@ -101,18 +102,20 @@ exports.actions = {
 
   deleteComment: commentModel.delete,
 
-  insert: function (p, callback) {
-    var q = {
-      uId: p.uId,
-      uNm: p.uNm,
-      text: p.text || '',
-      pl: undefined, // to be parsed/populated before calling actualInsert()
+  /**
+   * @param createPlaylist {import('../../domain/api/Features').CreatePlaylist}
+   */
+  insert: async function (httpRequestParams, callback, _, { createPlaylist }) {
+    var postRequest = {
+      uId: httpRequestParams.uId,
+      uNm: httpRequestParams.uNm,
+      text: httpRequestParams.text || '',
       // fields that will be ignored by rePost():
-      name: p.name,
-      eId: p.eId,
+      name: httpRequestParams.name,
+      eId: httpRequestParams.eId,
     };
 
-    if (p.ctx) q.ctx = p.ctx;
+    if (httpRequestParams.ctx) postRequest.ctx = httpRequestParams.ctx;
 
     function tryJsonParse(p) {
       try {
@@ -123,57 +126,55 @@ exports.actions = {
     }
 
     async function actualInsert() {
-      if (p.pId) postModel.rePost(p.pId, q, callback);
+      if (httpRequestParams.pId)
+        postModel.rePost(httpRequestParams.pId, postRequest, callback);
       else {
-        if (p._id) {
+        if (httpRequestParams._id) {
           // edit mode
           const existingPost = await new Promise((resolve) =>
-            postModel.fetchPostById(p._id, resolve)
+            postModel.fetchPostById(httpRequestParams._id, resolve)
           );
-          if (existingPost.uId !== q.uId) {
+          if (existingPost.uId !== postRequest.uId) {
             callback({ error: "updating another user's post is not allowed" });
             return;
           }
-          q._id = p._id;
+          postRequest._id = httpRequestParams._id;
         }
 
-        if (p.img && p.img != 'null') q.img = p.img;
+        if (httpRequestParams.img && httpRequestParams.img != 'null')
+          postRequest.img = httpRequestParams.img;
 
-        if (p.src)
+        if (httpRequestParams.src)
           // source webpage of the content: {id,name} provided by bookmarklet
-          q.src = typeof p.src == 'object' ? p.src : tryJsonParse(p.src);
-        else if (p['src[id]'] && p['src[name]'])
-          q.src = {
-            id: p['src[id]'],
-            name: p['src[name]'],
+          postRequest.src =
+            typeof httpRequestParams.src == 'object'
+              ? httpRequestParams.src
+              : tryJsonParse(httpRequestParams.src);
+        else if (httpRequestParams['src[id]'] && httpRequestParams['src[name]'])
+          postRequest.src = {
+            id: httpRequestParams['src[id]'],
+            name: httpRequestParams['src[name]'],
           };
-        if (!q.src || !q.src.id) delete q.src;
+        if (!postRequest.src || !postRequest.src.id) delete postRequest.src;
 
-        postModel.savePost(q, callback);
+        postModel.savePost(postRequest, callback);
       }
     }
 
-    // process playlist
-    try {
-      q.pl = typeof p.pl == 'object' ? p.pl : JSON.parse(p.pl);
-    } catch (e) {
-      q.pl = {
-        id: p['pl[id]'],
-        name: p['pl[name]'],
+    // Muter post avec la notion de playlist provenant des params
+    // Clean code => Pure function
+    const playlistRequest = extractPlaylistRequestFrom(httpRequestParams);
+
+    if (needToCreatePlaylist(playlistRequest)) {
+      postRequest.pl = await createPlaylist(
+        httpRequestParams.uId,
+        playlistRequest.name
+      );
+    } else if (hasAValidPlaylistId(playlistRequest.id)) {
+      postRequest.pl = {
+        id: parseInt(playlistRequest.id, 10),
+        name: playlistRequest.name,
       };
-    }
-    if (q.pl.id == 'create') {
-      userModel.createPlaylist(p.uId, q.pl.name, function (playlist) {
-        if (playlist) {
-          q.pl.id = playlist.id;
-          // console.log('playlist was created', q.pl);
-        }
-        actualInsert();
-      });
-      return; // avoid inserting twice
-    } else {
-      q.pl.id = parseInt(q.pl.id);
-      if (isNaN(q.pl.id)) delete q.pl; //q.pl = null;
     }
 
     actualInsert();
@@ -271,7 +272,10 @@ exports.actions = {
   },
 };
 
-exports.handleRequest = function (request, reqParams, response) {
+/**
+ * @param features {import('../../domain/api/Features').Features}
+ */
+exports.handleRequest = function (request, reqParams, response, features) {
   request.logToConsole('api.post.handleRequest', reqParams);
 
   function resultHandler(res, args) {
@@ -300,16 +304,46 @@ exports.handleRequest = function (request, reqParams, response) {
   if (!user || !user.id) return response.badRequest();
 
   if (reqParams.action && exports.actions[reqParams.action])
-    exports.actions[reqParams.action](reqParams, resultHandler, request);
+    exports.actions[reqParams.action](
+      reqParams,
+      resultHandler,
+      request,
+      features
+    );
   else response.badRequest();
 };
 
-exports.controller = function (request, getParams, response) {
+/**
+ * @param features {import('../../domain/api/Features').Features}
+ */
+exports.controller = function (request, getParams, response, features) {
   //request.logToConsole("api.post", getParams);
   var params = snip.translateFields(getParams || {}, sequencedParameters);
 
   //if (request.method.toLowerCase() === 'post')
   for (let i in request.body) params[i] = request.body[i];
 
-  exports.handleRequest(request, params, response);
+  exports.handleRequest(request, params, response, features);
 };
+
+function hasAValidPlaylistId(id) {
+  return parseInt(id) >= 0;
+}
+
+function needToCreatePlaylist(playlistRequest) {
+  return playlistRequest.id == 'create';
+}
+
+function extractPlaylistRequestFrom(httpRequestParams) {
+  // Attention double responsabilité: parsing et mapping
+  try {
+    return typeof httpRequestParams.pl == 'object'
+      ? httpRequestParams.pl
+      : JSON.parse(httpRequestParams.pl);
+  } catch (e) {
+    return {
+      id: httpRequestParams['pl[id]'],
+      name: httpRequestParams['pl[name]'],
+    };
+  }
+}

app/controllers/subdir.js
@@ -4,7 +4,7 @@
  * @author adrienjoly, whyd
  */
 
-exports.controller = function (request, reqParams, response) {
+exports.controller = function (request, reqParams, response, features) {
   //request.logToConsole("[subdir]", reqParams);
 
   var path = request.url.split('?')[0];
@@ -17,7 +17,7 @@ exports.controller = function (request, reqParams, response) {
   try {
     const safeCtrPath = `./${subDir}/${ctrName}`.replace(/\.\./g, '');
     const { controller } = require(safeCtrPath);
-    controller(request, reqParams, response);
+    controller(request, reqParams, response, features);
   } catch (err) {
     console.error('[subdir] error while contacting ' + request.url, err);
     response.notFound();

app/domain/OpenWhydFeatures.js
@@ -1,0 +1,50 @@
+//@ts-check
+/**
+ * @typedef {import('../../app/domain/user/types').User} User
+ * @typedef {import('../../app/domain/user/types').Playlist} Playlist
+ * @typedef {import('../../app/domain/spi/UserRepository').UserRepository} UserRepository
+ * @typedef {import('./api/Features').Features} Features
+ * @typedef {import('./api/Features').CreatePlaylist} CreatePlaylist
+ */
+
+/**
+ *
+ * @param {UserRepository} userRepository
+ * @returns {Features}
+ */
+exports.features = function (userRepository) {
+  /**
+   * @type {([User, Playlist]) => Promise<Playlist>}
+   */
+  const insertPlaylist = ([user, playlist]) =>
+    userRepository
+      .insertPlaylist(user.id, playlist)
+      .then(() => Promise.resolve(playlist));
+
+  /**
+   * @param {string} playlistName
+   * @returns {(user: User) =>  Promise<[User, Playlist]>}
+   */
+  function addNewPlayListToUser(playlistName) {
+    /**
+     * @param {User} user
+     * @returns { Promise<[User, Playlist]>}
+     */
+
+    return (user) => user.addNewPlaylist(playlistName);
+  }
+
+  return {
+    /**
+     * @type {CreatePlaylist}
+     * @returns {Promise<Playlist>}
+     */
+    createPlaylist: (userId, playlistName) =>
+      Promise.resolve(
+        userRepository
+          .getByUserId(userId)
+          .then(addNewPlayListToUser(playlistName))
+          .then(insertPlaylist)
+      ),
+  };
+};

app/domain/api/Features.ts
@@ -1,0 +1,13 @@
+import { Playlist } from '../user/types';
+
+/**
+ * Hexagonal Architecture Domain API (primary ports)
+ */
+export interface Features {
+  createPlaylist: CreatePlaylist;
+}
+
+export type CreatePlaylist = (
+  userId: string,
+  playlistName: string
+) => Promise<Playlist>;

app/domain/spi/UserRepository.ts
@@ -1,0 +1,12 @@
+import { Playlist, User } from '../user/types';
+
+export interface UserRepository {
+  getByUserId: GetByUserId;
+  insertPlaylist: InsertPlaylist;
+}
+
+export type GetByUserId = (userId: string) => Promise<User>;
+export type InsertPlaylist = (
+  userid: string,
+  playlist: Playlist
+) => Promise<void>;

app/domain/user/User.js
@@ -1,0 +1,43 @@
+//@ts-check
+
+/**
+ * @typedef {import('./types').Playlist} Playlist
+ * @typedef {import('./types').User} User
+ */
+
+/**
+ * @type {User}
+ */
+module.exports = class User {
+  /**
+   * @param {string} id
+   * @param {Playlist[]} playlists
+   */
+  constructor(id, playlists) {
+    this.id = id;
+    this.playlists = playlists || [];
+  }
+
+  /**
+   * @param {string} playlistName
+   * @returns {Promise<[User,Playlist]>}
+   */
+  addNewPlaylist = (playlistName) => {
+    const newPlaylist = {
+      id: nextAvailablePlaylistId(this.playlists),
+      name: playlistName,
+    };
+    return Promise.resolve([
+      new User(this.id, [...this.playlists, newPlaylist]),
+      newPlaylist,
+    ]);
+  };
+};
+
+/**
+ * @param {Playlist[]} playlists
+ * @returns {number}
+ */
+function nextAvailablePlaylistId(playlists) {
+  return Math.max(...playlists.map(({ id }) => id), -1) + 1;
+}

app/domain/user/types.ts
@@ -1,0 +1,10 @@
+export interface Playlist {
+  id: number;
+  name: string;
+}
+
+export interface User {
+  id: string;
+  playlists: Playlist[];
+  addNewPlaylist: (playlistName: string) => Promise<[User, Playlist]>;
+}

commit 8a98016c8cd5b1a0c02a7f84ff090f5d01eee79f

fix: Regression on playlist sharing API (#564)
Fixes a regression from https://github.com/openwhyd/openwhyd/pull/563.

app/controllers/api/playlist.js
@@ -6,9 +6,11 @@
 
 var postModel = require('../../models/post.js');
 var userModel = require('../../models/user.js');
+var notifModel = require('../../models/notif.js');
 var uploadCtr = require('../uploadedFile.js');
 
 exports.actions = {
+  sendToUsers: notifModel.sendPlaylistToUsers,
   /**
    * @param playlistRequest {{uId:string, name:string}}
    * @param createPlaylist {import ('../../domain/api/Features').CreatePlaylist}