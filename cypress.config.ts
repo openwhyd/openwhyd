29f4c7934b93fc88b238a015f8ccf644cb2810c4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-12-30 10:29:32 +0100

    format cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: '8mw8bh',
@@ -21,10 +21,10 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      return require('./cypress/plugins/index.ts')(on, config);
     },
     baseUrl: 'http://localhost:8080',
     slowTestThreshold: 30000,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-})
+});
