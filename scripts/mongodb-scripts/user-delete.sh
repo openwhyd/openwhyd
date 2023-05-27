c29bd9c833482799062e16334bbc8a232e827d3e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-05-27 10:43:38 +0200

    chore: make sure that user is deleted by _id

--- scripts/mongodb-scripts/user-delete.sh
+++ scripts/mongodb-scripts/user-delete.sh
@@ -1,22 +1,29 @@
 USER_ID=$1
 
+echo "user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
-"db.user.deleteOne({id: \"$USER_ID\"})"
+"db.user.deleteOne({_id: \"$USER_ID\"})"
 
+echo "activity of user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.activity.deleteMany({id: \"$USER_ID\"})"
 
+echo "comments of user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.comment.deleteMany({uId: \"$USER_ID\"})"
 
+echo "subscriptions of user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.follow.deleteMany({uId: \"$USER_ID\"})"
 
+echo "subscriptions to user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.follow.deleteMany({tId: \"$USER_ID\"})"
 
+echo "notifications of user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.notif.deleteMany({uId: \"$USER_ID\"})"
 
+echo "posts of user $USER_ID..."
 mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --quiet --eval \
 "db.post.deleteMany({uId: \"$USER_ID\"})"
