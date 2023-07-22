4312df3999e6d8e4d63ec5c31310898e63a32fdd
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-07-22 11:38:46 +0200

    🔴 test param validation, to make sure that errors are returned

--- test/3rd-party/algoliasearch.test.js
+++ test/3rd-party/algoliasearch.test.js
@@ -1,5 +1,7 @@
 // $ ALGOLIA_APP_ID="XXXXX" ALGOLIA_API_KEY="YYYYY" npx jest test/3rd-party/algoliasearch.test.js
 
+const util = require('util');
+
 describe('Algolia search wrapper', () => {
   let searchModel;
 
@@ -11,11 +13,19 @@ describe('Algolia search wrapper', () => {
     searchModel = require('../../app/models/search.js');
   });
 
-  it('should index a post', () => {
-    const result = searchModel.indexTyped('post', {
+  it('should fail to index a post if name is missing', async () => {
+    const promise = util.promisify(searchModel.indexTyped)('post', {
+      _id: 'xyz',
+    });
+    await expect(promise).rejects.toThrow('indexTyped: missing parameters');
+  });
+
+  it('should index a post', async () => {
+    const result = await util.promisify(searchModel.indexTyped)('post', {
       _id: 'xyz',
       name: 'a post',
     });
+
     console.log({ result });
   });
 });
