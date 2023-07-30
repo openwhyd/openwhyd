13310828ca062e0856506c96c2cce03db82362eb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-07-30 15:29:51 +0200

    🧹 snip.tests.js

--- test/old/snip.tests.js
+++ test/old/snip.tests.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+//@ts-check
 
 /**
  * snippet tester
@@ -9,7 +9,6 @@ var assert = require('assert');
 
 describe('snip.httpRequest', function () {
   var snip = require('../../app/snip.js');
-  //var testRunner = new require("../app/serverTestRunner.js").ServerTestRunner();
 
   var YOUTUBE_API_KEY = 'AIzaSyDEkfynWx7RpE5Vd0EVubBvl1qq4a6vjio';
   var YOUTUBE_VIDEO_ID = 'aZT8VlTV1YY';
@@ -43,35 +42,4 @@ describe('snip.httpRequest', function () {
     assert(success);
     // AJ note: what's the meaning of this test? I forgot...
   });
-  /*
-	testRunner.addTest("youtube.com requests are run sequentially, after limiter is set", function(p, ee, cb){
-		var req1 = snip.httpRequestJSON(url, {}, handler);
-		var req2 = snip.httpRequestJSON(url, {}, function(err, res){
-			var success = req1 && !req2 && res.data.id == "7vlElmIWmNo";
-			cb(!success);
-		});
-	});
-
-	testRunner.addTest("youtube.com requests call back exactly once per call, after limiter is set", function(p, ee, cb){
-		var count = 0;
-		function counter(cb){
-			return function(err, res){
-				++count;
-				cb && cb(err, res);
-			};
-		}
-		var req1 = snip.httpRequestJSON(url, {}, counter(function(err, res){
-			if (count != 1){
-				cb(true);
-				cb = function(){};
-			}
-		}));
-		var req2 = snip.httpRequestJSON(url, {}, counter(function(err, res){
-			setTimeout(function(){
-				var success = count == 2;
-				cb(!success);
-			}, 3000);
-		}));
-	});
-  */
 });
