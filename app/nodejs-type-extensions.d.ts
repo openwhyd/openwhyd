1e1fd2109f7e4c411a372ab0098d349b9c10d3fe
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-07-30 16:11:12 +0200

    modernize app.js => adjust tsconfig and types

--- app/nodejs-type-extensions.d.ts
+++ app/nodejs-type-extensions.d.ts
@@ -6,6 +6,26 @@ type AppParams = {
   mongoDbPort: string;
   mongoDbAuthUser: string;
   mongoDbAuthPassword: string;
+  port: string | number;
+  urlPrefix: string;
+  color: boolean;
+  genuineSignupSecret: string;
+  searchModule: string;
+  emailModule: string;
+  digestInterval: number;
+  digestImmediate: boolean;
+  feedbackEmail: string;
+  version: string;
+  startTime: Date;
+  nbPostsPerNewsfeedPage: number;
+  nbTracksPerPlaylistEmbed: number;
+  paths: {
+    whydPath: string;
+    uploadDirName: string;
+    uAvatarImgDirName: string;
+    uCoverImgDirName: string;
+    uPlaylistDirName: string;
+  };
 };
 
 declare namespace NodeJS {
