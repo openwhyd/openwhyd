c18de6f244aef0356f9c38789a7f5ebe9a9cb4e7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-01 08:39:50 +0200

    update db init files so they can be run directly in vm.Script
    fixes `Cannot read properties of undefined (reading 'createIndex')`
    cf https://github.com/openwhyd/openwhyd/actions/runs/5723410041/job/15507960711?pr=643#step:7:12

--- config/initdb.js
+++ config/initdb.js
@@ -1,63 +1,70 @@
 /* globals db */
 
-// Usage: this file should be run by mongo's CLI:
-// $ mongo openwhyd_data whydDB/initdb.js
+// Usage: this file should be run by mongodb-shell-runner.js, on startup
 
 //print("connecting to openwhyd_data database...");
 //db = db.getSiblingDB("openwhyd_data"); //connect("localhost:27017/whyd_music");
 
 print('creating openwhyd collections...');
-db.createCollection('config');
-db.createCollection('email');
-db.createCollection('invite');
-db.createCollection('notif');
-db.createCollection('user');
-db.createCollection('follow');
-db.createCollection('post');
-db.createCollection('activity');
-db.createCollection('track');
-db.createCollection('comment');
+await Promise.all([
+  db.createCollection('config'),
+  db.createCollection('email'),
+  db.createCollection('invite'),
+  db.createCollection('notif'),
+  db.createCollection('user'),
+  db.createCollection('follow'),
+  db.createCollection('post'),
+  db.createCollection('activity'),
+  db.createCollection('track'),
+  db.createCollection('comment'),
+]);
 
 // print('indexing post collection...');
-db.post.createIndex({ uId: 1 });
-db.post.createIndex({ uId: 1, 'pl.id': 1 }, { sparse: true });
-db.post.createIndex({ 'pl.id': 1 }, { sparse: true });
-db.post.createIndex({ order: 1 }, { sparse: true });
-db.post.createIndex({ eId: 1 });
-db.post.createIndex({ lov: 1 }, { sparse: true });
-db.post.createIndex({ 'repost.pId': 1 }, { sparse: true });
-db.post.createIndex({ 'repost.uId': 1 }, { sparse: true });
+await db.collection('post').createIndex({ uId: 1 });
+await db
+  .collection('post')
+  .createIndex({ uId: 1, 'pl.id': 1 }, { sparse: true });
+await db.collection('post').createIndex({ 'pl.id': 1 }, { sparse: true });
+await db.collection('post').createIndex({ order: 1 }, { sparse: true });
+await db.collection('post').createIndex({ eId: 1 });
+await db.collection('post').createIndex({ lov: 1 }, { sparse: true });
+await db.collection('post').createIndex({ 'repost.pId': 1 }, { sparse: true });
+await db.collection('post').createIndex({ 'repost.uId': 1 }, { sparse: true });
 
 // print('indexing follow collection...');
-db.follow.createIndex({ uId: 1 });
-db.follow.createIndex({ tId: 1 });
+await db.collection('follow').createIndex({ uId: 1 });
+await db.collection('follow').createIndex({ tId: 1 });
 
 // print('indexing user collection...');
-db.user.createIndex({ email: 1 });
-db.user.createIndex({ handle: 1 }, { sparse: true });
-db.user.createIndex({ fbId: 1 }, { sparse: true });
-db.user.createIndex({ n: 1 }, { sparse: true });
-db.user.createIndex({ 'pref.pendEN': 1 }, { sparse: true });
-db.user.createIndex({ 'pref.nextEN': 1 }, { sparse: true });
-db.user.createIndex({ 'sp.id': 1 }, { sparse: true }); // spotify id
+await db.collection('user').createIndex({ email: 1 });
+await db.collection('user').createIndex({ handle: 1 }, { sparse: true });
+await db.collection('user').createIndex({ fbId: 1 }, { sparse: true });
+await db.collection('user').createIndex({ n: 1 }, { sparse: true });
+await db.collection('user').createIndex({ 'pref.pendEN': 1 }, { sparse: true });
+await db.collection('user').createIndex({ 'pref.nextEN': 1 }, { sparse: true });
+await db.collection('user').createIndex({ 'sp.id': 1 }, { sparse: true }); // spotify id
 
 // print('removing legacy fields on user collection...');
-db.user.dropIndex({ apTok: 1 });
-db.user.updateMany({}, { $unset: { apTok: 1 } });
+await db.collection('user').dropIndex({ apTok: 1 });
+await db.collection('user').updateMany({}, { $unset: { apTok: 1 } });
 
 // print('indexing activity collection...');
-db.activity.createIndex({ id: 1 }, { sparse: true }); /*poster.id*/
-db.activity.createIndex({ 'like.id': 1 }, { sparse: true });
-db.activity.createIndex({ 'like.pId': 1 }, { sparse: true });
+await db
+  .collection('activity')
+  .createIndex({ id: 1 }, { sparse: true }); /*poster.id*/
+await db.collection('activity').createIndex({ 'like.id': 1 }, { sparse: true });
+await db
+  .collection('activity')
+  .createIndex({ 'like.pId': 1 }, { sparse: true });
 
 // print('indexing track collection...');
-db.track.createIndex({ eId: 1 });
-db.track.createIndex({ score: 1 });
+await db.collection('track').createIndex({ eId: 1 });
+await db.collection('track').createIndex({ score: 1 });
 
 // print('indexing comment collection...');
-db.comment.createIndex({ pId: 1 });
+await db.collection('comment').createIndex({ pId: 1 });
 
 // print('indexing notif collection...');
-db.notif.createIndex({ uId: 1 });
+await db.collection('notif').createIndex({ uId: 1 });
 
 print('done! :-)');
