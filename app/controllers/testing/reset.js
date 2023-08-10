c132a79838fd9af61890be9413d1f050a154562c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-10 18:56:26 +0200

    fix: delete uploaded files before each test

--- app/controllers/testing/reset.js
+++ app/controllers/testing/reset.js
@@ -1,5 +1,8 @@
 // A private controller for Cypress to be able to reset the database between tests
 
+const path = require('path');
+const { readdirSync, rmSync } = require('fs');
+
 const mongodb = require('../../models/mongodb.js');
 
 exports.controller = async function (request, getParams, response) {
@@ -12,8 +15,17 @@ exports.controller = async function (request, getParams, response) {
     return response.forbidden(new Error('allowed on test database only'));
   }
   try {
+    // reinitialize database state
     await mongodb.clearCollections();
     await mongodb.initCollections({ addTestData: true });
+
+    // delete uploaded files
+    const appDir = process.cwd();
+    ['uAvatarImg', 'uCoverImg', 'upload_data'].forEach((subDir) => {
+      const dir = path.join(appDir, subDir);
+      readdirSync(dir).forEach((file) => rmSync(`${dir}/${file}`));
+    });
+
     response.renderJSON({ ok: true });
   } catch (err) {
     response.renderJSON({ error: err.message });
