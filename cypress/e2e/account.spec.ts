b07a69c7f7d15eeddefd2e66beaf185ad690a932
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-11 11:45:55 +0200

    fix user deletion test

--- cypress/e2e/account.spec.ts
+++ cypress/e2e/account.spec.ts
@@ -8,10 +8,14 @@ context('Openwhyd account', () => {
       cy.get('#loginDiv .username').should('have.text', admin.name);
     });
 
+    cy.contains('Got it!').click(); // click on the cookie banner, so it does not hide the "Delete your account" link
+
+    cy.scrollTo('bottom');
     cy.contains('Delete your account').click();
     cy.get('.dlg').should('be.visible');
 
     cy.contains('Delete my account').click();
+    cy.contains('Delete my data now!').click();
 
     cy.loginAsAdmin();
 
