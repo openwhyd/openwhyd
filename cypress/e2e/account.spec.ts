28ce2a597c56e8dac18fecf8393aee0bb71ee7bb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-11 15:11:37 +0200

    fix(tests): cover user account deletion

--- cypress/e2e/account.spec.ts
+++ cypress/e2e/account.spec.ts
@@ -1,28 +1,22 @@
 context('Openwhyd account', () => {
-  // TODO: fix user deletion so this test can pass
-  it.skip('can be deleted', function () {
+  it('can be deleted', function () {
     cy.loginAsAdmin();
     cy.visit('/settings');
 
     // user is logged in
-    cy.fixture('users.js').then(({ admin }) => {
-      cy.get('#loginDiv .username').should('have.text', admin.name);
-    });
+    cy.get('body').should('not.have.text', 'Sign up');
 
     cy.contains('Got it!').click(); // click on the cookie banner, so it does not hide the "Delete your account" link
 
     cy.scrollTo('bottom');
-    cy.contains('Delete your account').click();
+    cy.contains('Delete your account').click({ force: true });
     cy.get('.dlg').should('be.visible');
 
     cy.contains('Delete my account').click();
     cy.contains('Delete my data now!').click();
 
-    cy.loginAsAdmin();
-
     // user is NOT logged in
-    cy.fixture('users.js').then(({ admin }) => {
-      cy.get('#loginDiv .username').should('not.have.text', admin.name);
-    });
+    cy.visit('/');
+    cy.contains('Sign up');
   });
 });
