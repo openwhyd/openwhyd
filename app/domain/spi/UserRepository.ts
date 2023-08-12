82c8734d12b6e929481512cabf84b3afd12add09
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-12 11:51:19 +0200

    define types necessary to implement the feature

--- app/domain/spi/UserRepository.ts
+++ app/domain/spi/UserRepository.ts
@@ -3,10 +3,17 @@ import { Playlist, User } from '../user/types';
 export interface UserRepository {
   getByUserId: GetByUserId;
   insertPlaylist: InsertPlaylist;
+  removePlaylist: RemovePlaylist;
 }
 
 export type GetByUserId = (userId: string) => Promise<User>;
+
 export type InsertPlaylist = (
   userid: string,
   playlist: Playlist,
+) => Promise<void>;
+
+export type RemovePlaylist = (
+  userId: string,
+  playlistId: number,
 ) => Promise<void>;
