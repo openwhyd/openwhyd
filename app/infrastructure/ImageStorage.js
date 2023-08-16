09203d5ca4b610ef3aa46ef7d90377295da752bb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-16 14:56:15 +0200

    document types for ImageStorage

--- app/infrastructure/ImageStorage.js
+++ app/infrastructure/ImageStorage.js
@@ -2,13 +2,28 @@
 
 const fs = require('node:fs/promises');
 
-/** @typedef {import("../domain/spi/ImageRepository").ImageRepository} ImageRepository */
+/**
+ * @typedef {import("../domain/spi/ImageRepository").ImageRepository} ImageRepository
+ * @typedef {import('../domain/user/types').User} User
+ * @typedef {import('../domain/user/types').Playlist} Playlist
+ * @typedef {"uPlaylistImg"} SupportedImageDirectories // to be completed
+ */
 
+/**
+ * @param {SupportedImageDirectories} type
+ * @param {User["id"]} userId
+ * @param {Playlist["id"]} playlistId
+ */
 const getFilePath = (type, userId, playlistId) =>
   `./${type}/${userId}_${playlistId}`;
 
+/**
+ * @param {SupportedImageDirectories} type
+ * @param {User["id"]} userId
+ * @param {Playlist["id"]} playlistId
+ */
 const getFileUrl = (type, userId, playlistId) =>
-  `./${type}/${userId}_${playlistId}`;
+  `/${type}/${userId}_${playlistId}`;
 
 /** @implements {ImageRepository} */
 class ImageStorage {
