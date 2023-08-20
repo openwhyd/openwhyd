bfc199aa23a29997f33a08ef2a3e4b27a2902ce3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-20 14:35:19 +0200

    fix(security): fix: remove `jquery.history` contributes to #669. fixes https://github.com/openwhyd/openwhyd/security/code-scanning/15 and https://github.com/openwhyd/openwhyd/security/code-scanning/14

--- app/templates/mainTemplate.js
+++ app/templates/mainTemplate.js
@@ -214,14 +214,8 @@ exports.renderWhydFrame = function (html, params) {
     );
 
   out.push(
-    //	'    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>',
-    //	'    <script type="text/javascript" src="'+render.urlPrefix+'/js/jquery-1.10.2.min.js"></script>',
     '    <script src="/js/jquery-1.10.2.min.js"></script>',
     '    <script src="/js/jquery-migrate-1.2.1.js"></script>',
-    '    <script type="text/javascript" src="' +
-      render.urlPrefix +
-      '/js/jquery.history.js"></script>',
-    //	'    <script src="/js/soundmanager2.js"></script>',
     '    <script src="/js/soundmanager2-nodebug-jsmin.js"></script>',
     '    <script>soundManager.setup({url: "/swf/", flashVersion: 9, onready: function() {soundManager.isReady=true;}});</script>',
   );
