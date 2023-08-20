625c1aaf48db698f124c1a0ceabd1f34a355a789
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-20 09:36:36 +0200

    fix(pm2): `spawn node ENOENT` error after removing previous version of Node.js contributes to #656.
    inspiration
    - use of `--interpreter`: https://github.com/Unitech/pm2/issues/3648#issuecomment-1381662195
    - use of `nvm which default`: https://github.com/Unitech/pm2/issues/1413#issuecomment-125201497

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,12 +3,13 @@
 # Constants
 ROOT_DIR="`pwd`/.."
 PORT="1095"
-VERSION=$(node -p "require('$ROOT_DIR/package.json').version")
+VERSION=$(node -p "require('${ROOT_DIR}/package.json').version")
+NODE="$(nvm which default)"
 
 # NGINX_ENBLD=/etc/nginx/sites-enabled
 # NGINX_TEMPLATE=$ROOT_DIR/config/nginx-site-template
 # sudo nginx -t # make sure that the configuration is valid
 # sudo service nginx restart
 
-echo "👋  Restarting Openwhyd server ($VERSION) on port $PORT..."
-cd $ROOT_DIR && source env-vars-local.sh && WHYD_PORT=$PORT npx --yes pm2 restart app.js
+echo "👋  Restarting Openwhyd server ${VERSION} on port ${PORT}, with ${NODE}..."
+cd ${ROOT_DIR} && source env-vars-local.sh && WHYD_PORT=${PORT} npx --yes pm2 restart app.js --interpreter=${NODE}
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,8 @@
 
 ROOT_DIR="`pwd`/.."
 PORT="1095"
+VERSION=$(node -p "require('${ROOT_DIR}/package.json').version")
+NODE="$(nvm which default)"
 
-echo "🌄  Starting Openwhyd with WHYD_PORT=$PORT..."
-cd $ROOT_DIR && source env-vars-local.sh && WHYD_PORT=$PORT npx --yes pm2 start app.js
+echo "🌄  Starting Openwhyd ${VERSION} on port ${PORT}, with ${NODE}..."
+cd ${ROOT_DIR} && source env-vars-local.sh && WHYD_PORT=${PORT} npx --yes pm2 start app.js --interpreter=${NODE}
