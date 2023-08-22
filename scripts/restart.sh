8858b4ae3a50da8383fceb2a5f62d3dc0d9984cf
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-22 16:59:00 +0200

    fix(production): configure Datadog APM for telemetry cf https://app.datadoghq.com/source-code/setup/apm

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e # stop an any error
+
 # Constants
 ROOT_DIR="`pwd`/.."
 PORT="1095"
 VERSION=$(node -p "require('${ROOT_DIR}/package.json').version")
 NODE="$(which node)"
+GIT_COMMIT_SHA=$(git rev-parse HEAD)
+GIT_REPOSITORY_URL="github.com/openwhyd/openwhyd"
 
 # NGINX_ENBLD=/etc/nginx/sites-enabled
 # NGINX_TEMPLATE=$ROOT_DIR/config/nginx-site-template
@@ -12,4 +16,8 @@ NODE="$(which node)"
 # sudo service nginx restart
 
 echo "👋  Restarting Openwhyd server ${VERSION} on port ${PORT}, with ${NODE}..."
-cd ${ROOT_DIR} && source env-vars-local.sh && WHYD_PORT=${PORT} npx --yes pm2 restart app.js --interpreter=${NODE}
+cd ${ROOT_DIR}
+source env-vars-local.sh
+DD_TAGS="git.commit.sha:${GIT_COMMIT_SHA},git.repository_url:${GIT_REPOSITORY_URL}" \
+WHYD_PORT=${PORT} \
+  npx --yes pm2 restart app.js --interpreter=${NODE}
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+set -e # stop an any error
+
+# Constants
 ROOT_DIR="`pwd`/.."
 PORT="1095"
 VERSION=$(node -p "require('${ROOT_DIR}/package.json').version")
 NODE="$(which node)"
+GIT_COMMIT_SHA=$(git rev-parse HEAD)
+GIT_REPOSITORY_URL="github.com/openwhyd/openwhyd"
 
 echo "🌄  Starting Openwhyd ${VERSION} on port ${PORT}, with ${NODE}..."
-cd ${ROOT_DIR} && source env-vars-local.sh && WHYD_PORT=${PORT} npx --yes pm2 start app.js --interpreter=${NODE}
+cd ${ROOT_DIR}
+source env-vars-local.sh
+DD_TAGS="git.commit.sha:${GIT_COMMIT_SHA},git.repository_url:${GIT_REPOSITORY_URL}" \
+WHYD_PORT=${PORT} \
+  npx --yes pm2 start app.js --interpreter=${NODE}
