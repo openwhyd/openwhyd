130927439f7a21987ef3ddf88becb56a73983fc6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-28 11:11:29 +0200

    playlist tests: make sure that freshly created playlist is persisted in db

--- test/api/playlist.api.tests.js
+++ test/api/playlist.api.tests.js
@@ -5,8 +5,14 @@ const request = require('request');
 const { cleanup, URL_PREFIX, DUMMY_USER } = require('../fixtures.js');
 const api = require('../api-client.js');
 const { START_WITH_ENV_FILE } = process.env;
-const { startOpenwhydServer } = require('../approval-tests-helpers.js');
+const {
+  startOpenwhydServer,
+  dumpMongoCollection,
+} = require('../approval-tests-helpers.js');
 const randomString = () => Math.random().toString(36).substring(2, 9);
+
+const MONGODB_URL =
+  process.env.MONGODB_URL || 'mongodb://localhost:27117/openwhyd_test';
 
 function callPlaylistApi(jar, form) {
   return new Promise((resolve, reject) =>
@@ -45,12 +51,20 @@ describe(`playlist api`, function () {
   );
 
   it('should create a playlist', async function () {
+    // When a user creates a playlist
     const playlistName = `playlist-${randomString()}`;
     const res = await callPlaylistApi(jar, {
       action: 'create',
       name: playlistName,
     });
 
+    // Then the playlist is persisted for that user
+    const user = await dumpMongoCollection(MONGODB_URL, 'user').then((users) =>
+      users.find((user) => user._id.toString() === DUMMY_USER.id),
+    );
+    assert.deepEqual(user.pl, [{ id: 0, name: playlistName }]);
+
+    // And the API returns the playlist's id and name
     const { id, name } = JSON.parse(res.body);
     assert.equal(name, playlistName);
     assert.equal(id, 0);
