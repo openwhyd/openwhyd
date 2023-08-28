e9af1c8c03434762e5c9ff77d60e31eff129d867
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-08-28 14:57:58 +0200

    fixtures: make test users immutable

--- test/fixtures.js
+++ test/fixtures.js
@@ -20,7 +20,7 @@ exports.FAKE_ID = 'a0000000000000000000000a';
 exports.URL_PREFIX = 'http://localhost:8080';
 
 // inserted by config/initdb_testing.js
-exports.ADMIN_USER = {
+exports.ADMIN_USER = Object.freeze({
   id: '000000000000000000000001',
   email: process.env.WHYD_ADMIN_EMAIL || 'test@openwhyd.org',
   name: 'admin',
@@ -28,10 +28,10 @@ exports.ADMIN_USER = {
   password: 'admin',
   pwd: 'admin',
   md5: '21232f297a57a5a743894a0e4a801fc3',
-};
+});
 
 // inserted by config/initdb_testing.js
-exports.DUMMY_USER = {
+exports.DUMMY_USER = Object.freeze({
   id: '000000000000000000000002',
   email: 'dummy@openwhyd.org',
   name: 'dummy',
@@ -39,16 +39,16 @@ exports.DUMMY_USER = {
   password: 'admin',
   pwd: 'admin',
   md5: '21232f297a57a5a743894a0e4a801fc3',
-};
+});
 
-exports.TEST_USER = {
+exports.TEST_USER = Object.freeze({
   email: 'test-user@openwhyd.org',
   name: 'Test User',
   username: 'test-user',
   pwd: 'test-user',
   password: 'test-user', // for the /register api endpoint
   md5: '42b27efc1480b4fe6d7eaa5eec47424d',
-};
+});
 
 // Call this before each test to prevent side effects between tests
 exports.cleanup = async function ({ silent } = { silent: false }) {
