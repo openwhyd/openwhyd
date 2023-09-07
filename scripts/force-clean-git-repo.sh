414719582a8010da223f335d3d96b98495c814c4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-09-07 17:22:05 +0200

    fix(scripts): 🧹 remove old git script

--- scripts/force-clean-git-repo.sh
+++
@@ -1,7 +0,0 @@
-git checkout main
-git branch -m backup  # Rename the current branch to backup
-git checkout --orphan newBranch
-git add -A  # Add all files and commit them
-git commit -m "clean openwhyd release"
-git branch -m main  # Rename the current branch to main
-git push -f origin main  # Force push main branch to github
