0652dd3ba11fb7b429a6b3fb3091f7c8487b6f3b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-09-20 15:49:15 +0200

    🧹 drop the `track` collection

--- app/models/track.js
+++ app/models/track.js
@@ -1,40 +1,5 @@
 // @ts-check
 
-/**
- * track model
- * - maintained by post model: updateByEid()
- * - read by hot tracks controller: getHotTracksFromDb()
- * - read by notif template: getHotTracksFromDb()
- * @author adrienjoly, whyd
- **/
-
-/*
-  - - - structure of a track record - - -
-  _id: automatically-assigned identifier (ObjectId)
-  eId: uri used to identify and embed this track's stream from its source
-  img: url of the image thumb of that track
-  name: name of the track (combines artist and title) // TODO: which version is stored in case of conflict?
-  score: the higher this number, the higher this track will rank in the hot tracks. computed from various metrics.
-  nbR: number of posts/reposts within the HOT_TRACK_TIME_WINDOW period
-  nbL: number of likes
-  nbP: number of plays
-  prev: score value at last snapshot (used to compute trends)
-  meta: {art, tit, dur, c, t}: concise metadata extracted from 3rd-parties (artist name, track title, duration, confidence 0<1, timestamp)
-  alt: [eId1, eId2...]: array of alternative souces for that track (cross-platform mappings)
-
-  - - - old fields - - -
-  prevRank: used to store the rank/index of that record in the track collection, before the score was updated
-  nbFreshPosts: used to store the number of posts within the HOT_TRACK_TIME_WINDOW
-  nbTotalPosts: duplicate of nbR
-  nbTotalLikes: duplicate of nbL
-  nbTotalPlays: duplicate of nbP
-
-  - - - notice - - -
-  since the periodic call of refreshHotTracksCache() was removed:
-  -> the prevRank is never updated => no movement information will be displayed on openwhyd.org/hot
-  -> the score of a track will only be updated on a (re-)post, like, or play operation on that track
-*/
-
 const config = require('./config.js');
 const mongodb = require('./mongodb.js');
 const feature = require('../features/hot-tracks.js');
