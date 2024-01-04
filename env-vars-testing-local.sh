57c66a6a165ab5cb015b262a55969df8f37909ae
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2024-01-04 19:53:49 +0100

    chore: script to import user tracks to another user (#759)
    ## Goal
    
    Help Robert (and other users) import his playlists to his new account.
    
    ## Important
    
    - the list of playlists of the destination user will be overwritten
       => existing tracks (if any) on that profile may end up in old playlists.
    - on each imported track, the number of plays, reposts and likes will be reset to zero.
    
    ## Usage in development
    
    ```sh
    $ . ./env-vars-testing-local.sh
    $ node scripts/import-from-prod.js adrien # imports 21 tracks from openwhyd.com/adrien --> local id: 4d94501d1f78ac091dbc9b4d
    $ node scripts/import-posts.js 4d94501d1f78ac091dbc9b4d 000000000000000000000002 # copy to local id: 000000000000000000000002
    ```
    
    ## Usage in production
    
    ```sh
    $ . env-vars-local.sh
    $ node scripts/import-posts.js 5f0f0d701125f9d3afca1bcc 6591c8dd0890c870388d2508 # imports all posts from https://openwhyd.org/u/5f0f0d701125f9d3afca1bcc
    ```

--- env-vars-testing-local.sh
+++ env-vars-testing-local.sh
@@ -1,3 +1,4 @@
 . ./env-vars-testing.sh
 . ./.env-local # helpful to store secrets, e.g. credentials for auth0 development tenant
 export MONGODB_PORT=27117
+export URL_PREFIX="http://localhost:8080"
