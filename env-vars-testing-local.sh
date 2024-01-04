caa08163a389e4fe33be575f0064598e89f2400a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2024-01-04 20:01:14 +0100

    fix(import-posts): use WHYD_URL_PREFIX env var

--- env-vars-testing-local.sh
+++ env-vars-testing-local.sh
@@ -1,4 +1,3 @@
 . ./env-vars-testing.sh
 . ./.env-local # helpful to store secrets, e.g. credentials for auth0 development tenant
 export MONGODB_PORT=27117
-export URL_PREFIX="http://localhost:8080"
