fb12711c6ba0c94f74486062db2330503039ac57
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2024-08-18 12:21:18 +0200

    fix(lint): `Property 'BandcampPlayer' does not exist on type 'Window & typeof globalThis'.` in public/js/whydPlayer.js

---
+++ public/js/types.d.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface Window {
+  SoundCloudPlayer;
+  VimeoPlayer;
+  DailymotionPlayer;
+  DeezerPlayer;
+  BandcampPlayer;
+  JamendoPlayer;
+}
