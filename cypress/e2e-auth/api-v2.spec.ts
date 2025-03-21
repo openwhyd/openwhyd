46c3d90a130db2c55635e08a64840ab2dc2a0cff
Adrien JOLY <531781+adrienjoly@users.noreply.github.com>
2025-03-21 16:35:35 +0100

    fix `CypressError: Cypress detected that you returned a promise from a command while also invoking one or more cy commands in that promise`

--- cypress/e2e-auth/api-v2.spec.ts
+++ cypress/e2e-auth/api-v2.spec.ts
@@ -1,5 +1,5 @@
 context('Openwhyd API v2, with auth0', () => {
-  it('should respond with a 401 status code when trying to add a track without token', async function () {
+  it('should respond with a 401 status code when trying to add a track without token', function () {
     cy.request('POST', `/api/v2/postTrack`, {}).its('status').should('be', 401);
   });
 });
