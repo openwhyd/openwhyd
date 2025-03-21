32a6474d251a028ed7ea8cd56052b586a4ed25ac
Adrien JOLY <531781+adrienjoly@users.noreply.github.com>
2025-03-21 14:33:07 +0100

    test(auth): forbids legacy login

--- cypress/e2e-auth/account.spec.ts
+++ cypress/e2e-auth/account.spec.ts
@@ -1,9 +1,12 @@
-context('Openwhyd auth', () => {
+context('Openwhyd with auth0', () => {
   it('redirects to a auth0 domain to sign up', function () {
     cy.visit('/');
-
     cy.get('#signup').click();
-
     cy.url().should('include', 'auth0.com/');
+  });
+
+  it('forbids legacy login', function () {
+    cy.loginAsAdmin();
+    cy.get('#signup').should('be.visible');
   });
 });
