f57cb732612cce69919d81ad1109c08443864d65
Adrien JOLY <531781+adrienjoly@users.noreply.github.com>
2025-03-21 16:31:51 +0100

    fix: turn API v2 test into Cypress test, so CI can use the Auth0 creds required by that endpoint

---
+++ cypress/e2e-auth/api-v2.spec.ts
@@ -0,0 +1,5 @@
+context('Openwhyd API v2, with auth0', () => {
+  it('should respond with a 401 status code when trying to add a track without token', async function () {
+    cy.request('POST', `/api/v2/postTrack`, {}).its('status').should('be', 401);
+  });
+});
