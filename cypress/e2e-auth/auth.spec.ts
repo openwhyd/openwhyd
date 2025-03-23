95433156c1898efab769e18d02706d3dc97eaf5f
Adrien JOLY <531781+adrienjoly@users.noreply.github.com>
2025-03-23 15:45:17 +0100

    fix(ci): give more time for auth0 page to load cf https://cloud.cypress.io/projects/8mw8bh/runs/1882/overview/0736468f-383a-4032-85aa-c91a5a6aa283?roarHideRunsWithDiffGroupsAndTags=1

--- cypress/e2e-auth/auth.spec.ts
+++ cypress/e2e-auth/auth.spec.ts
@@ -1,7 +1,7 @@
 context('Openwhyd with auth0', () => {
   it('redirects to a auth0 domain to sign up', function () {
     cy.visit('/');
-    cy.get('#signup').click({ timeout: 30000 });
+    cy.get('#signup').click();
     cy.url().should('include', 'auth0.com/');
   });
 
