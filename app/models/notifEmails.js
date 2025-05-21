425b27b3a3ebaa4306c62ff632f2078c7801ce85
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-05-21 14:00:21 +0200

    fix(notif): notify mentioned user instead of user who posted the track

--- app/models/notifEmails.js
+++ app/models/notifEmails.js
@@ -231,7 +231,7 @@ exports.sendComment = async function (post, comment, cb) {
 
 // 6) "XXX mentioned you"
 exports.sendMention = async function (mentionedUid, post, comment, cb) {
-  const prefs = await getUserPrefs(post.uId); // TODO: there may be a bug here => replace by mentionedUid?
+  const prefs = await getUserPrefs(mentionedUid);
   if (prefs['emMen'] == -1)
     cb && cb({ warn: 'no email notification will be sent (disabled by user)' });
   else
