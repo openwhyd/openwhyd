797f07591cca0a5ad03ca11a06f62ff12ec6f1df
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-06-11 15:02:53 +0200

    test: clarify that we send text and html version of the same email + make async

--- app/controllers/admin/testEmail.js
+++ app/controllers/admin/testEmail.js
@@ -3,37 +3,35 @@
 const notifModel = require('../../templates/notif.js');
 const emailModel = require('../../models/email.js');
 
-function sendEmails(user, template, cb) {
-  //var fbCookie = request.getFacebookCookie();
-  //if (!fbCookie || !fbCookie.uid)
-  //	emailModel.notif(user.id, template.subject, template.bodyText, fbCookie.access_token);
-  //else
-  emailModel.email(
-    user.email,
-    '[TEXT] ' + template.subject,
-    template.bodyText,
-    null,
-    user.name,
-    function (resText) {
-      emailModel.email(
-        user.email,
-        '[HTML] ' + template.subject,
-        null,
-        template.bodyHtml,
-        user.name,
-        function (resHtml) {
-          cb({
-            to: user.email,
-            subject: template.subject,
-            textResult: resText,
-            htmlResult: resHtml,
-            bodyText: template.bodyText,
-            bodyHtml: template.bodyHtml,
-          });
-        },
-      );
-    },
+async function sendEmails(user, template) {
+  const resText = await new Promise((resolve) =>
+    emailModel.email(
+      user.email,
+      template.subject + ' - TEXT version',
+      template.bodyText,
+      null,
+      user.name,
+      resolve,
+    ),
   );
+  const resHtml = await new Promise((resolve) =>
+    emailModel.email(
+      user.email,
+      template.subject + ' - HTML version',
+      null,
+      template.bodyHtml,
+      user.name,
+      resolve,
+    ),
+  );
+  return {
+    to: user.email,
+    subject: template.subject,
+    textResult: resText,
+    htmlResult: resHtml,
+    bodyText: template.bodyText,
+    bodyHtml: template.bodyHtml,
+  };
 }
 
 exports.sendTestEmail = async function (user) {
@@ -45,7 +43,7 @@ exports.sendTestEmail = async function (user) {
   //     resolve,
   //   ),
   // );
-  return await new Promise((resolve) => sendEmails(user, email, resolve));
+  return await sendEmails(user, email);
 };
 
 exports.controller = async function (request, reqParams, response) {
