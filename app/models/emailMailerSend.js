8add3d29d127ef33d73344136a07926abf5560f8
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-06-11 14:35:08 +0200

    fix function to send emails

--- app/models/emailMailerSend.js
+++ app/models/emailMailerSend.js
@@ -9,7 +9,7 @@ if (MAILERSEND_API_KEY === undefined)
   throw new Error(`missing env var: MAILERSEND_API_KEY`);
 
 /** may reject in case of wrong credentials */
-exports.email = async function (
+exports.email = function (
   emailAddr,
   subject,
   textContent,
@@ -27,7 +27,7 @@ exports.email = async function (
   if (process.env['MAILERSEND_API_FROM_NAME'] === undefined)
     throw new Error(`missing env var: MAILERSEND_API_FROM_NAME`);
 
-  const res = await fetch('https://api.mailersend.com/v1/email', {
+  fetch('https://api.mailersend.com/v1/email', {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${MAILERSEND_API_KEY}`,
@@ -49,36 +49,13 @@ exports.email = async function (
       text: textContent,
     }),
   })
-    try {
-    const response = await fetch('https://api.mailersend.com/v1/email', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${MAILERSEND_API_KEY}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        from: {
-          email: process.env.MAILERSEND_API_FROM_EMAIL, // e.g. "no-reply@whyd.org",
-          name: process.env.MAILERSEND_API_FROM_NAME, // e.g. "whyd",
-        },
-        to: [
-          {
-            email: emailAddr,
-            ...(userName ? { name: userName } : {}),
-          },
-        ],
-        subject,
-        html: htmlContent,
-        text: textContent,
-      }),
+    .then((response) => response.text())
+    .then((res) => {
+      console.log('[EMAIL] response:', res);
+      callback?.({ ok: true });
+    })
+    .catch((error) => {
+      console.error('[EMAIL] send error:', error);
+      callback?.({ error });
     });
-    const res = await response.json();
-  } catch (error) {
-    console.error('[EMAIL] send error:', error);
-    callback?.({ error });
-    return;
-  }
-
-  console.log('[EMAIL] response:', res);
-  callback?.({ ok: true });
 };
